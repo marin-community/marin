5d23d8d3fd10e01fb2f6c92eee976a59f990bc4f
David Hall <dlwh@stanford.edu>
2024-03-27 22:58:57 -0700

    wip

--- process_rpv2.py
+++ process_rpv2.py
@@ -2,8 +2,11 @@ import gzip
 import json
 import logging
 import os
+from urllib.parse import urljoin
 
 import html2text
+from bs4 import BeautifulSoup
+
 import trafilatura
 import readabilipy
 
@@ -17,9 +20,19 @@ h = html2text.HTML2Text()
 h.ignore_links = False  # Optionally ignore links
 h.body_width = 0  # Optionally disable line wrapping
 # TODO: html2text uses [code]...[/code] for code blocks. would prefer github markdown style
+# Could also use some kind of PL lang-id to highlight code blocks, but probably not super necessary
 h.mark_code = True  # Optionally convert code blocks to markdown
 h.include_sup_sub = True  # Optionally include <sup> and <sub> tags
 h.pad_tables = False  # We disable b/c it seems like a silly thing for the LLM to do
+
+
+def make_image_links_absolute(html_content, base_url):
+    """Converts relative image URLs to absolute URLs."""
+    soup = BeautifulSoup(html_content, "html.parser")
+    for img in soup.find_all("img"):
+        if img.has_attr("src"):
+            img["src"] = urljoin(base_url, img["src"])
+    return str(soup)
 
 def process_single_doc(url, snapshot_id):
     # Implement or integrate your methods for fetching the WARC location and HTML content
@@ -28,23 +41,32 @@ def process_single_doc(url, snapshot_id):
         logger.warning(f"WARC location not found for URL: {url}")
         return None
 
-    html_content = fetch_page_from_cc(warc_location)
+    html_content = fetch_page_from_cc(warc_location[0:1])
     if not html_content:
         logger.warning(f"Failed to fetch content for URL: {url}")
         return None
 
-    trafilatura_result = trafilatura.extract(html_content, include_formatting=True, output_format="txt", include_images=True, include_links=True)
-    trafilatura_xml = trafilatura.extract(html_content, output_format="xml")
+    html_content = make_image_links_absolute(html_content, url)
+
+    # trafilatura_result = trafilatura.extract(html_content, include_formatting=True, output_format="txt", include_images=True, include_links=True)
+    # trafilatura_xml = trafilatura.extract(html_content, output_format="xml")
 
     cleaned_article = readabilipy.simple_json_from_html_string(html_content, use_readability=True)
     if cleaned_article:
         cleaned_html = cleaned_article["content"]
         cleaned_md = h.handle(cleaned_html)
+        # add title and byline to the markdown
+        if cleaned_article["title"]:
+            prefix = f"# {cleaned_article['title']}\n\n"
+        else:
+            prefix = ""
+        if cleaned_article["byline"]:
+            prefix += f"Author: {cleaned_article['byline']}\n\n"
+        if prefix:
+            cleaned_md = prefix + cleaned_md
     else:
         logger.warning(f"Failed to extract content from URL: {url}")
         return None
-
-    plain_text = '\n\n'.join(p['text'] for p in cleaned_article["plain_text"][-1:])
 
     return {
         "content": cleaned_md,
@@ -54,9 +76,8 @@ def process_single_doc(url, snapshot_id):
         "plain_content": cleaned_article["plain_content"],
         "original_html": html_content,
         "readable_html": cleaned_html,
-        "readable_text": plain_text,
-        "patched_traf_content": trafilatura_result,
-        "traf_xml": trafilatura_xml,
+        # "patched_traf_content": trafilatura_result,
+        # "traf_xml": trafilatura_xml,
     }
 
 
@@ -72,23 +93,13 @@ def process_files(snapshot, n, lang, part):
                     "id": qs["id"],
                     "id_int": qs["id_int"],
                     "metadata": qs["metadata"],
-                    "content": cleaned_article["content"],
-                    "title": cleaned_article["title"],
-                    "byline": cleaned_article["byline"],
-                    "date": cleaned_article["date"],
-                    "plain_content": cleaned_article["plain_content"],
                     # "plain_text": cleaned_article["plain_text"],
                     "is_duplicate": qs["is_duplicate"],
                     "quality_signals": qs["quality_signals"],  # Include the entire quality signals data
-                    "original_html": cleaned_article["original_html"],
-                    "readable_html": cleaned_article["readable_html"],
-                    "readable_text": cleaned_article["readable_text"],
-                    # "traf_content": cleaned_article["traf_content"],
-                    "traf_xml": cleaned_article["traf_xml"],
-                    "patched_traf_content": cleaned_article["patched_traf_content"],
+                    **cleaned_article,
                 }
             else:
-                logger.exception(f"Content could not be processed for {doc_id} URL: {url}")
+                logger.error(f"Content could not be processed for {doc_id} URL: {url}")
         except Exception as e:
             logger.exception(f"Error processing {doc_id}: {e}")
 
@@ -97,14 +108,16 @@ if __name__ == "__main__":
     snapshot = "2023-06"
     part = "middle"
     i = 0
+    base_dir = f"output/{snapshot}/{lang}"
+    os.makedirs(base_dir, exist_ok=True)
     for n in range(NUM_SHARDS):
         with gzip.open(f"{lang}_{n:04d}.jsonl.gz", "wt") as f:
             for doc in process_files(snapshot, n, lang, part):
                 i += 1
                 f.write(json.dumps(doc) + "\n")
                 id = doc["id_int"]
-                os.makedirs(f"{lang}_{n:04d}_{id}", exist_ok=True)
-                for key in ["content", "readable_html", "readable_text", "original_html", "patched_traf_content", "traf_xml"]:
+                os.makedirs(f"{base_dir}/{lang}_{n:04d}_{id}", exist_ok=True)
+                for key in ["content", "readable_html", "original_html"]:
                     suf = "md"
                     if "html" in key:
                         suf = "html"
@@ -112,11 +125,11 @@ if __name__ == "__main__":
                         suf = "txt"
                     elif "xml" in key:
                         suf = "xml"
-                    with open(f"{lang}_{n:04d}_{id}/{key}.{suf}", "w") as out_f:
+                    with open(f"{base_dir}/{lang}_{n:04d}_{id}/{key}.{suf}", "w") as out_f:
                         out_f.write(str(doc[key]))
 
                 qs = doc["quality_signals"]
-                with open(f"{lang}_{n:04d}_{id}/quality_signals.json", "w") as out_f:
+                with open(f"{base_dir}/{lang}_{n:04d}_{id}/quality_signals.json", "w") as out_f:
                     json.dump(qs, out_f, indent=2)
 
                 if i % 100 == 0:
