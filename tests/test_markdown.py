0d759902f22c41cb1bbd64e6ccaeeb42623b52fd
David Hall <dlwh@stanford.edu>
2024-04-01 00:41:17 -0700

    wip mathml to latex

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -1,4 +1,6 @@
-from markdown import minimal_markdown_escape
+import pytest
+
+from markdown import mathml_to_markdown, minimal_markdown_escape
 
 
 def test_always_escaped():
@@ -51,21 +53,69 @@ def test_line_start_escaping():
         assert minimal_markdown_escape(text) == expected
 
 
-
 def test_combined_scenarios():
     """Tests with multiple special characters."""
     input_text = \
-    r"""This is *bold* and _italic_.
+        r"""This is *bold* and _italic_.
     # Header 1
     - List item 1
     + List item 2
     This has [brackets] and \backslashes\.
     """
     expected_text = \
-    r"""This is \*bold\* and \_italic\_.
+        r"""This is \*bold\* and \_italic\_.
     \# Header 1
     \- List item 1
     \+ List item 2
     This has \[brackets\] and \backslashes\\.
     """
     assert minimal_markdown_escape(input_text) == expected_text
+
+
+def test_mathml_to_markdown():
+    """Tests conversion of MathML to markdown."""
+    test_cases = [
+        ("<math><mi>x</mi><mo>+</mo><mn>1</mn></math>", "$ x+1$"),
+        # integration
+        ("<math><mo>&#x222B;</mo><mi>x</mi><mi>d</mi><mi>x</mi></math>", r"$ \int xdx$"),
+        # some fancy information theory: KL divergence
+        ("<math><mi>D</mi><mo stretchy=\"false\">(</mo><mi>P</mi><mo>&#x2225;</mo><mi>Q</mi><mo stretchy=\"false\">)</mo></math>", r"$ D(P\parallel Q)$"),
+        # chain rule of probability
+        ("<math><mi>P</mi><mo stretchy=\"false\">(</mo><mi>A</mi><mo stretchy=\"false\">)</mo><mo>=</mo><mi>P</mi><mo stretchy=\"false\">(</mo><mi>A</mi><mo stretchy=\"false\">|</mo><mi>B</mi><mo stretchy=\"false\">)</mo><mi>P</mi><mo stretchy=\"false\">(</mo><mi>B</mi><mo stretchy=\"false\">)</mo></math>",
+         r"$ P(A)=P(A|B)P(B)$"),
+        # a fraction
+        ("""
+            <math xmlns='http://www.w3.org/1998/Math/MathML' display='inline'>  
+      <mfrac>  
+        <mfrac>  
+          <mn>1</mn>  
+          <mi>x</mi>  
+        </mfrac>  
+        <mrow>  
+          <mi>y</mi>  
+          <mo>-</mo>  
+          <mn>2</mn>  
+        </mrow>  
+      </mfrac>  
+    </math>""", r"$ \frac{\frac{1}{x}}{y-2}$"),
+        # pythagorean theorem
+        # ("""<math xmlns='http://www.w3.org/1998/Math/MathML' display="inline"> <mrow> <msup> <mi>a</mi> <mn>2</mn> </msup> <mo>+</mo> <msup> <mi>b</mi> <mn>2</mn> </msup> <mo>=</mo> <msup> <mi>c</mi> <mn>2</mn> </msup> </mrow> </math>""",
+        #  r"$ a^2+b^2=c^2$"),
+        # More complicated exponentiation gets brackets
+        ('<math display="inline"><msup><mi>x</mi><mrow><mi>y</mi><mo>+</mo><mn>1</mn></mrow></msup></math>', "$ x^{y+1}$"),
+        # subscripts
+        ('<math display="inline"><msub><mi>x</mi><mi>i</mi></msub></math>', "$ x_i$"),
+        # subscripts and superscripts
+        ('<math display="inline"><msubsup><mi>x</mi><mi>i</mi><mn>2</mn></msubsup></math>', "$ x_i^2$"),
+        # complex subscripts and superscripts
+        ('<math display="inline"><msubsup><mi>x</mi><mrow><mi>i</mi><mo>+</mo><mn>1</mn></mrow><mrow><mi>z</mi><mo>-</mo><mn>1</mn></mrow></msubsup></math>', "$ x_{i+1}^{z-1}$"),
+    ]
+    for mathml, expected in test_cases:
+        assert mathml_to_markdown(mathml) == expected
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", "test_markdown.py"])
+
+
+
