e12ee4c8d2b0ee9d29856dfbcad1476b0020ad4e
David Hall <dlwh@stanford.edu>
2024-04-19 22:40:40 -0700

    add a readme with thoughts on markdown conversion

--- scripts/web/process_url.py
+++ scripts/web/process_url.py
@@ -6,10 +6,13 @@ import sys
 
 import fsspec
 
-from markweb.markdown import html2text_markdown, to_markdown
+import markweb.markweb
+from markweb.markdown import to_markdown
 
 
 if __name__ == '__main__':
+    out_dir = "output"
+    os.makedirs(out_dir, exist_ok=True)
     for orig_html_path in sys.argv[1:]:
         # being a little sneaky. not really doing crawling.
         with fsspec.open(orig_html_path, "r",
@@ -27,6 +30,10 @@ if __name__ == '__main__':
 
         if orig_html_path.endswith("/"):
             orig_html_path = orig_html_path[:-1]
+        if "?" in orig_html_path:
+            maybe_orig_html_path = orig_html_path.split("?")[0]
+            if maybe_orig_html_path:
+                orig_html_path = maybe_orig_html_path
 
         base_name = os.path.basename(orig_html_path)
         base_name = os.path.splitext(base_name)[0]
@@ -51,25 +58,18 @@ if __name__ == '__main__':
             # with open(f"{base_name}.traf.md", "w") as f:
             #     print(markdown, file=f)
 
-        import readabilipy
-        reabilitied = readabilipy.simple_json_from_html_string(html, use_readability=True)
-        # readabilipy is beautifulsoup-ba
-        # tree_str = ET.tostring(tree, pretty_print=True).decode()
-        tree = reabilitied["content"]
-        tree_str = str(tree)
-        markdown2 = html2text_markdown(tree_str)
+        out = markweb.markweb.convert_page(html, url=orig_html_path)
+        title = out["title"]
+        md = out["content"]
 
-        title = reabilitied["title"]
+        with open(f"{out_dir}/{base_name}.orig.html", "w") as f:
+            print(html, file=f)
 
-        with open(f"{base_name}.readability.html", "w") as f:
-            f.write(tree_str)
 
-        with open(f"{base_name}.readability.md", "w") as f:
-            print(f"# {title}\n", file=f)
-            print(markdown2, file=f)
+        with open(f"{out_dir}/{base_name}.readability.html", "w") as f:
+            print(out["html"], file=f)
 
-        md = to_markdown(tree)
-        with open(f"{base_name}.readability.markdownify.md", "w") as f:
+        with open(f"{out_dir}/{base_name}.md", "w") as f:
             print(f"# {title}\n", file=f)
             print(md, file=f)
 
