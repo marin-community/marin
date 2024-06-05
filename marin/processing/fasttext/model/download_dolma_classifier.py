f973aef1b0c3f3b90e1cb6e57e046e6b6988fdf9
Christopher Chou <emu4yolife@gmail.com>
2024-06-05 23:59:35 +0000

    Download dolma model

---
+++ marin/processing/fasttext/model/download_dolma_classifier.py
@@ -0,0 +1,23 @@
+"""
+Usage:
+python marin/processing/fasttext/model/download_dolma_classifier.py
+ray job submit --working-dir . --no-wait -- python marin/processing/fasttext/model/download_dolma_classifier.py
+"""
+import os
+from huggingface_hub import hf_hub_download
+
+# Define the repository and file details
+repo_id = "allenai/dolma-1_7-fasttext-quality-filter"
+filename = "model.bin"
+local_filename = "model.bin"
+local_dir = os.path.expanduser("~/dolma_fasttext_model")
+
+# Create the local folder if it doesn't exist
+os.makedirs(local_dir, exist_ok=True)
+
+# Download the file
+downloaded_file_path = hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir, local_dir_use_symlinks=False)
+
+# Move the downloaded file to the desired location
+destination_path = os.path.join(local_dir, local_filename)
+print(f"File downloaded to: {destination_path}")
