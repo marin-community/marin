5ee82e94e8c4bb759a1452b4b6cdf23722fe8095
Joel Niklaus <me@joelniklaus.ch>
2024-06-06 08:22:29 -0700

    Added the option to pass the Hugging Face dataset config and revision to enable the copying of more datasets. Since the pile of law and the MultiLegalPile are complicated composite datasets, their data loading scripts compile the sources dynamically, thus hindering the vanilla script to work. Therefore I added an ugly hack to copy the MultiLegalPile also containing the pile of law.

--- scripts/copy_hf_dataset_to_gcs.py
+++ scripts/copy_hf_dataset_to_gcs.py
@@ -18,31 +18,41 @@ from google.auth import default
 from google.cloud import storage, storage_transfer_v1
 
 
-
 def get_urls_to_copy(dataset_name: str, **kwargs):
     fs = HfFileSystem()
-    ds_builder = datasets.load_dataset_builder(dataset_name, **kwargs)
+    ds_builder = datasets.load_dataset_builder(dataset_name, trust_remote_code=True, **kwargs)
     config = ds_builder.config
     download_config = DownloadConfig(token=ds_builder.token, storage_options=ds_builder.storage_options)
     config._resolve_data_files(base_path=ds_builder.base_path, download_config=download_config)
 
+    if dataset_name == "joelniklaus/MultiLegalPileWikipediaFiltered":  # ugly hack to download this dataset
+        base_url = "https://huggingface.co/datasets/joelniklaus/MultiLegalPileWikipediaFiltered/resolve/main/data"
+        urls = [f"{base_url}/en_caselaw_train_{i}" for i in range(66)]
+        urls.extend([f"{base_url}/en_contracts_train_{i}" for i in range(16)])
+        urls.extend([f"{base_url}/en_legislation_train_{i}" for i in range(5)])
+        urls.extend([f"{base_url}/en_legislation_train_{i}" for i in range(15)])
+        return urls
+
     data_files = config.data_files
     if data_files is None:
-        raise ValueError("Data files are not available for this dataset")
-
-    urls = []
-
-    for split, files in data_files.items():
-        for file in files:
-            resolved_path = fs.resolve_path(file)
-            url = hf_hub_url(
-                resolved_path.repo_id,
-                resolved_path.path_in_repo,
-                repo_type=resolved_path.repo_type,
-                revision=resolved_path.revision,
-            )
-            urls.append(url)
+        if hasattr(config, "data_url"):
+            return [config.data_url]
+        else:
+            raise ValueError("There is not data available for this dataset (data_files and data_url are None).")
+    else:
+        urls = []
+        for split, files in data_files.items():
+            for file in files:
+                resolved_path = fs.resolve_path(file)
+                url = hf_hub_url(
+                    resolved_path.repo_id,
+                    resolved_path.path_in_repo,
+                    repo_type=resolved_path.repo_type,
+                    revision=resolved_path.revision,
+                )
+                urls.append(url)
     return urls
+
 
 def save_urls_to_tsv(urls, file_path):
     with open(file_path, 'w') as f:
@@ -50,6 +60,7 @@ def save_urls_to_tsv(urls, file_path):
         for url in urls:
             f.write(f"{url}\n")
     print(f"URLs have been saved to {file_path}")
+
 
 def upload_file_to_gcs(source_file, destination_path):
     storage_client = storage.Client()
@@ -63,10 +74,10 @@ def upload_file_to_gcs(source_file, destination_path):
 
     return f"{bucket_name}/{destination_blob_name}"
 
+
 def create_transfer_job(dataset_name, project_id, source_file, gcs_dest_path):
     client = storage_transfer_v1.StorageTransferServiceClient()
     gcs_bucket_name, gcs_dest_path_in_bucket = _split_gcs_path(gcs_dest_path)
-
 
     transfer_job = storage_transfer_v1.TransferJob(
         description=f"Transfer job for Hugging Face dataset {dataset_name}",
@@ -101,8 +112,6 @@ def create_transfer_job(dataset_name, project_id, source_file, gcs_dest_path):
     print(f"https://console.cloud.google.com/transfer/jobs/{escaped_name}/monitoring?hl=en&project={project_id}")
 
 
-
-
 def _split_gcs_path(path):
     if path.startswith('gs://'):
         gcs_bucket_name = path.split('/')[2]
@@ -122,20 +131,25 @@ def get_gcloud_project():
     if project_id:
         return project_id
     else:
-        raise EnvironmentError("Google Cloud project ID is not set. Use 'gcloud config set project PROJECT_ID' to set it.")
+        raise EnvironmentError(
+            "Google Cloud project ID is not set. Use 'gcloud config set project PROJECT_ID' to set it.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Transfer a Hugging Face dataset to Google Cloud Storage.")
     parser.add_argument('--dataset_name', required=True, help="The name of the Hugging Face dataset.")
+    parser.add_argument('--config', required=False, help="The Hugging Face dataset config to use.", default="default")
+    parser.add_argument('--revision', required=False, help="The Hugging Face dataset revision to use.", default="main")
     parser.add_argument('--destination_path', required=True, help="The name of the destination in a GCS bucket.")
-    parser.add_argument('--urls_dir', required=True, help="The GCS path to save the URLs file. Should be an HTTP accessible location.")
+    parser.add_argument('--urls_dir', required=True,
+                        help="The GCS path to save the URLs file. Should be an HTTP accessible location.")
     args = parser.parse_args()
 
     # Automatically determine the project ID
     project_id = get_gcloud_project()
 
     # Step 1: Get URLs from the Hugging Face dataset
-    urls = get_urls_to_copy(args.dataset_name)
+    urls = get_urls_to_copy(args.dataset_name, name=args.config, revision=args.revision)
 
     url_tsv_name = f'{args.dataset_name.replace("/", "-")}.tsv'
 
