97bea78e5da68994a719223e2b691c13f1f3f5b9
Christopher Chou <emu4yolife@gmail.com>
2024-06-20 23:00:32 +0000

    Move classifier class out of inference file

--- marin/processing/fasttext/download_dolma_classifier.py
+++ marin/processing/fasttext/download_dolma_classifier.py
@@ -7,20 +7,21 @@ import os
 from huggingface_hub import hf_hub_download
 
 # Define the repository and file details
-repo_id = "allenai/dolma-1_7-fasttext-quality-filter"
-filename = "model.bin"
-local_filename = "model.bin"
-local_dir = os.path.expanduser("~/dolma_fasttext_model")
+REPO_ID = "allenai/dolma-1_7-fasttext-quality-filter"
+FILENAME = "model.bin"
 
-def download_file():
+def download_file(output_dir=os.path.expanduser("~/dolma_fasttext_model")):
+    destination_path = os.path.join(output_dir, FILENAME)
+
+    # if os.path.exists(destination_path):
+    #     return destination_path
+
     # Create the local folder if it doesn't exist
-    os.makedirs(local_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
 
     # Download the file
-    downloaded_file_path = hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir, local_dir_use_symlinks=False)
+    downloaded_file_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir=output_dir)
 
-    # Move the downloaded file to the desired location
-    destination_path = os.path.join(local_dir, local_filename)
     print(f"File downloaded to: {destination_path}")
     
     return destination_path
--- marin/processing/fasttext/model/inference.py
+++ marin/processing/fasttext/model/inference.py
@@ -1,90 +1,50 @@
 """
 Usage:
-ray job submit --working-dir . --no-wait -- python marin/processing/fasttext/model/inference.py
+ray job submit --working-dir . --no-wait -- \
+python -m marin.processing.fasttext.model.inference --input_dir gs://marin-data/processed/fineweb/fw-v1.0/CC-MAIN-2023-50/000_00000/  --output_dir gs://marin-data/scratch/chrisc/quality/fineweb/fw-v1.0/CC-MAIN-2024-10/000_00000/
 """
+import argparse
 import datetime
 import json
 import os
 
-# import fasttext
 import fsspec
 import ray
 
-from marin.core.runtime import cached_or_construct_output
-from marin.processing.fasttext.download_dolma_classifier import download_file
+from marin.core.runtime import cached_or_construct_output, map_files_in_directory
+from marin.processing.fasttext.model.classifier import FasttextQualityClassifier
 
-MODEL_PATH = os.path.expanduser("~/dolma_fasttext_model/model.bin")
-FILENAMES = [
-    "gs://marin-data/processed/fineweb/fw-v1.0/CC-MAIN-2023-50/000_00000/0_processed_md.jsonl.gz"
-]
+def is_json_serializable(value):
+    try:
+        json.dumps(value)
+        return True
+    except (TypeError, OverflowError):
+        return False
 
-class BaseQualityClassifier:
-    def __init__(self, model):
-        self.model = model
-    
-    def predict(self, document):
-        raise NotImplementedError
-
-    def __call__(self, batch):
-        raise NotImplementedError
-
-class DummyQualityClassiifer(BaseQualityClassifier):
-    def __init__(self, model):
-        self.model = model
-
-    def predict(self, document):
-        label, score = "__label__test", 1.0
-        return label, score
-    
-    def __call__(self, row):
-        label, score = self.predict(row["text"])
-        row.update({
-            "label": label,
-            "score": score,
-        })
-        return row
-
-
-class FasttextQualityClassifier(BaseQualityClassifier):
-    def __init__(self, model):
-        import fasttext
-
-        model_path = download_file()
-        self.model = fasttext.load_model(model_path)
-
-    def predict(self, document):
-        return self.model.predict(document)
-
-    def __call__(self, row):
-        text = row["text"].replace("\n", " ")
-        label, score_arr = self.predict(text)
-        row.update({
-            "label": label,
-            "score": score_arr[0],
-        })
-
-        return row
-
+def make_serializable(obj):
+    if isinstance(obj, dict):
+        return {key: make_serializable(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [make_serializable(element) for element in obj]
+    elif isinstance(obj, tuple):
+        return tuple(make_serializable(element) for element in obj)
+    elif not is_json_serializable(obj):
+        return str(obj)
+    return obj
 
 @ray.remote(memory= 5 * 1024 * 1024 * 1024, runtime_env={"pip": ["fasttext"]})
-# @cached_or_construct_output(success_suffix="SUCCESS")
+@cached_or_construct_output(success_suffix="SUCCESS")
 def process_file(input_filename, output_filename):
-    # Load fasttext classifier in the global object store
-    # model = "DUMMY RAY MODEL"
-    # model = fasttext.load_model(MODEL_PATH)
-    # model_ref = ray.put(model)
-
     ds = ray.data.read_json(input_filename)
 
-    print(f"[*] Reading in dataset {input_filename}")
-
-    print("[*] Beginning quality classification")
+    print(f"[*] Read in dataset {input_filename}")
     
     # NOTE(chris): Could we parallelize using map_batches?
+    # There are sometimes some issues with initialization when concurrency increases
     results = ds.map(
         FasttextQualityClassifier,
-        num_gpus=0,
-        concurrency=(1,16),
+        # concurrency=(1,16),
+        concurrency=1,
         fn_constructor_args=("",)
     )
 
@@ -92,31 +52,36 @@ def process_file(input_filename, output_filename):
     
     with fsspec.open(input_filename, "rt", compression="gzip") as f_in:
         with fsspec.open(output_filename, "wt", compression="gzip") as f_out:
-            for row in results.iter_rows():
-                
-                # TODO(chris): support more documents, not just fineweb
-                if "fineweb_metadata" in row['metadata'] and "date" in row['metadata']["fineweb_metadata"]:
-                    row['metadata']['fineweb_metadata']["date"] = row['metadata']['fineweb_metadata']['date'].strftime('%m/%d/%Y')
-
-                json_row = json.dumps(row)
+            for row in results.iter_rows():                
+                json_row = json.dumps(make_serializable(row))
                 f_out.write(json_row + "\n")
 
-def main():
+def main(input_dir, output_dir):
     ray.init()
 
-    # model = fasttext.load_model(MODEL_PATH)
-    # model_path = download_file()
-
-    # # Load fasttext classifier in the global object store
-    # # model = "DUMMY RAY MODEL"
+    # TODO(chris): At some point we should be able to place the fasttext object in the global object store. Currently seems not serializable
+    # Load fasttext classifier in the global object store
     # model = fasttext.load_model(MODEL_PATH)
     # model_ref = ray.put(model)
 
-    output_filename = "gs://marin-data/scratch/chrisc/0_processed_md.jsonl.gz"
-    for filename in FILENAMES:
-        ray.get(process_file.remote(filename, output_filename))
+    # output_filename = "gs://marin-data/scratch/chrisc/0_processed_md.jsonl.gz"
+    # for filename in FILENAMES:
+    #     ray.get(process_file.remote(filename, output_filename))
+
+    responses = map_files_in_directory(process_file.remote, input_dir, "**/*_md.jsonl.gz", output_dir)
+
+    try:
+        ray.get(responses)
+    except Exception as e:
+        print(f"Error processing: {e}")
 
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Example script to convert fineweb html to markdown.")
+    parser.add_argument('--input_dir', type=str, help='Path to the unprocessed data directory', required=True)
+    parser.add_argument('--output_dir', type=str, help='Path to store data updated with quality scores directory', required=True)
+
+    args = parser.parse_args()
+
+    main(input_dir=args.input_dir, output_dir=args.output_dir)
