2096ba20ba58fb3b1ca2aa6985db10e8813f54f0
Christopher Chou <emu4yolife@gmail.com>
2024-06-24 02:13:33 +0000

    Don't use the Ray Data

--- marin/processing/download_gcs_data.py
+++ marin/processing/download_gcs_data.py
@@ -16,16 +16,16 @@ filenames = [
                 # "gs://marin-data/raw/dolma/dolma-v1.7/wiki-0001.json.gz",
                 # "gs://marin-data/raw/dolma/dolma-v1.7/c4-0001.json.gz",
                 # "gs://marin-data/scratch/chrisc/test.json.gz",
-                "gs://marin-data/scratch/chrisc/dataset.txt.gz",
-                "gs://marin-data/scratch/chrisc/fasttext_train.txt.gz",
-                "gs://marin-data/scratch/chrisc/fasttext_test.txt.gz",
-                "gs://marin-data/scratch/chrisc/0_processed_md.jsonl.gz"
+                # "gs://marin-data/scratch/chrisc/dataset.txt.gz",
+                # "gs://marin-data/scratch/chrisc/fasttext_train.txt.gz",
+                # "gs://marin-data/scratch/chrisc/fasttext_test.txt.gz",
+                "gs://marin-data/scratch/chrisc/processed/fineweb/fw-v1.0/CC-MAIN-2024-10/attributes/fasttext-quality/000_00000/0_processed_md.jsonl.gz"
             ]
 
 def download_file(filename, output_dir):
     output_filename = os.path.basename(filename).replace(".gz", "")
     output_file_path = os.path.join(output_dir, output_filename)
-    file_format = filename.split(".")[1]
+    file_format = os.path.basename(output_filename).split(".")[1]
 
     with fsspec.open(filename, 'r', compression="gzip") as f:
         with open(output_file_path, "w", encoding="utf-8") as f_out:
