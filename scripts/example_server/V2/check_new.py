3d3d759ac92cb84f91ccd0ce64a76e3050b1383d
Abhinav Garg <abhinavg@stanford.edu>
2024-07-07 04:16:02 -0400

    Example server V2

---
+++ scripts/example_server/V2/check_new.py
@@ -0,0 +1,111 @@
+import json
+import os
+import random
+
+import fsspec
+
+from marin.utils import rebase_file_path
+
+
+def check_and_sample(domain, version):
+    fs = fsspec.filesystem("gcs")
+    base_path = f"gcs://marin-data/processed/{domain}/{version}/"
+    examples_path = f"gcs://marin-data/examples/{domain}/{version}/"
+
+    # Get list of formats (directories) that start with XML, HTML, md, or txt
+    formats = fs.ls(base_path, use_listings_cache=False)
+    valid_formats = [fmt for fmt in formats if
+                     os.path.basename(fmt).lower().startswith(('xml', 'html', 'md', 'txt', "text"))]
+
+    # Collect md5Hash for all files in each valid format
+    all_md5_hashes = {}
+    for format_type in valid_formats:
+        files = fs.glob(os.path.join(format_type, "*.jsonl.gz"), detail=True, use_listings_cache=False)
+        all_md5_hashes[format_type] = {file: info['md5Hash'] for file, info in files.items()}
+
+    # Load previous md5 hashes if available
+    md5_hashes_path = os.path.join(examples_path, 'md5_hashes.json')
+    if fs.exists(md5_hashes_path):
+        with fsspec.open(md5_hashes_path, 'r', use_listings_cache=False) as f:
+            previous_md5_hashes = json.load(f)
+    else:
+        previous_md5_hashes = {}
+
+    # Check if any hash has changed
+    reprocess = False
+    for format_type, hashes in all_md5_hashes.items():
+        if format_type not in previous_md5_hashes or previous_md5_hashes[format_type] != hashes:
+            reprocess = True
+            break
+
+    if not reprocess:
+        print("No changes detected, skipping reprocessing.")
+        # return "No changes detected, skipping reprocessing."
+
+    # Perform reservoir sampling for all formats at once
+    sample_size = 1000
+    sample_lines = {fmt: [''] * sample_size for fmt in valid_formats}
+    sample_ids = []
+
+    # Get file paths for the first format
+    primary_format = valid_formats[0]
+    primary_files = fs.glob(os.path.join(primary_format, "*.jsonl.gz"), use_listings_cache=False)
+
+    # Randomly shuffle the files
+    random.shuffle(primary_files)
+    # import pdb; pdb.set_trace()
+
+    idx = 0
+    print("Processing files...")
+    # Iterate over the primary files and corresponding files in other formats
+    for num_file, primary_file in enumerate(primary_files):
+        if num_file > 0:
+            break
+
+        files_in_diff_fmts = []
+        for fmt in valid_formats:
+            corresponding_file = rebase_file_path(primary_format, primary_file, fmt)
+            files_in_diff_fmts.append(corresponding_file)
+
+        file_pointers = [fs.open(file, 'rt', compression="infer", use_listings_cache=False) for file in
+                         files_in_diff_fmts]
+
+        for i, lines in enumerate(zip(*file_pointers)):
+            # Do reservoir sampling
+            if idx < sample_size:
+                for j, line in enumerate(lines):
+                    sample_lines[valid_formats[j]][idx] = line
+            else:
+                j = random.randint(0, idx)
+                if j < sample_size:
+                    for k, line in enumerate(lines):
+                        sample_lines[valid_formats[k]][j] = line
+
+            idx += 1
+            if i > sample_size * 20:
+                break
+            if idx % 10000 == 0:
+                print(f"Processed {idx} lines")
+
+    # Save sampled lines to examples directory
+    # first get the path from format type and then save
+    return_text = ""
+    if idx == 0:
+        print("No samples were found or processed. Exiting.")
+        return "No samples were processed. Exiting."
+    elif idx < sample_size:
+        print(f"Only {idx} samples were processed.")
+        return_text = f"Only {idx} samples were processed."
+
+    for format_type, lines in sample_lines.items():
+        example_file_path = os.path.join(examples_path, os.path.basename(format_type), "samples.jsonl")
+        with fsspec.open(example_file_path, 'wt', use_listings_cache=False) as example_file:
+            for line in lines:
+                example_file.write(line)
+
+    # Save the current md5 hashes
+    with fsspec.open(md5_hashes_path, 'w', use_listings_cache=False) as f:
+        json.dump(all_md5_hashes, f)
+
+    print("New samples have been processed and saved.")
+    return f"New samples have been processed and saved, {return_text}"
---
+++ scripts/example_server/V2/server.py
@@ -0,0 +1,174 @@
+import json
+import os
+import traceback
+
+import fsspec
+import grip
+from flask import Flask, render_template_string
+from markupsafe import escape
+
+app = Flask(__name__)
+githubRenderer = grip.GitHubRenderer()
+
+
+# Function to render content based on type
+def render_content(content, format_type):
+    if format_type == 'text' or format_type == 'txt':
+        return '<p>{}</p>'.format(escape(content))
+    elif format_type == 'html' or format_type == 'xml':
+        iframe_content = f'<iframe srcdoc="{escape(content)}" style="width:100%; height:500px; border:none;"></iframe>'
+        return iframe_content
+    elif format_type == 'md':
+        return githubRenderer.render(content)
+    else:
+        return '<p>Unsupported content type, Rending as plain text <br> {}</p>'.format(escape(content))
+
+
+# Route to display the home page
+@app.route('/', methods=['GET'])
+def display_home():
+    fs = fsspec.filesystem("gcs")
+    files = fs.ls("gcs://marin-data/examples/", use_listings_cache=False)
+    files = [file.split('/')[-1][:-6] for file in files]
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+    <h1>Home</h1>"""
+    for file in files:
+        # add links to files if they exist
+        if file:
+            html_content += f'''<li><a href="/content/{file}/0">{file}</a><br>'''
+    html_content += '''
+    </body>
+    </html>
+    '''
+    return render_template_string(html_content)
+
+
+# Route to display the content
+@app.route('/content/<path:domain>/<path:version>/<int:idx>', methods=['GET'])
+def display_content(domain, version, idx):
+    try:
+        content_data = {}
+        fs = fsspec.filesystem("gcs")
+        base_path = f"gcs://marin-data/examples/{domain}/{version}/"
+        formats = fs.ls(base_path, use_listings_cache=False)
+        formats = [fmt for fmt in formats if not os.path.basename(fmt).lower().endswith('.json')]
+        formats = [os.path.basename(fmt) for fmt in formats if
+                   os.path.basename(fmt).lower().startswith(('xml', 'html', 'md', 'txt', "text"))]
+        # formats = [fmt.split('/')[-2] for fmt in formats]
+
+        for fmt in formats:
+            filename = f"gcs://marin-data/examples/{domain}/{version}/{fmt}/samples.jsonl"
+            i = 0
+            with fsspec.open(filename, 'rt', use_listings_cache=False) as file:
+                for line in file:
+                    if i == idx:
+                        record = json.loads(line.strip())
+                        content_data[fmt] = record['text']
+                        break
+                    i += 1
+                else:
+                    content_data[fmt] = None
+
+        rendered_content = []
+        for fmt in formats:
+            if content_data[fmt]:
+                rendered_content.append({
+                    'title': f"{fmt.upper()}",
+                    'rendered': render_content(content_data[fmt], fmt)
+                })
+                if fmt == "md":
+                    html_content = ('<iframe srcdoc="{}" style="width:100%; height:500px; border:none;"></iframe>'.
+                                    format(escape(content_data[fmt]).replace('\n', '<br>')))
+                    rendered_content.append({
+                        'title': f"raw {fmt.upper()}",
+                        'rendered': render_content(html_content, "html")
+                    })
+
+        html_content = '''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Example in {}/{}</title>
+            <style>
+                .dropdown-header {{
+                    background-color: #f2f2f2;
+                    padding: 10px;
+                    margin: 0;
+                    cursor: pointer;
+                }}
+                .dropdown-content {{
+                    display: none;
+                    padding: 10px;
+                }}
+                .dropdown-content.show {{
+                    display: block;
+                }}
+            </style>
+            <script>
+                function toggleContent(id) {{
+                    var content = document.getElementById(id);
+                    content.classList.toggle("show");
+                }}
+            </script>
+        </head>
+        <body>
+            <h1>Example in {}/{}</h1>
+            <p> Each title is a dropdown, you can click on it</p>
+        '''.format(domain, version, domain, version)
+
+        for i, item in enumerate(rendered_content):
+            html_content += '''
+            <div class="dropdown">
+                <h2 class="dropdown-header" onclick="toggleContent('content-{}')">{}</h2>
+                <div id="content-{}" class="dropdown-content">{}</div>
+            </div>
+            '''.format(i, escape(item['title']), i, item['rendered'])
+
+        if int(idx) > 0:
+            html_content += f'<a href="/content/{domain}/{version}/0">First</a>'
+            html_content += ' | '
+            html_content += f'<a href="/content/{domain}/{version}/{int(idx) - 1}">Previous</a>'
+            html_content += ' | '
+        html_content += f'<a href="/content/{domain}/{version}/{int(idx) + 1}">Next</a>'
+
+        html_content += '''
+        </body>
+        </html>
+        '''
+
+        return render_template_string(html_content)
+    except Exception as e:
+        stack = traceback.format_exc().replace("\n", "<br>")
+        error_msg = f'''Server internal Error<br>Error: {e}<br>Stack Trace: {stack}'''
+        return error_msg, 500
+
+
+# Route to check for new/modified files and perform sampling
+@app.route('/check-new/<path:domain>/<path:version>', methods=['GET'])
+def check_new(domain, version):
+    # make sure version is basepath
+    if os.path.basename(version) != version:
+        return "Invalid version or domain, they cannot be a path", 400
+    try:
+        from check_new import check_and_sample
+        return check_and_sample(domain, version)
+    except Exception as e:
+        stack = traceback.format_exc().replace("\n", "<br>")
+        error_msg = f'''Server internal Error<br>Error: {e}<br>Stack Trace: {stack}'''
+        return error_msg, 500
+
+
+if __name__ == '__main__':
+    # Check if the deployment variable is set (to run on the server)
+    if 'DEPLOYMENT' in os.environ:
+        app.run(host='0.0.0.0', port=80)
+    else:
+        app.run(debug=True)
