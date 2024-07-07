44040d770bbb943506b1e6334e083a47d27c00ff
Christopher Chou <emu4yolife@gmail.com>
2024-07-07 06:13:22 +0000

    Make prettier

--- marin/processing/fasttext/model/classifier.py
+++ marin/processing/fasttext/model/classifier.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 
+import ray
 from google.cloud import storage
 
 from marin.processing.fasttext.download_dolma_classifier import download_file
@@ -32,62 +33,78 @@ class DummyQualityClassifier(BaseQualityClassifier):
         return row
 
 class FasttextQualityClassifier(BaseQualityClassifier):
-    GCS_BUCKET_NAME = "marin-data"
-    GCS_BLOB_NAME = "scratch/chrisc/dolma_fasttext_model/model.bin"
-    HF_REPO_ID = "allenai/dolma-1_7-fasttext-quality-filter"
-    HF_FILENAME = "model.bin"
+    LOCAL_FILEPATH = os.path.expanduser("~/dolma_fasttext_model/model.bin")
 
     def __init__(self, model_ref):
-        # self.model = self.download_dolma_classifier(model_path)    
         self.model = self.load_model(model_ref)
 
     def load_model(self, model_ref):
         from fasttext.FastText import _FastText
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.bin') as temp_file:
-            temp_filename = temp_file.name
-            
-            # Write the model content to the temporary file
-            temp_file.write(model_ref.getvalue())
         try:
-            model = _FastText(temp_filename)
-        finally:
-            os.unlink(temp_filename)
+            if not os.path.exists(self.LOCAL_FILEPATH):
+                directory, basename = os.path.dirname(self.LOCAL_FILEPATH), os.path.basename(self.LOCAL_FILEPATH)
+                os.makedirs(directory, exist_ok=True)
 
-        return model
+                with open(self.LOCAL_FILEPATH, "wb") as f:
+                    f.write(model_ref.getvalue())
 
-    def download_dolma_classifier(self, model_path):
-        from huggingface_hub import hf_hub_download
-        from marin.processing.utils import download_huggingface_file_with_backoff, download_gcs_file_with_backoff
-
-        print(f"Trying to download classifier to {model_path}", flush=True)
-
-        directory, basename = os.path.dirname(model_path), os.path.basename(model_path)
-        os.makedirs(directory, exist_ok=True)
-        
-        # NOTE(chris): 1 try theoretically should work but there are sometimes some SystemExceptions from either FastText loading or Huggingface about metadata
-        try:
-            try:
-                print("Download using huggingface start.")
-                download_huggingface_file_with_backoff(self.HF_REPO_ID, self.HF_FILENAME, directory)
-                print("Downloaded from huggingface.")
-            except Exception as e:
-                print(e, flush=True)
-                
-                try:
-                    print("Download using GCS start.")
-                    download_gcs_file_with_backoff(self.GCS_BUCKET_NAME, self.GCS_BLOB_NAME, model_path)
-                    print("Download from GCS.")
-                except Exception as e:
-                    print(e, flush=True)
-            
-            print(f"Model size: {os.path.getsize(model_path) / 1024 / 1024 / 1024} GB")
-            model = _FastText(model_path)
+            print(f"Size of model: {os.path.getsize(self.LOCAL_FILEPATH)}")
+            model = _FastText(self.LOCAL_FILEPATH)
         except Exception as e:
-            raise e
-
+            print(e)
+            print("failed to load model")
+            
         return model
 
+    def predict(self, document):
+        return self.model.predict(document)
+
+    def __call__(self, row):
+        if row["text"]:
+            text = row["text"].replace("\n", " ")
+        else:
+            text = ""
+
+        label_arr, score_arr = self.predict(text)
+        fasttext_quality_dict = {}
+        for label, score in zip(label_arr, score_arr):
+            fasttext_quality_dict.update({
+                label: score
+            })
+
+        row.update({
+            "attributes" : {
+                "fasttext-quality" : fasttext_quality_dict
+            }
+        })
+
+        return row
+
+class BatchFasttextQualityClassifier(BaseQualityClassifier):
+    LOCAL_FILEPATH = os.path.expanduser("~/dolma_fasttext_model/model_quantized.bin")
+
+    def __init__(self, model_ref):
+        self.model = self.load_model(model_ref)
+
+    def load_model(self, model_ref):
+        from fasttext.FastText import _FastText
+
+        try:
+            if not os.path.exists(self.LOCAL_FILEPATH):
+                directory, basename = os.path.dirname(self.LOCAL_FILEPATH), os.path.basename(self.LOCAL_FILEPATH)
+                os.makedirs(directory, exist_ok=True)
+
+                with open(self.LOCAL_FILEPATH, "wb") as f:
+                    f.write(model_ref.getvalue())
+
+            print(f"Size of model: {os.path.getsize(self.LOCAL_FILEPATH)}")
+            model = _FastText(self.LOCAL_FILEPATH)
+        except Exception as e:
+            print(e)
+            print("failed to load model")
+
+        return model
 
     def predict(self, documents):
         return self.model.predict(documents)
--- marin/processing/fasttext/model/inference.py
+++ marin/processing/fasttext/model/inference.py
@@ -1,7 +1,7 @@
 """
 Usage:
 ray job submit --working-dir . --no-wait -- \
-python -m marin.processing.fasttext.model.inference --input_dir gs://marin-data/processed/fineweb/fw-v1.0/CC-MAIN-2023-50/  --output_dir gs://marin-data/scratch/chrisc/test-attributes/fasttext-quality/fineweb/fw-v1.0/CC-MAIN-2023-50/
+python -m marin.processing.fasttext.model.inference --input_dir gs://marin-data/processed/fineweb/fw-v1.0/md/CC-MAIN-2023-50/  --output_dir gs://marin-data/attributes/fasttext-quality/fineweb/fw-v1.0/md/CC-MAIN-2024-10/
 """
 import argparse
 import datetime
@@ -13,13 +13,15 @@ import fsspec
 import ray
 
 from marin.core.runtime import cached_or_construct_output, map_files_in_directory
-from marin.processing.fasttext.model.classifier import FasttextQualityClassifier, DummyQualityClassifier
+from marin.processing.fasttext.model.classifier import BatchFasttextQualityClassifier, DummyQualityClassifier, FasttextQualityClassifier
 from marin.processing.utils import download_huggingface_file_with_backoff, download_gcs_file_with_backoff
 
-GCS_BUCKET_NAME = "marin-data"
-GCS_BLOB_NAME = "scratch/chrisc/dolma_fasttext_model/model.bin"
-HF_REPO_ID = "allenai/dolma-1_7-fasttext-quality-filter"
-HF_FILENAME = "model.bin"
+class StorageConfig:
+    def __init__(self, gcs_bucket_name, gcs_blob_name, hf_repo_id, hf_filename):
+        self.gcs_bucket_name = gcs_bucket_name
+        self.gcs_blob_name = gcs_blob_name
+        self.hf_repo_id = hf_repo_id
+        self.hf_filename = hf_filename
 
 def is_json_serializable(value):
     try:
@@ -41,17 +43,17 @@ def make_serializable(obj):
 
 
 # TODO(chris): fix this code, perhaps having fasttext dependency on head node can make it easier to load model ref to workers
-@ray.remote(memory= 15 * 1024 * 1024 * 1024, runtime_env={"pip": ["fasttext", "tenacity"]})
+@ray.remote(memory= 16 * 1024 * 1024 * 1024, runtime_env={"pip": ["fasttext"]})
 @cached_or_construct_output(success_suffix="SUCCESS")
 def process_file_using_actor_pool(input_filename, output_filename, model_ref):
-    ds = ray.data.read_json(input_filename)
+    ds = ray.data.read_json(input_filename, arrow_open_stream_args={"compression": "gzip"},)
 
     print(f"[*] Read in dataset {input_filename}")
     
     # FIXME(chris): Could we parallelize using map_batches?
     # There are sometimes some issues with initialization when concurrency increases
     results = ds.map_batches(
-        FasttextQualityClassifier,
+        BatchFasttextQualityClassifier,
         # concurrency=(1,16),
         concurrency=1,
         fn_constructor_args=(model_ref,),
@@ -60,13 +62,13 @@ def process_file_using_actor_pool(input_filename, output_filename, model_ref):
 
     print("[*] Finished quality classification")
     
-    with fsspec.open(input_filename, "rt", compression="gzip") as f_in:
-        with fsspec.open(output_filename, "wt", compression="gzip") as f_out:
-            for row in results.iter_rows():                
-                json_row = json.dumps(make_serializable(row))
-                f_out.write(json_row + "\n")
+    # with fsspec.open(input_filename, "rt", compression="gzip") as f_in:
+    with fsspec.open(output_filename, "wt", compression="gzip") as f_out:
+        for row in results.iter_rows():                
+            json_row = json.dumps(make_serializable(row))
+            f_out.write(json_row + "\n")
 
-@ray.remote(memory= 5 * 1024 * 1024 * 1024, runtime_env={"pip": ["fasttext"]})
+@ray.remote(memory= 16 * 1024 * 1024 * 1024, runtime_env={"pip": ["fasttext"]})
 @cached_or_construct_output(success_suffix="SUCCESS")
 def process_file(input_filename, output_filename, model_ref):
     print(f"[*] Read in dataset {input_filename}")
@@ -87,11 +89,8 @@ def process_file(input_filename, output_filename, model_ref):
                 f_out.write(json_row + "\n")
 
 
-def main(input_dir, output_dir, model_path):
+def main(input_dir: str, output_dir: str, model_path: str, storage_config: StorageConfig):
     ray.init()
-
-    # responses = map_files_in_directory(process_file.remote, input_dir, "**/*_md.jsonl.gz", output_dir, None, model_path)
-    # FIXME(chris): only one file rn
 
     byte_buffer = io.BytesIO()
     directory, basename = os.path.dirname(model_path), os.path.basename(model_path)
@@ -101,14 +100,14 @@ def main(input_dir, output_dir, model_path):
     try:
         try:
             print("Download using huggingface start.")
-            download_huggingface_file_with_backoff(HF_REPO_ID, HF_FILENAME, directory)
+            download_huggingface_file_with_backoff(storage_config.hf_repo_id, storage_config.hf_filename, directory)
             print("Downloaded from huggingface.")
         except Exception as e:
             print(e, flush=True)
             
             try:
                 print("Download using GCS start.")
-                download_gcs_file_with_backoff(GCS_BUCKET_NAME, GCS_BLOB_NAME, model_path)
+                download_gcs_file_with_backoff(storage_config.gcs_bucket_name, storage_config.gcs_blob_name, model_path)
                 print("Downloaded from GCS.")
             except Exception as e:
                 print(e, flush=True)
@@ -120,7 +119,10 @@ def main(input_dir, output_dir, model_path):
         raise e
 
     model_ref = ray.put(byte_buffer)
-    responses = map_files_in_directory(process_file_using_actor_pool.remote, input_dir, "**/*_md.jsonl.gz", output_dir, None, model_ref)
+    # responses = map_files_in_directory(process_file_using_actor_pool.remote, input_dir, "**/*_md.jsonl.gz", output_dir, None, model_ref)
+    # responses = map_files_in_directory(process_file_using_actor_pool.remote, input_dir, "**/0_*_md.jsonl.gz", output_dir, None, model_ref)
+    responses = map_files_in_directory(process_file.remote, input_dir, "**/*_md.jsonl.gz", output_dir, None, model_ref)
+
 
     try:
         ray.get(responses)
@@ -133,8 +135,30 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Example script to convert fineweb html to markdown.")
     parser.add_argument('--input_dir', type=str, help='Path to the unprocessed data directory', required=True)
     parser.add_argument('--output_dir', type=str, help='Path to store data updated with quality scores directory', required=True)
-    parser.add_argument('--model-path', type=str, default=os.path.expanduser("~/dolma_fasttext_model/model.bin"), help="Model path of fasttext classifier")
+    parser.add_argument('--model-path', type=str, default="", help="Model path of fasttext classifier")
+    parser.add_argument('--quantized', type=str)
 
     args = parser.parse_args()
 
-    main(input_dir=args.input_dir, output_dir=args.output_dir, model_path=args.model_path)
+    if args.model_path == "":
+        if args.quantized:
+            args.model_path = os.path.expanduser("~/dolma_fasttext_model/model_quantized.bin")
+        else:
+            args.model_path = os.path.expanduser("~/dolma_fasttext_model/model.bin")
+
+    if args.quantized:
+        storage_config = StorageConfig(
+            gcs_bucket_name="marin-data",
+            gcs_blob_name="scratch/chrisc/dolma_fasttext_model/model_quantized.bin",
+            hf_repo_id="BabyChou/dolma-fasttext-model-quantized",
+            hf_filename="model.bin"
+        )
+    else:
+        storage_config = StorageConfig(
+            gcs_bucket_name="marin-data",
+            gcs_blob_name="scratch/chrisc/dolma_fasttext_model/model.bin",
+            hf_repo_id="allenai/dolma-1_7-fasttext-quality-filter",
+            hf_filename="model.bin"
+        )
+
+    main(input_dir=args.input_dir, output_dir=args.output_dir, model_path=args.model_path, storage_config=storage_config)
