4cd9783fd80a80713d81392a4624ec0b5f0e48a9
Ahmed Ahmed <ahmedah@stanford.edu>
2024-07-10 16:25:50 -0700

    final pretty fixes

---
+++ marin/processing/instruct/utils.py
@@ -0,0 +1,34 @@
+
+from marin.web.convert import convert_page
+
+def process_instruction_tulu(source, id, message_dict):
+    """
+    Process the instruction for Tulu language.
+
+    Args:
+        source (str): The source of the instruction.
+        id (int): The ID of the instruction.
+        message_dict (dict): A dictionary containing the messages.
+
+    Returns:
+        tuple: A tuple containing the Markdown content and the plain text content.
+    """
+    title = f"Instruction Doc: {id} Source: {source}"
+    md_content = f"# {title} \n\n"
+    txt_content = f"{title}\n\n"
+
+    for message in message_dict['messages']:
+        role = message.get("role", "")
+        text = message.get("content", "")
+
+        out = convert_page(text, url="")
+        title = out.get("title", )
+        md = out["content"]
+
+        if not role:
+            raise ValueError("Role cannot be empty.")
+
+        
+        md_content += f" \n\n ## **{role}** \n {md}  \n\n"
+        txt_content += f"\n\n{role.upper()}\n{text}\n\n"
+    return md_content, txt_content
