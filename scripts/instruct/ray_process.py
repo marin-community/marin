ddcfcd02e87102638efa11161acfe50bbf8be088
Ahmed Ahmed <ahmedah@stanford.edu>
2024-07-10 16:25:50 -0700

    final pretty fixes

--- scripts/instruct/ray_process.py
+++ scripts/instruct/ray_process.py
@@ -17,12 +17,11 @@ import fsspec
 import os
 import ray
 import pandas as pd
-import sys
 
 from marin.core.runtime import RayConfig, TaskConfig, cached_or_construct_output, map_files_in_directory
-from marin.web.convert import convert_page
-import datetime
 from tqdm import tqdm
+from marin.processing.instruct.utils import process_instruction
+
 
 
 # This function will be executed on the worker nodes. It is important to keep the function idempotent and resumable.
@@ -31,60 +30,30 @@ from tqdm import tqdm
 # Ray will not impose any physical limits on the resources used by the function, these numbers are used for scheduling.
 @ray.remote(memory=1 * 1024 * 1024 * 1024, runtime_env={"pip": ["s3fs"]}, num_cpus=1)  # 1 GB
 @cached_or_construct_output(success_suffix="SUCCESS")
-def html_to_md(input_file_path, output_dir):
+def process_raw_chunk(input_file_path, output_dir):
     input_type = "parquet" if input_file_path.endswith(".parquet") else "jsonl"
     base_name = os.path.splitext(os.path.basename(input_file_path))[0]
     print(f"\n input type: {input_type}, input_file_path: {input_file_path}, output_dir: {output_dir}")
+    md_json_files = []
+    text_json_files = []
+
     if input_type == 'jsonl':
         with fsspec.open(input_file_path, "rt", compression="gzip") as f:
             for idx, line in enumerate(f):
                 data = json.loads(line)
+                id = data[0].get("dataset", f"{idx}")
                 try:
-                    md_content = ""
-                    html_content = ""
-
-                    for item in data:
-                        role = item.get("role", "")
-                        text = item.get("content", "")
-
-                        out = convert_page(text, url="")
-                        title = out["title"]
-                        md = out["content"]
-                        html = out["html"]
-
-                        if role:
-                            md = f"# {role} \n{md}"
-                            html = f"<h1>{role}</h1> \n{html}"
-
-                        md_content += md + "\n\n"
-                        html_content += html + "\n\n"
+                    text_content, md_content = process_instruction(source, id, data)
                 except Exception as e:
                     print(f"Error {e} in processing {idx = }, file: {input_file_path}")
                     continue
 
-
-                id = data[0].get("dataset", f"{idx}")
-                source = data[0].get("dataset", "")
                 metadata = data[0].get("metadata", {})
-
-                # Save the Markdown JSONL file
-                # TODO: investigate output dir bug
-                md_output_path = os.path.join(output_dir, version, "md", f"{base_name}.jsonl.gz")
-                print(f"MD path Saving to {md_output_path} with input file path {input_file_path}")
-                with fsspec.open(md_output_path, "at", compression="gzip") as md_file:
-                    md_file.write(json.dumps({"id": id, "text": md_content, "source": source, "metadata": metadata}) + "\n")
-
-                # Save the HTML JSONL file
-                html_output_path = os.path.join(output_dir, version, "html", f"{base_name}.jsonl.gz")
-                print(f"HTML path Saving to {html_output_path} with input file path {input_file_path}")
-                with fsspec.open(html_output_path, "at", compression="gzip") as html_file:
-                    html_file.write(json.dumps({"id": id, "text": html_content, "source": source, "metadata": metadata}) + "\n")
+                md_json_files.append({"id": id, "text": md_content, "source": source, "metadata": metadata})
+                text_json_files.append({"id": id, "text": text_content, "source": source, "metadata": metadata})
 
     elif input_type == 'parquet':
-
         df = pd.read_parquet(input_file_path)
-        md_json_files = []
-        html_json_files = []
         for idx, row in tqdm(enumerate(df.iterrows()), total=len(df)):
             if len(row) != 2:
                 raise ValueError("Row should have 2 elements, index and data")
@@ -93,25 +62,7 @@ def html_to_md(input_file_path, output_dir):
             id = data_dict.get("id", f"{data_idx}")
             source = data_dict.get("dataset", "")
             try:
-                title = f"Instruction Doc: {id} Source: {source}"
-                md_content = f"# {title} \n\n"
-                html_content = f"<h1> {title} </h1> \n\n"
-
-                for message in data_dict['messages']:
-                    role = message.get("role", "")
-                    text = message.get("content", "")
-
-                    out = convert_page(text, url="")
-                    title = out.get("title", )
-                    md = out["content"]
-                    html = out["html"]
-
-                    if not role:
-                        raise ValueError("Role cannot be empty.")
-
-                    
-                    md_content += f" \n\n ## **{role}** \n {md}  \n\n"
-                    html_content += f" \n\n <h2><strong> {role} </strong></h2> \n {html} \n\n"
+                html_content, md_content = process_instruction(source, id, data_dict['messages'])
             except Exception as e:
                 print(f"Error {e} in processing {idx = }, file: {input_file_path}")
                 continue
@@ -119,27 +70,27 @@ def html_to_md(input_file_path, output_dir):
             
             metadata = data_dict.get("metadata", {})
             md_json_files.append({"id": id, "text": md_content, "source": source, "metadata": metadata})
-            html_json_files.append({"id": id, "text": html_content, "source": source, "metadata": metadata})
+            text_json_files.append({"id": id, "text": html_content, "source": source, "metadata": metadata})
 
-        # Save the Markdown JSONL file
-        version = "v1_olmo_mix"
-        save_path = "/".join(output_dir.split("/")[:5])
+    # Save the Markdown JSONL file
+    version = "v1_olmo_mix"
+    save_path = "/".join(output_dir.split("/")[:5])
 
-        
-        md_output_path = os.path.join(save_path,  version, "md", f"{base_name}.jsonl.gz")
-        print(f"MD path Saving to {md_output_path} with input file path {input_file_path}")
-        print(f" Number of md files {len(md_json_files)}")
-        with fsspec.open(md_output_path, "at", compression="gzip") as md_file:
-            for md_json in md_json_files:
-                md_file.write(json.dumps(md_json) + "\n")
+    
+    md_output_path = os.path.join(save_path,  version, "md", f"{base_name}.jsonl.gz")
+    print(f"MD path Saving to {md_output_path} with input file path {input_file_path}")
+    print(f" Number of md files {len(md_json_files)}")
+    with fsspec.open(md_output_path, "at", compression="gzip") as md_file:
+        for md_json in md_json_files:
+            md_file.write(json.dumps(md_json) + "\n")
 
-        # Save the HTML JSONL file
-        html_output_path = os.path.join(save_path, version, "html", f"{base_name}.jsonl.gz")
-        print(f"HTML path Saving to {html_output_path} with input file path {input_file_path}")
-        print(f" Number of html files {len(html_json_files)}")
-        with fsspec.open(html_output_path, "at", compression="gzip") as html_file:
-            for html_json in html_json_files:
-                html_file.write(json.dumps(html_json) + "\n")
+    # Save the Text JSONL file
+    text_output_path = os.path.join(save_path, version, "text", f"{base_name}.jsonl.gz")
+    print(f"HTML path Saving to {text_output_path} with input file path {input_file_path}")
+    print(f" Number of html files {len(text_json_files)}")
+    with fsspec.open(text_output_path, "at", compression="gzip") as text_file:
+        for text_json in text_json_files:
+            text_file.write(json.dumps(text_json) + "\n")
 
     return True
 
@@ -164,7 +115,7 @@ def main(config: RayConfig):
 
 
     responses = map_files_in_directory(
-        html_to_md,
+        process_raw_chunk,
         config.input_dir,
         "**/*.jsonl.gz",
         config.output_dir,
@@ -176,7 +127,7 @@ def main(config: RayConfig):
         print(f"Error processing: {e}")
         # Put your retry logic here, incase you want to get the file for which the processing failed, please see:
         # https://docs.ray.io/en/latest/ray-core/fault-tolerance.html
-        # In practice, since we make html_to_md resumable and idempotent, you can just look at the logs in Ray dashboard
+        # In practice, since we make process_raw_chunk resumable and idempotent, you can just look at the logs in Ray dashboard
         # And retry the same command after fixing the bug.
 
 
@@ -198,12 +149,12 @@ if __name__ == '__main__':
 
     ray.init()
     if args.input_type == 'parquet':
-        responses = map_files_in_directory(html_to_md.remote, args.input_dir, "**/*.parquet", args.output_dir, args.input_type)
+        responses = map_files_in_directory(process_raw_chunk.remote, args.input_dir, "**/*.parquet", args.output_dir, args.input_type)
     else:
-        responses = map_files_in_directory(html_to_md.remote, args.input_dir, "**/*.jsonl.gz", args.output_dir, args.input_type)
+        responses = map_files_in_directory(process_raw_chunk.remote, args.input_dir, "**/*.jsonl.gz", args.output_dir, args.input_type)
 
     # Wait for all the tasks to finish.
-    # The try and catch is important here as incase html_to_md throws any exception, that exception is passed here,
+    # The try and catch is important here as incase process_raw_chunk throws any exception, that exception is passed here,
     # And if we don't catch it here, the script will exit, which will kill all the other tasks.
     try:
         ray.get(responses)
@@ -211,5 +162,5 @@ if __name__ == '__main__':
         print(f"Error processing: {e}")
         # Put your retry logic here, incase you want to get the file for which the processing failed, please see:
         # https://docs.ray.io/en/latest/ray-core/fault-tolerance.html
-        # In practice, since we make html_to_md resumable and idempotent, you can just look at the logs in Ray dashboard
+        # In practice, since we make process_raw_chunk resumable and idempotent, you can just look at the logs in Ray dashboard
         # And retry the same command after fixing the bug.
