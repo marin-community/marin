d3fcec36bd867f867802d015f9d8497c4e6f0e09
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2024-08-01 14:17:47 -0700

    json to jsonl

--- scripts/reddit/process_txt.py
+++ scripts/reddit/process_txt.py
@@ -7,10 +7,10 @@ import ray
 from marin.core.runtime import RayConfig, TaskConfig, cached_or_construct_output, map_files_in_directory
 from scripts.reddit.utils import convert_thread
 
-@ray.remote(memory=10 * 1024 * 1024 * 1024, runtime_env={"pip": ["s3fs"]}, num_cpus=1)  # 1 GB
+@ray.remote(memory=1 * 1024 * 1024 * 1024, runtime_env={"pip": ["s3fs"]}, num_cpus=1)  # 1 GB
 @cached_or_construct_output(success_suffix="SUCCESS") # We use this decorator to make this function idempotent
 def json_to_md(input_file_path, output_file_path):
-    # Read the input file
+    output_file_path = output_file_path.replace("json","jsonl")
     with fsspec.open(input_file_path, "rt", compression="gzip") as f, \
             fsspec.open(output_file_path, "wt", compression="gzip") as output:
         for line in f:
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Example script to convert fineweb html to markdown.")
     
     parser.add_argument('--input_dir', type=str, default='gs://marin-data/raw/dolma/dolma-v1.7/', help='Path to the dolma 1.7 directory', required=False)
-    parser.add_argument('--output_dir', type=str, default='gs://marin-data/scratch/rohithk/processed/reddit/v0/text/', help='Path to store reddit markdown files', required=False)
+    parser.add_argument('--output_dir', type=str, default='gs://marin-data/processed/reddit/v0/text/', help='Path to store reddit markdown files', required=False)
 
     args = parser.parse_args()
 
