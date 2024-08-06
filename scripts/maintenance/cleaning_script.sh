1d5ad3b3b886026f946e5ef166f83096ce886a82
Ivan Zhou <ivan.zhouyq@gmail.com>
2024-08-05 20:37:46 -0700

    Add a comprehensive cleaning script (#90)

---
+++ scripts/maintenance/cleaning_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# set cron job to run the following script daily
+
+# Print current date and time
+echo "Script started at: 
+
+# clean up cache directory
+rm -rf /home/shared/.cache
+
+# clean up old docker image
+bash /home/shared/maintenance/delete_old_docker_images.sh
+
+# Print the status of the disk
+df
+
+# clean up inactive TPU queued resources
+bash /home/shared/maintenance/delete_inactive_tpu_queued_resources.sh us-central2-b
+bash /home/shared/maintenance/delete_inactive_tpu_queued_resources.sh us-west4-a
+bash /home/shared/maintenance/delete_inactive_tpu_queued_resources.sh europe-west4-b
