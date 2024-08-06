0eff1ba9b96450c7f5902c7db2c933d36391b0d7
Jason Wang <blahblahj.wsy@gmail.com>
2024-08-05 23:40:04 -0700

    add docs for model size script

--- scripts/training/scaling_law/calc_model_size.py
+++ scripts/training/scaling_law/calc_model_size.py
@@ -17,16 +17,28 @@ def calculate_model_size(
     num_kv_heads: int = 8,
     **kwargs,
 ):
+    """Calculates number of parameters given a llama model config.
+    Calculations done with the following assumptions:
+    1. llama3 tokenizer (128256 vocab size)
+    2. swiglu activation (3 linear layers in MLP)
+    3. untied embedding and lm_head weights
+    4. rotary embeddings (ignore frequency buffers)
+    5. RMSNorm
+    """
+
     token_embedding = vocab_size * hidden_dim
 
     head_size = hidden_dim // num_heads
-    attn = hidden_dim * head_size * (2 * num_kv_heads + num_heads) + num_heads * head_size * hidden_dim
+    q_proj = hidden_dim * head_size * num_heads
+    kv_proj = 2 * hidden_dim * head_size * num_kv_heads
+    o_proj = head_size * num_heads * hidden_dim
+    attn = q_proj + kv_proj + o_proj
 
     mlp = 3 * hidden_dim * intermediate_dim
 
-    transformer_layer = attn + mlp + 2 * hidden_dim
+    transformer_layer = attn + mlp + 2 * hidden_dim # plus 2 rmsnorm
 
-    transformer = num_layers * transformer_layer + hidden_dim
+    transformer = num_layers * transformer_layer + hidden_dim # plus final rmsnorm
 
     lm_head = hidden_dim * vocab_size
 
@@ -41,6 +53,7 @@ if __name__ == "__main__":
 
     assert args.model_path.exists(), f"{args.model_path} does not exist."
     
+    print(f"Model config: {args.model_path}")
     with open(args.model_path, 'r') as f:
         data = yaml.safe_load(f)
 
