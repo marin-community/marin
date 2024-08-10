e0662f0369c9964094bc4bca5558fdf949425bc9
Abhinav Garg <abhinavg@stanford.edu>
2024-08-10 13:04:58 -0400

    Changes as per PR comments

--- scripts/example_server_browser/browser.py
+++ scripts/example_server_browser/browser.py
@@ -1,9 +1,9 @@
 import json
+import os
 import traceback
 
 import comrak_py
 import fsspec
-import os
 from flask import render_template_string, jsonify, Flask, request, url_for
 from markupsafe import escape
 
@@ -12,6 +12,17 @@ from marin.utils import get_gcs_path
 app = Flask(__name__)
 
 
+def get_format_type(format_type):
+    '''Given format_type from jsonl file, get rendering format'''
+    if format_type == 'md' or format_type == 'markdown':
+        return 'markdown'
+    elif format_type == 'html' or format_type == 'xml':
+        return 'html'
+    elif format_type == 'txt' or format_type == 'text':
+        return 'text'
+    else:  # We support the only three types above
+        raise ValueError(f'Unsupported format type: {format_type}')
+
 @app.route('/')
 def list_files():
     path = request.args.get('path', '')
@@ -19,23 +30,23 @@ def list_files():
     if path:
         files = fs.ls(f"gcs://{path}")
     else:
-        files = fs.ls("gcs://")
+        return jsonify({'error': 'Path parameter is required'}), 400
 
     html_content = """
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>GCP File Browser</title>
+        <title>Marin Data Browser</title>
     </head>
     <body>
-    <h1>GCP File Browser</h1>
+    <h1>Marin Data Browser</h1>
     <ul>
     """
 
     parent_path = os.path.dirname(path) if path else ''
     if parent_path:
-        html_content += f'<li><a href="{url_for("list_files", path=parent_path)}">.. (Back)</a></li>'
+        html_content += f'<li><a href="{url_for("list_files", path=parent_path)}">.. (Parent)</a></li>'
 
     for file in files:
         file_name = file.split('/')[-1]
@@ -54,22 +65,39 @@ def list_files():
 
 
 def render_content(content, format_type):
-    if format_type == 'text' or format_type == 'txt':
-        return '<p>{}</p>'.format(escape(content))
-    elif format_type == 'html' or format_type == 'xml':
-        iframe_content = f'<iframe srcdoc="{escape(content)}" style="width:100%; height:500px; border:none;"></iframe>'
-        return iframe_content
-    elif format_type == 'md':
-        return comrak_py.gfm_to_html(content)
-    else:
-        return '<p>Unsupported content type, Rendering as plain text <br> {}</p>'.format(escape(content))
+    '''Render content based on format type'''
 
+    content_render = ""
+    if format_type == 'text':
+        content_render = '<p style="white-space: pre-line;">{}</p>'.format(escape(content))
+    elif format_type == 'html':
+        content_render = f'<iframe srcdoc="{escape(content)}" style="width:100%; height:500px; border:none;"></iframe>'
+    elif format_type == 'markdown':
+        content_render = comrak_py.gfm_to_html(content)
+    else:
+        raise ValueError(f'Unsupported format type: {format_type}')
+
+    return content_render
+
+
+def render(path, record):
+    '''Render the title and content of the record'''
+
+    # If "format" is not present then we render it as text
+    rendered_content = []
+    format_type = get_format_type(record.get('format', 'text'))
+    rendered_content.append({'title': f"{path} {format_type}", 'rendered': render_content(record['text'], format_type)})
+
+    if format_type == 'markdown':  # We also render raw markdown
+        rendered_content.append({'title': f"{path} Raw Markdown", 'rendered': render_content(record['text'], 'text')})
+
+    return rendered_content
 
 # Route to display the content
 @app.route('/content', methods=['GET'])
 def display_content():
     paths = request.args.get('paths')
-    idx = int(request.args.get('index', 0))
+    index = int(request.args.get('index', 0))
 
     if not paths:
         return jsonify({'error': 'Paths parameter is required'}), 400
@@ -82,35 +110,24 @@ def display_content():
             filename = get_gcs_path(path)
             with fsspec.open(filename, 'rt', compression='gzip', use_listings_cache=False) as file:
                 for i, line in enumerate(file):
-                    if i == idx:
+                    if i == index:
                         record = json.loads(line.strip())
                         break
 
             if not record:
-                return jsonify({'error': f'Record not found in {path} at index {idx}'}), 404
+                return jsonify({'error': f'Record not found in {path} at index {index}'}), 404
 
             source_id = record['id']
 
-            if "format" not in record:
-                record["format"] = "unsupported text"
+            rendered_content += render(path, record)
 
-            if record['format'] == 'md':
-                html_content = ('<iframe srcdoc="{}" style="width:100%; height:500px; border:none;"></iframe>'.
-                                format(escape(record["text"]).replace('\n', '<br>')))
-                rendered_content.append({
-                    'title': f"{path},raw md",
-                    'rendered': render_content(html_content, "html")
-                })
-
-            rendered_content.append(
-                {'title': f"{path}, {record['format']}", 'rendered': render_content(record["text"], record["format"])})
 
         html_content = '''
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <title>Example with ID: {}, in {}</title>
+            <title>Line {} of {}</title>
             <style>
                 .dropdown-header {{
                     background-color: #f2f2f2;
@@ -146,12 +163,12 @@ def display_content():
             </div>
             '''.format(i, escape(item['title']), i, item['rendered'])
 
-        if int(idx) > 0:
+        if int(index) > 0:
             html_content += f'<a href="/content?paths={",".join(paths)}&index={0}">First</a>'
             html_content += ' | '
-            html_content += f'<a href="/content?paths={",".join(paths)}&index={int(idx) - 1}">Previous</a>'
+            html_content += f'<a href="/content?paths={",".join(paths)}&index={int(index) - 1}">Previous</a>'
             html_content += ' | '
-        html_content += f'<a href="/content?paths={",".join(paths)}&index={int(idx) + 1}">Next</a>'
+        html_content += f'<a href="/content?paths={",".join(paths)}&index={int(index) + 1}">Next</a>'
 
         html_content += '''
         </body>
