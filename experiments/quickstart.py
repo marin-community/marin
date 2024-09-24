a5b64a2cc7363226d4337fab71288cdaf92a141f
Percy Liang <percyliang@gmail.com>
2024-09-23 23:54:29 -0700

    black

--- experiments/quickstart.py
+++ experiments/quickstart.py
@@ -4,14 +4,19 @@ from marin.execution.executor import ExecutorStep, executor_main, get_input, get
 # Download the pretraining data
 from operations.download.huggingface.download import DownloadConfig, download
 
-raw_download_step = ExecutorStep(name="raw/hello_world_fw-pliang", fn=download, config=DownloadConfig(
-    hf_dataset_id="skaramcheti/hello_world_fw",
-    revision="8fd6e8e",
-    gcs_output_path=get_output(),
-    wait_for_completion=True,
-))
-raw_data = get_input(raw_download_step,
-    "8fd6e8e/huggingface.co/datasets/skaramcheti/hello_world_fw/resolve/8fd6e8e/data/CC-MAIN-2024-10")
+raw_download_step = ExecutorStep(
+    name="raw/hello_world_fw-pliang",
+    fn=download,
+    config=DownloadConfig(
+        hf_dataset_id="skaramcheti/hello_world_fw",
+        revision="8fd6e8e",
+        gcs_output_path=get_output(),
+        wait_for_completion=True,
+    ),
+)
+raw_data = get_input(
+    raw_download_step, "8fd6e8e/huggingface.co/datasets/skaramcheti/hello_world_fw/resolve/8fd6e8e/data/CC-MAIN-2024-10"
+)
 
 ############################################################
 # Transform HTML to text
@@ -19,44 +24,54 @@ raw_data = get_input(raw_download_step,
 from marin.schemas.web.convert import TrafilaturaConfig  # noqa
 from scripts.hello_world_fw.process import FineWebConfig, transform  # noqa
 
-transform_trafilatura_step = ExecutorStep(name="documents/hello_world_fw-pliang-trafilatura",
-                                          fn=transform,
-                                          config=FineWebConfig(
-    input_path=raw_data,
-    output_path=get_output(),
-    extract_method=versioned("trafilatura"),
-    config=TrafilaturaConfig(
-        favor_precision=versioned(False),
-        favor_recall=versioned(True),
-        include_comments=versioned(False),
-        deduplicate=versioned(False),
+transform_trafilatura_step = ExecutorStep(
+    name="documents/hello_world_fw-pliang-trafilatura",
+    fn=transform,
+    config=FineWebConfig(
+        input_path=raw_data,
+        output_path=get_output(),
+        extract_method=versioned("trafilatura"),
+        config=TrafilaturaConfig(
+            favor_precision=versioned(False),
+            favor_recall=versioned(True),
+            include_comments=versioned(False),
+            deduplicate=versioned(False),
+        ),
     ),
-))
+)
 
-transform_resiliparse_step = ExecutorStep(name="documents/hello_world_fw-pliang-resiliparse",
-                                          fn=transform,
-                                          config=FineWebConfig(
-    input_path=raw_data,
-    output_path=get_output(),
-    extract_method=versioned("resiliparse"),
-))
+transform_resiliparse_step = ExecutorStep(
+    name="documents/hello_world_fw-pliang-resiliparse",
+    fn=transform,
+    config=FineWebConfig(
+        input_path=raw_data,
+        output_path=get_output(),
+        extract_method=versioned("resiliparse"),
+    ),
+)
 
-transform_readability_step = ExecutorStep(name="documents/hello_world_fw-pliang-readability",
-                                          fn=transform,
-                                          config=FineWebConfig(
-    input_path=raw_data,
-    output_path=get_output(),
-    extract_method=versioned("readability"),
-))
+transform_readability_step = ExecutorStep(
+    name="documents/hello_world_fw-pliang-readability",
+    fn=transform,
+    config=FineWebConfig(
+        input_path=raw_data,
+        output_path=get_output(),
+        extract_method=versioned("readability"),
+    ),
+)
 
 ############################################################
 # Download good data
 
-download_sft_step = ExecutorStep(name="raw/tulu-v2-sft-mixture", fn=download, config=DownloadConfig(
-    hf_dataset_id="allenai/tulu-v2-sft-mixture",
-    revision="6248b17",
-    gcs_output_path=get_output(),
-))
+download_sft_step = ExecutorStep(
+    name="raw/tulu-v2-sft-mixture",
+    fn=download,
+    config=DownloadConfig(
+        hf_dataset_id="allenai/tulu-v2-sft-mixture",
+        revision="6248b17",
+        gcs_output_path=get_output(),
+    ),
+)
 
 # TODO: convert the data into Dolma format
 # For now, just use the one that's already given
@@ -67,77 +82,93 @@ sft_data = "gs://marin-us-central2/documents/instruct/v1_olmo_mix/text"
 
 from scripts.fasttext.train_fasttext import TrainFasttextClassifierConfig, train  # noqa
 
-train_quality_step = ExecutorStep(name="classifiers/hello_world_fw-pliang",
-                                  fn=train,
-                                  config=TrainFasttextClassifierConfig(
-    pos_doc_path=sft_data,
-    neg_doc_path=get_input(transform_trafilatura_step),
-    output_path=get_output(),
-    pos_sampling_rate=0.1,
-    neg_sampling_rate=1.0,
-    fasttext_args={"lr": 0.1},
-))
+train_quality_step = ExecutorStep(
+    name="classifiers/hello_world_fw-pliang",
+    fn=train,
+    config=TrainFasttextClassifierConfig(
+        pos_doc_path=sft_data,
+        neg_doc_path=get_input(transform_trafilatura_step),
+        output_path=get_output(),
+        pos_sampling_rate=0.1,
+        neg_sampling_rate=1.0,
+        fasttext_args={"lr": 0.1},
+    ),
+)
 
 ############################################################
 # Run inference with quality classifier
 
 from marin.processing.classification.inference import InferenceConfig, run_inference  # noqa
 
-inference_quality_step = ExecutorStep(name="attributes/hello_world_fw-pliang", fn=run_inference, config=InferenceConfig(
-    input_path=get_input(transform_trafilatura_step),
-    output_path=get_output(),
-    model_name="allenai/dolma-1_7-fasttext-quality-filter",
-    model_type="fasttext",
-    attribute_name="olmo-fasttext-quality",
-))
+inference_quality_step = ExecutorStep(
+    name="attributes/hello_world_fw-pliang",
+    fn=run_inference,
+    config=InferenceConfig(
+        input_path=get_input(transform_trafilatura_step),
+        output_path=get_output(),
+        model_name="allenai/dolma-1_7-fasttext-quality-filter",
+        model_type="fasttext",
+        attribute_name="olmo-fasttext-quality",
+    ),
+)
 
 ############################################################
 # Deduplicate
 
 from marin.processing.classification.dedupe import DedupeConfig, dedupe  # noqa
 
-dedupe_step = ExecutorStep(name="attributes/hello_world_fw-pliang-dedupe", fn=dedupe, config=DedupeConfig(
-  input_path=get_input(transform_trafilatura_step),
-  output_path=get_output(),
-))
+dedupe_step = ExecutorStep(
+    name="attributes/hello_world_fw-pliang-dedupe",
+    fn=dedupe,
+    config=DedupeConfig(
+        input_path=get_input(transform_trafilatura_step),
+        output_path=get_output(),
+    ),
+)
 
 ############################################################
 # Consolidate
 
 from marin.processing.classification.consolidate import ConsolidateConfig, FilterConfig, consolidate  # noqa
 
-consolidate_step = ExecutorStep(name="documents/hello_world_fw-pliang-consolidate",
-                                fn=consolidate,
-                                config=ConsolidateConfig(
-    input_path=get_input(transform_trafilatura_step),
-    output_path=get_output(),
-    filters=[
-        FilterConfig(
-            type=versioned("classify"),
-            attribute_path=get_input(inference_quality_step),
-            name=versioned("olmo-fasttext-quality"),
-            label="__label__hq",
-            threshold=versioned(0.1),
-        ),
-        FilterConfig(
-            type=versioned("dedupe"),
-            attribute_path=get_input(dedupe_step),
-            name=versioned("duplicate_text"),
-        ),
-    ],
-))
+consolidate_step = ExecutorStep(
+    name="documents/hello_world_fw-pliang-consolidate",
+    fn=consolidate,
+    config=ConsolidateConfig(
+        input_path=get_input(transform_trafilatura_step),
+        output_path=get_output(),
+        filters=[
+            FilterConfig(
+                type=versioned("classify"),
+                attribute_path=get_input(inference_quality_step),
+                name=versioned("olmo-fasttext-quality"),
+                label="__label__hq",
+                threshold=versioned(0.1),
+            ),
+            FilterConfig(
+                type=versioned("dedupe"),
+                attribute_path=get_input(dedupe_step),
+                name=versioned("duplicate_text"),
+            ),
+        ],
+    ),
+)
 
 ############################################################
 # Tokenize
 
 from marin.processing.tokenize import TokenizeConfig, tokenize  # noqa
 
-tokenize_step = ExecutorStep(name="tokenized/llama3/hello_world_fw-pliang", fn=tokenize, config=TokenizeConfig(
-    input_path=get_input(consolidate_step),
-    cache_path=get_output(),
-    dataset_name="hello_world_fw-pliang",  # Does this have to be unique?
-    tokenizer="meta-llama/Meta-Llama-3.1-8B",
-))
+tokenize_step = ExecutorStep(
+    name="tokenized/llama3/hello_world_fw-pliang",
+    fn=tokenize,
+    config=TokenizeConfig(
+        input_path=get_input(consolidate_step),
+        cache_path=get_output(),
+        dataset_name="hello_world_fw-pliang",  # Does this have to be unique?
+        tokenizer="meta-llama/Meta-Llama-3.1-8B",
+    ),
+)
 
 ############################################################
 # Training
@@ -152,10 +183,12 @@ tokenize_step = ExecutorStep(name="tokenized/llama3/hello_world_fw-pliang", fn=t
 ############################################################
 
 if __name__ == "__main__":
-    executor_main(steps=[
-        transform_trafilatura_step,
-        transform_resiliparse_step,  # Not used
-        transform_readability_step,  # Not used
-        #train_quality_step,  # Not used  (TODO: fails right now)
-        tokenize_step,
-    ])
+    executor_main(
+        steps=[
+            transform_trafilatura_step,
+            transform_resiliparse_step,  # Not used
+            transform_readability_step,  # Not used
+            # train_quality_step,  # Not used  (TODO: fails right now)
+            tokenize_step,
+        ]
+    )
