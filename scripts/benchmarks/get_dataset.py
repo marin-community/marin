e06fbdbb1f6cafec23b128436803d83622b41915
J38 <jebolton@stanford.edu>
2024-09-24 23:09:23 -0700

    fix code format

--- scripts/benchmarks/get_dataset.py
+++ scripts/benchmarks/get_dataset.py
@@ -10,6 +10,7 @@ from typing import List
 from dataclasses import dataclass, field
 import draccus
 
+
 @dataclass
 class DatasetConfig:
     dataset_name: str
@@ -25,11 +26,11 @@ class DatasetConfig:
     output_choices: List[str] = field(default_factory=list)
     options_key: str = ""
     output_format: str = ""
-    
+
 
 def load_datasets(config):
     """Load the dataset from huggingface cais.
-    
+
     This function returns all data for the given split (rather than subject specific data).
     """
     datasets = []
@@ -38,18 +39,19 @@ def load_datasets(config):
     for file_name in config.file_names:
         datasets.append(load_dataset(hf_path, path, split=file_name))
     return datasets
-    
+
+
 @draccus.wrap()
 def main(cfg: DatasetConfig):
 
     # Load config parameters
     datasets = load_datasets(cfg)
-    
+
     # Load dataset from huggingface dataset
     if cfg.output_format == "decontamination":
         for dataset, file_name in zip(datasets, cfg.file_names):
             output_path = os.path.join(cfg.output_prefix, f"{cfg.dataset_name}-{file_name}-decontamination.jsonl.gz")
-            with fsspec.open(output_path, "wt", compression='gzip') as dolma_file:
+            with fsspec.open(output_path, "wt", compression="gzip") as dolma_file:
                 for idx, example in enumerate(dataset):
                     subject = example.get(cfg.subject_key, "")
                     if cfg.answer_text_key:
@@ -76,14 +78,19 @@ def main(cfg: DatasetConfig):
     elif cfg.output_format == "evaluation":
         for dataset, data_file in zip(datasets, cfg.file_names):
             # Storing the data in a dictionary with the subject as the key
-            subject_files = defaultdict(lambda: '')
-            
+            subject_files = defaultdict(lambda: "")
+
             for example in dataset:
                 question = example[cfg.prompt_key]
-                
+
                 choices = example.get(cfg.options_key, [])
-                
-                input = question.strip() + "\n" + "\n".join([f"{cfg.output_choices[i]}. {choice}" for i, choice in enumerate(choices)]) + "\nAnswer:"
+
+                input = (
+                    question.strip()
+                    + "\n"
+                    + "\n".join([f"{cfg.output_choices[i]}. {choice}" for i, choice in enumerate(choices)])
+                    + "\nAnswer:"
+                )
                 if cfg.answer_text_key:
                     answer = example[cfg.answer_text_key]
                 elif cfg.answer_idx_key:
@@ -91,20 +98,22 @@ def main(cfg: DatasetConfig):
                     answer = cfg.output_choices[answer_idx]
                 else:
                     raise ValueError("Please specify either answer_text_key or answer_idx_key.")
-                
+
                 subject = example.get(cfg.subject_key, "")
-                
-                subject_files[subject] += (json.dumps({"input": input, "output": answer}) + "\n")
-            
+
+                subject_files[subject] += json.dumps({"input": input, "output": answer}) + "\n"
+
             # Writing from subject dict to corresponding files for each subject
             for subject in subject_files:
-                output_path = os.path.join(cfg.output_prefix, f"{cfg.dataset_name}-{subject}-{data_file}-evaluation.jsonl.gz")
+                output_path = os.path.join(
+                    cfg.output_prefix, f"{cfg.dataset_name}-{subject}-{data_file}-evaluation.jsonl.gz"
+                )
                 print(output_path)
                 with fsspec.open(output_path, "wt", compression="gzip") as f:
                     f.write(subject_files[subject])
     else:
         raise ValueError("Please specify either decontamination or evaluation for output_format.")
-    
-    
+
+
 if __name__ == "__main__":
     main()
