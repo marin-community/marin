c88d4f73b6f9fe8dd53e786921f93ce6a7be13ba
Tony Lee <tonyh.lee@yahoo.com>
2024-09-28 15:10:59 -0700

    resolved merge conflict

--- scripts/evaluation/simple_evaluator.py
+++ scripts/evaluation/simple_evaluator.py
@@ -3,9 +3,6 @@ from typing import Dict, List
 import time
 import traceback
 
-import ray
-
-from marin.utils import remove_tpu_lockfile_on_exit
 from scripts.evaluation.vllm_tpu_evaluator import VllmTpuEvaluator
 from scripts.evaluation.evaluator import ModelConfig
 
@@ -120,8 +117,6 @@ class SimpleEvaluator(VllmTpuEvaluator):
         "many_outputs": MANY_OUTPUTS_TEST_PLAN,
     }
 
-    @ray.remote(memory=64 * 1024 * 1024 * 1024, resources={"TPU": 4, "TPU-v4-8-head": 1})  # 64 GB of memory
-    @remove_tpu_lockfile_on_exit
     def run(
         self, model: ModelConfig, evals: List[str], output_path: str, max_eval_instances: int | None = None
     ) -> None:
