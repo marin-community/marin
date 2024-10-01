445774aa6fbad120fab41d69354687bc81c90c5d
Nikil Ravi <nravi@stanford.edu>
2024-10-01 13:46:25 -0700

    fix vocab issue, update quickstart

--- experiments/quickstart-synthetic.py
+++ experiments/quickstart-synthetic.py
@@ -1,38 +1,23 @@
-from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned, ExecutorMainConfig
+from marin.execution.executor import ExecutorStep, executor_main, output_path_of, this_output_path, versioned
+from marin.processing.classification.consolidate import ConsolidateConfig, FilterConfig, consolidate
+from marin.processing.classification.dedupe import DedupeConfig, dedupe
+from marin.processing.classification.inference import InferenceConfig, run_inference
+from marin.schemas.web.convert import TrafilaturaConfig
+from scripts.fasttext.train_fasttext import TrainFasttextClassifierConfig, train
+from scripts.hello_world_fw.process import FineWebConfig, transform
 
-############################################################
-# Download the pretraining data
-# from operations.download.huggingface.download import DownloadConfig, download
+synth_data = "gs://marin-us-central2/documents/nikil-synthetic-data-v1/"
 
-# raw_download_step = ExecutorStep(
-#     name="raw/hello_world_fw-pliang",
-#     fn=download,
-#     config=DownloadConfig(
-#         hf_dataset_id="skaramcheti/hello_world_fw",
-#         revision="8fd6e8e",
-#         gcs_output_path=this_output_path(),
-#         wait_for_completion=True,
-#     ),
-# )
-# raw_data = output_path_of(
-#     raw_download_step, "8fd6e8e/huggingface.co/datasets/skaramcheti/hello_world_fw/resolve/8fd6e8e/data/CC-MAIN-2024-10"
-# )
-
-############################################################
+# ############################################################
 # Transform HTML to text
 
-synth_data = "/Users/nikilravi/Desktop/Projects/data/"
-
-from marin.schemas.web.convert import TrafilaturaConfig  # noqa
-from scripts.hello_world_fw.process import FineWebConfig, transform  # noqa
-
-transform_trafilatura_step = ExecutorStep(
-    name="hq-transformed",
+transform_hq_data_step = ExecutorStep(
+    name="quickstart-data/hq-transformed",
     fn=transform,
     config=FineWebConfig(
-        input_path=synth_data,
-        output_path="/Users/nikilravi/Desktop/Projects/data/transformed",
-        extract_method=versioned("trafilatura"),
+        input_path=synth_data + "pos",
+        output_path=this_output_path(),
+        extract_method=versioned("readability"),
         config=TrafilaturaConfig(
             favor_precision=versioned(False),
             favor_recall=versioned(True),
@@ -42,99 +27,104 @@ transform_trafilatura_step = ExecutorStep(
     ),
 )
 
-# ############################################################
-# # Download good data
-
-# download_sft_step = ExecutorStep(
-#     name="raw/tulu-v2-sft-mixture",
-#     fn=download,
-#     config=DownloadConfig(
-#         hf_dataset_id="allenai/tulu-v2-sft-mixture",
-#         revision="6248b17",
-#         gcs_output_path=this_output_path(),
-#     ),
-# )
-
-# # TODO: convert the data into Dolma format
-# # For now, just use the one that's already given
-# sft_data = "gs://marin-us-central2/documents/instruct/v1_olmo_mix/text"
-
-# ############################################################
-# # Train quality classifier
-
-from scripts.fasttext.train_fasttext import TrainFasttextClassifierConfig, train
-
-train_quality_step = ExecutorStep(
-    name="synth-classifier",
-    fn=train,
-    config=TrainFasttextClassifierConfig(
-        pos_doc_path="/Users/nikilravi/Desktop/Projects/data/pos",
-        neg_doc_path="/Users/nikilravi/Desktop/Projects/data/neg",
-        output_path="/Users/nikilravi/Desktop/Projects/classifiers",
-        pos_sampling_rate=0.3,
-        neg_sampling_rate=1.0,
-        fasttext_args={"lr": 0.1},
+transform_lq_data_step = ExecutorStep(
+    name="quickstart-data/lq-transformed",
+    fn=transform,
+    config=FineWebConfig(
+        input_path=synth_data + "neg",
+        output_path=this_output_path(),
+        extract_method=versioned("readability"),
+        config=TrafilaturaConfig(
+            favor_precision=versioned(True),
+            favor_recall=versioned(False),
+            include_comments=versioned(False),
+            deduplicate=versioned(False),
+        ),
     ),
 )
 
 # ############################################################
-# # Run inference with quality classifier
+# # Train quality classifier
 
-# from marin.processing.classification.inference import InferenceConfig, run_inference
 
-# inference_quality_step = ExecutorStep(
-#     name="attributes/hello_world_fw-pliang",
-#     fn=run_inference,
-#     config=InferenceConfig(
-#         input_path=output_path_of(transform_trafilatura_step),
-#         output_path=this_output_path(),
-#         model_name="allenai/dolma-1_7-fasttext-quality-filter",
-#         model_type="fasttext",
-#         attribute_name="olmo-fasttext-quality",
-#     ),
-# )
+train_quality_step = ExecutorStep(
+    name="classifiers/synth-classifier-nikil",
+    fn=train,
+    config=TrainFasttextClassifierConfig(
+        pos_doc_path=output_path_of(transform_hq_data_step),
+        neg_doc_path=output_path_of(transform_lq_data_step),
+        output_path=this_output_path(),
+        pos_sampling_rate=0.3,
+        neg_sampling_rate=1.0,
+        fasttext_args={"lr": 0.01, "minCount": 1},
+    ),
+)
 
-# ############################################################
-# # Deduplicate
+############################################################
+# Run inference with quality classifier
 
-# from marin.processing.classification.dedupe import DedupeConfig, dedupe
+inference_hq_step = ExecutorStep(
+    name="quickstart-data/hq-inference",
+    fn=run_inference,
+    config=InferenceConfig(
+        input_path=output_path_of(transform_hq_data_step),
+        output_path=this_output_path(),
+        model_name="allenai/dolma-1_7-fasttext-quality-filter",
+        model_type="fasttext",
+        attribute_name="olmo-fasttext-quality",
+    ),
+)
 
-# dedupe_step = ExecutorStep(
-#     name="attributes/hello_world_fw-pliang-dedupe",
-#     fn=dedupe,
-#     config=DedupeConfig(
-#         input_path=output_path_of(transform_trafilatura_step),
-#         output_path=this_output_path(),
-#     ),
-# )
+inference_lq_step = ExecutorStep(
+    name="quickstart-data/hq-inference",
+    fn=run_inference,
+    config=InferenceConfig(
+        input_path=output_path_of(transform_lq_data_step),
+        output_path=this_output_path(),
+        model_name=output_path_of(train_quality_step),
+        model_type="fasttext",
+        attribute_name="quickstart-fasttext-quality",
+    ),
+)
 
-# ############################################################
-# # Consolidate
+############################################################
+# Deduplicate
 
-# from marin.processing.classification.consolidate import ConsolidateConfig, FilterConfig, consolidate
+dedupe_step = ExecutorStep(
+    name="attributes/hello_world_fw-pliang-dedupe",
+    fn=dedupe,
+    config=DedupeConfig(
+        input_path=output_path_of(transform_hq_data_step),
+        output_path=this_output_path(),
+    ),
+)
 
-# consolidate_step = ExecutorStep(
-#     name="documents/hello_world_fw-pliang-consolidate",
-#     fn=consolidate,
-#     config=ConsolidateConfig(
-#         input_path=output_path_of(transform_trafilatura_step),
-#         output_path=this_output_path(),
-#         filters=[
-#             FilterConfig(
-#                 type=versioned("classify"),
-#                 attribute_path=output_path_of(inference_quality_step),
-#                 name=versioned("olmo-fasttext-quality"),
-#                 label="__label__hq",
-#                 threshold=versioned(0.1),
-#             ),
-#             FilterConfig(
-#                 type=versioned("dedupe"),
-#                 attribute_path=output_path_of(dedupe_step),
-#                 name=versioned("duplicate_text"),
-#             ),
-#         ],
-#     ),
-# )
+############################################################
+# Consolidate
+
+
+consolidate_step = ExecutorStep(
+    name="documents/hello_world_fw-pliang-consolidate",
+    fn=consolidate,
+    config=ConsolidateConfig(
+        input_path=output_path_of(transform_hq_data_step),
+        output_path=this_output_path(),
+        filters=[
+            FilterConfig(
+                type=versioned("classify"),
+                attribute_path=output_path_of(inference_hq_step),
+                name=versioned("olmo-fasttext-quality"),
+                label="__label__hq",
+                threshold=versioned(0.1),
+            ),
+            FilterConfig(
+                type=versioned("dedupe"),
+                attribute_path=output_path_of(dedupe_step),
+                name=versioned("duplicate_text"),
+            ),
+        ],
+    ),
+)
 
 # ############################################################
 # # Tokenize
@@ -175,16 +165,13 @@ train_quality_step = ExecutorStep(
 #         ]
 #     )
 
-config = ExecutorMainConfig(
-    prefix="/Users/nikilravi/Desktop/Projects/data/transformed",
-    dry_run=False,
-    local_mode=True,
-)
-
 if __name__ == "__main__":
     executor_main(
         steps=[
-            #transform_trafilatura_step,
+            transform_hq_data_step,
+            transform_lq_data_step,
             train_quality_step,
+            # inference_hq_step,
+            # inference_lq_step,
         ]
     )
