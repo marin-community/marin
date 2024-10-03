d12eb4e06107057511ec377ba6f5eeb95689d490
Christopher Chou <emu4yolife@gmail.com>
2024-10-03 21:38:02 +0000

    Verify training works

--- experiments/164_quality_classifiers.py
+++ experiments/164_quality_classifiers.py
@@ -14,13 +14,13 @@ train_dclm_eli5_200k_rw_200k_step = ExecutorStep(
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/dclm/explainlikeimfive"),
                 label="__label__hq",
-                sampling_rate=versioned(200000),
+                absolute_sampling_rate=versioned(200000),
                 format=DatasetFormat.DOLMA_FORMATTED_JSONL,
             ),
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/dclm/negative_examples.txt"),
                 label="__label__lq",
-                sampling_rate=versioned(1.0),
+                relative_sampling_rate=versioned(1.0),
                 format=DatasetFormat.FASTTEXT,
             ),
         ],
@@ -37,19 +37,19 @@ train_dclm_eli5_100k_oh_100k_rw_200k_step = ExecutorStep(
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/dclm/explainlikeimfive"),
                 label="__label__hq",
-                sampling_rate=versioned(100000),
+                absolute_sampling_rate=versioned(100000),
                 format=DatasetFormat.DOLMA_FORMATTED_JSONL,
             ),
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/dclm/oh_100k.txt"),
                 label="__label__hq",
-                sampling_rate=versioned(100000),
+                absolute_sampling_rate=versioned(100000),
                 format=DatasetFormat.FASTTEXT,
             ),
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/dclm/negative_examples.txt"),
                 label="__label__lq",
-                sampling_rate=versioned(1.0),
+                relative_sampling_rate=versioned(1.0),
                 format=DatasetFormat.FASTTEXT,
             ),
         ],
@@ -66,13 +66,13 @@ train_dclm_oh_200k_rw_200k_step = ExecutorStep(
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/teknium--OpenHermes-2.5/v2024-09-29"),
                 label="__label__hq",
-                sampling_rate=versioned(200000),
+                absolute_sampling_rate=versioned(200000),
                 format=DatasetFormat.DOLMA_FORMATTED_JSONL,
             ),
             DatasetCurationConfig(
                 input_doc_path=versioned("gs://marin-us-central2/documents/dclm/negative_examples.txt"),
                 label="__label__lq",
-                sampling_rate=versioned(1.0),
+                relative_sampling_rate=versioned(1.0),
                 format=DatasetFormat.FASTTEXT,
             ),
         ],
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     executor_main(
         steps=[
             train_dclm_eli5_200k_rw_200k_step,
-            # train_dclm_eli5_100k_oh_100k_rw_200k_step,
-            # train_dclm_oh_200k_rw_200k_step,
+            train_dclm_eli5_100k_oh_100k_rw_200k_step,
+            train_dclm_oh_200k_rw_200k_step,
         ]
     )
