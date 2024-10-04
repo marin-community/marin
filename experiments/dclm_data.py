5007936d3b1f78fc2755ec0958b57a960a79baad
Nikil Ravi <nravi@stanford.edu>
2024-10-03 21:38:00 -0700

    add dclm-baseline dataset

--- experiments/dclm_data.py
+++ experiments/dclm_data.py
@@ -3,11 +3,25 @@ from marin.execution.executor import ExecutorStep, executor_main, this_output_pa
 from operations.download.huggingface.download import DownloadConfig, download
 
 """
-Downloads the following two datasets:
+Downloads the following datasets:
+- https://huggingface.co/datasets/mlfoundations/dclm-baseline-1.0
 - https://huggingface.co/datasets/bigcode/the-stack-dedup
 - https://huggingface.co/datasets/EleutherAI/proof-pile-2
 to GCS. These are the other datasets that were used in the DCLM paper (in addition to the actual DCLM-Baseline dataset).
 """
+
+############################################################
+# download DCLM-Baseline dataset
+# dclm_baseline_download_step = ExecutorStep(
+#     name="raw/dclm-baseline-1.0",
+#     fn=download,
+#     config=DownloadConfig(
+#         hf_dataset_id="mlfoundations/dclm-baseline-1.0",
+#         revision=versioned("a3b142c"), 
+#         gcs_output_path=this_output_path(),
+#         wait_for_completion=True,
+#     ),
+# )
 
 ############################################################
 # download The Stack dataset
@@ -40,6 +54,7 @@ proofpile_download_step = ExecutorStep(
 if __name__ == "__main__":
     executor_main(
         steps=[
+            # dclm_baseline_download_step,
             stack_download_step,
             proofpile_download_step,
         ]
