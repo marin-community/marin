ef6cc58a83944b9742241198a55e0edfccff77cc
Tony Lee <tonyh.lee@yahoo.com>
2024-10-05 22:34:51 -0700

    run ruff

--- experiments/evals.py
+++ experiments/evals.py
@@ -1,6 +1,6 @@
-from marin.execution.executor import ExecutorStep, executor_main, this_output_path
 from marin.evaluation.evaluation_config import EvaluationConfig
 from marin.evaluation.run import evaluate
+from marin.execution.executor import ExecutorStep, executor_main, this_output_path
 
 """
 Canonical set of evals.
