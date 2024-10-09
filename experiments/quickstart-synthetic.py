f84e1af61c4ae3a2d5dd7de64228c14c1fe78e62
Percy Liang <percyliang@gmail.com>
2024-10-08 21:20:03 -0700

    update docs

--- experiments/quickstart-synthetic.py
+++ experiments/quickstart-synthetic.py
@@ -34,7 +34,7 @@ class QuickstartExecutorConfig:
     prefix: str = "quickstart-tests"
 
     # path to synthetic test data
-    synth_data: str = "./tests/quick-start-tests"
+    synth_data: str = "./tests/quickstart-tests"
 
 
 def create_steps(config: QuickstartExecutorConfig) -> list[ExecutorStep]:
