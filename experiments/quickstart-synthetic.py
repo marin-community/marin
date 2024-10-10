44f556e2fad7da41e4bfc02cbbcfc602bed979ba
Nikil Ravi <nravi@stanford.edu>
2024-10-10 01:44:35 -0700

    another attempt at path

--- experiments/quickstart-synthetic.py
+++ experiments/quickstart-synthetic.py
@@ -6,6 +6,8 @@ import draccus
 from levanter.models.gpt2 import Gpt2Config
 from levanter.trainer import TrainerConfig
 
+from marin.evaluation.evaluation_config import EvaluationConfig
+from marin.evaluation.run import evaluate
 from marin.execution.executor import (
     ExecutorMainConfig,
     ExecutorStep,
@@ -207,18 +209,18 @@ def create_steps(config: QuickstartExecutorConfig) -> list[ExecutorStep]:
     ############################################################
     # Evaluation
 
-    # eval_step = ExecutorStep(
-    #     name=os.path.join(config.prefix, config.commit_hash, "eval"),
-    #     fn=evaluate,
-    #     config=EvaluationConfig(
-    #         evaluator="helm",
-    #         model_name=f"{run_id}/step-1",
-    #         model_path=os.path.join("/tmp", config.prefix, config.commit_hash, "train", run_id, "hf", run_id,"step-1"),
-    #         evaluation_path=this_output_path(),
-    #         evals=["mmlu"],
-    #         launch_with_ray=False,
-    #     ),
-    # )
+    eval_step = ExecutorStep(
+        name=os.path.join(config.prefix, config.commit_hash, "eval"),
+        fn=evaluate,
+        config=EvaluationConfig(
+            evaluator="helm",
+            model_name=f"{run_id}/step-1",
+            model_path=output_path_of(train_step, os.path.join("hf", run_id, "step-1")),
+            evaluation_path=this_output_path(),
+            evals=["mmlu"],
+            launch_with_ray=False,
+        ),
+    )
 
     return [
         transform_hq_data_step,
@@ -230,7 +232,7 @@ def create_steps(config: QuickstartExecutorConfig) -> list[ExecutorStep]:
         consolidate_step,
         tokenize_step,
         train_step,
-        # eval_step,
+        eval_step,
     ]
 
 
