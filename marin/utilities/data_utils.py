87e366186219906cd04a035cb67353c65d9081a8
J38 <jebolton@stanford.edu>
2024-10-11 03:01:14 -0700

    rename parameter

--- marin/utilities/data_utils.py
+++ marin/utilities/data_utils.py
@@ -1,6 +1,6 @@
 from dataclasses import asdict
 
-def dataclass_to_dict(document):
+def dataclass_to_dict(dataclass):
     """Convert dataclass to dictionary, omitting None values."""
-    return {k: v for k, v in asdict(document).items() if v is not None}
+    return {k: v for k, v in asdict(dataclass).items() if v is not None}
 
