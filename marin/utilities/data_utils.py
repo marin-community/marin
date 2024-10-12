b37b14ca2c7477720b81026a70e1312960ab70bc
J38 <jebolton@stanford.edu>
2024-10-12 01:26:32 -0700

    move function for dataclass to dict conversion into dataclass_utils

--- marin/utilities/data_utils.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import asdict
-
-
-def dataclass_to_dict(dataclass):
-    """Convert dataclass to dictionary, omitting None values."""
-    if not is_dataclass(obj):
-        raise ValueError(f"Expected dataclass, got '{obj}'")
-    return asdict(obj, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})
