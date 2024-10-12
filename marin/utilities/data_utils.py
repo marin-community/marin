bb8515f2d5e89ff3862cce66fe75e9e0d1b132e9
J38 <jebolton@stanford.edu>
2024-10-12 01:04:03 -0700

    raise error if not handed dataclass

--- marin/utilities/data_utils.py
+++ marin/utilities/data_utils.py
@@ -3,4 +3,6 @@ from dataclasses import asdict
 
 def dataclass_to_dict(dataclass):
     """Convert dataclass to dictionary, omitting None values."""
-    return {k: v for k, v in asdict(dataclass).items() if v is not None}
+    if not is_dataclass(obj):
+        raise ValueError(f"Expected dataclass, got '{obj}'")
+    return asdict(obj, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})```
