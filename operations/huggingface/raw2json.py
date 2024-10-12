15a0f30e10297b03c752211ba7e71337375c994f
J38 <jebolton@stanford.edu>
2024-10-12 01:27:13 -0700

    update dataclass to dict function name

--- operations/huggingface/raw2json.py
+++ operations/huggingface/raw2json.py
@@ -11,7 +11,7 @@ from datasets import Dataset, get_dataset_config_names, load_dataset
 from google.cloud import storage
 
 from marin.core.data import QAExample, QAExampleMetadata
-from marin.utilities.data_utils import dataclass_to_dict
+from marin.utilities.data_utils import dataclass_to_clean_dict
 
 
 class OutputFormatOptions(str, Enum):
@@ -361,7 +361,7 @@ def convert_dataset(cfg: DatasetConversionConfig) -> None:
                     document.prompt = prompt
                     document.response = response
                 # write json to output file
-                print(json.dumps(dataclass_to_dict(document)), file=dolma_file)
+                print(json.dumps(dataclass_to_clean_dict(document)), file=dolma_file)
 
 
 @draccus.wrap()
