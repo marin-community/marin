22196ae618274a5ef61423bc6c19376b6b7ba231
J38 <jebolton@stanford.edu>
2024-10-12 01:18:14 -0700

    remove quote characters

--- marin/utilities/data_utils.py
+++ marin/utilities/data_utils.py
@@ -5,4 +5,4 @@ def dataclass_to_dict(dataclass):
     """Convert dataclass to dictionary, omitting None values."""
     if not is_dataclass(obj):
         raise ValueError(f"Expected dataclass, got '{obj}'")
-    return asdict(obj, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})```
+    return asdict(obj, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})
