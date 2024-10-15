2f8d45c1a36f5641d50e1f04f006863b093f73b6
Herumb Shandilya <herumbshandilya123@gmail.com>
2024-10-14 22:33:47 -0700

    collapase extraction to same process and concatenate steps

--- experiments/246_web_extraction_method_training.py
+++ experiments/246_web_extraction_method_training.py
@@ -12,69 +12,153 @@ from marin.execution.executor import (
     this_output_path,
     versioned,
 )
+from marin.schemas.web.convert import HtmlToMarkdownConfig, ResiliparseConfig, TrafilaturaConfig
+from scripts.fineweb.process_parquet_fw import ParquetFWConfig, process_fw_dump
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("ray")
 
 
-@dataclass(frozen=True)
-class WebExtractionMethodConfig:
-    """
-    Configuration for the quickstart executor
+raw_data = "gs://marin-us-central2/raw/fineweb/cd85054/"
 
-    Attributes:
-    - extracted_data: str: The path to the data to train the model on based on the extraction method
-    - extraction_method_name: str: The name of the extraction method
-    """
-
-    extracted_data: str
-    extraction_method_name: str
-
-
-def create_steps(config: WebExtractionMethodConfig) -> list[ExecutorStep]:
-    fw_tokenized = default_tokenize(
-        name=f"fw-small-100B-{config.extraction_method_name}",
-        dataset=config.extracted_data,
-        tokenizer=llama3_tokenizer,
-    )
-    fw_100b_model = default_train(
-        name=f"fw-small-100B-1.4b-{config.extraction_method_name}",
-        tokenized=fw_tokenized,
-        model_config=llama_1_4b,
-        train_config=llama_1_4b_train_config,
+def get_extraction_steps():
+    transform_trafilatura_default_step = ExecutorStep(
+        name="documents/fineweb-small-trafilatura",
+        fn=process_fw_dump,
+        config=ParquetFWConfig(
+            input_path=raw_data,
+            cc_dumps=versioned(["CC-MAIN-2024-18"]),
+            output_path_md=this_output_path("md"),
+            output_path_text=this_output_path("text"),
+            extract_method=versioned("trafilatura"),
+            config=TrafilaturaConfig(
+                favor_precision=versioned(False),
+                favor_recall=versioned(True),
+                include_comments=versioned(False),
+                deduplicate=versioned(False),
+            ),
+        ),
     )
 
-    evaluate_step = ExecutorStep(
-        name=f"evaluation/fw-small-{config.extraction_method_name}",
-        fn=evaluate,
-        config=EvaluationConfig(
-            evaluator="helm",
-            model_name=versioned(config.extraction_method_name),
-            model_path=output_path_of(fw_100b_model),
-            evaluation_path=this_output_path(),
-            evals=["mmlu"],
+    transform_trafilatura_favor_precision_step = ExecutorStep(
+        name="documents/fineweb-small-trafilatura-favor-precision",
+        fn=process_fw_dump,
+        config=ParquetFWConfig(
+            input_path=raw_data,
+            cc_dumps=versioned(["CC-MAIN-2024-18"]),
+            output_path_md=this_output_path("md"),
+            output_path_text=this_output_path("text"),
+            extract_method=versioned("trafilatura"),
+            config=TrafilaturaConfig(
+                favor_precision=versioned(True),
+                favor_recall=versioned(False),
+                include_comments=versioned(False),
+                deduplicate=versioned(False),
+            ),
+        ),
+    )
+
+    transform_resiliparse_default_step = ExecutorStep(
+        name="documents/fineweb-small-resiliparse-default",
+        fn=process_fw_dump,
+        config=ParquetFWConfig(
+            input_path=raw_data,
+            cc_dumps=versioned(["CC-MAIN-2024-18"]),
+            output_path_md=this_output_path("md"),
+            output_path_text=this_output_path("text"),
+            extract_method=versioned("resiliparse"),
+            config=ResiliparseConfig(
+                preserve_formatting=versioned(False),
+                main_content=versioned(True),
+                links=versioned(False),
+            ),
+        ),
+    )
+
+    transform_resiliparse_preserve_formatting_step = ExecutorStep(
+        name="documents/fineweb-small-resiliparse-preserve-formatting",
+        fn=process_fw_dump,
+        config=ParquetFWConfig(
+            input_path=raw_data,
+            cc_dumps=versioned(["CC-MAIN-2024-18"]),
+            output_path_md=this_output_path("md"),
+            output_path_text=this_output_path("text"),
+            extract_method=versioned("resiliparse"),
+            config=ResiliparseConfig(
+                preserve_formatting=versioned(True),
+                main_content=versioned(True),
+                links=versioned(False),
+            ),
+        ),
+    )
+
+    transform_readability_step = ExecutorStep(
+        name="documents/fineweb-small-readability",
+        fn=process_fw_dump,
+        config=ParquetFWConfig(
+            input_path=raw_data,
+            cc_dumps=versioned(["CC-MAIN-2024-18"]),
+            output_path_md=this_output_path("md"),
+            output_path_text=this_output_path("text"),
+            extract_method=versioned("readability"),
+            config=HtmlToMarkdownConfig(
+                include_images=versioned(False),
+                include_links=versioned(False),
+            ),
         ),
     )
 
     return [
-        fw_tokenized,
-        fw_100b_model,
-        evaluate_step,
+        transform_trafilatura_default_step,
+        transform_trafilatura_favor_precision_step,
+        transform_resiliparse_default_step,
+        transform_resiliparse_preserve_formatting_step,
+        transform_readability_step,
     ]
 
 
-if __name__ == "__main__":
-    dataset = {
-        "readability": "gs://marin-us-central2/documents/fineweb-small-readability-473448",
-        "resiliparse": "gs://marin-us-central2/documents/fineweb-small-resiliparse-default-bfaf03",
-        "resiliparse_with_preserve_formatting": "gs://marin-us-central2/documents/fineweb-small-resiliparse-preserve-formatting-e8c6ec",
-        "trafilatura": "gs://marin-us-central2/documents/fineweb-small-trafilatura-0465ba",
-        "trafilatura_with_favor_precision": "gs://marin-us-central2/documents/fineweb-small-trafilatura-favor-precision-cf6c34",
-    }
+def create_steps() -> list[ExecutorStep]:
+    total_steps = []
 
-    for extraction_method, data_path in dataset.items():
-        steps = create_steps(
-            WebExtractionMethodConfig(extracted_data=data_path, extraction_method_name=extraction_method)
+    extraction_steps = get_extraction_steps()
+    
+    for step in extraction_steps:
+        step_name = step.name.split("/", 1)[1]
+        
+        fw_tokenized = default_tokenize(
+            name=f"fw-small-100B-{step_name}",
+            dataset=output_path_of(step),
+            tokenizer=llama3_tokenizer,
+        )
+        fw_100b_model = default_train(
+            name=f"fw-small-100B-1.4b-{step_name}",
+            tokenized=fw_tokenized,
+            model_config=llama_1_4b,
+            train_config=llama_1_4b_train_config,
         )
 
-        executor_main(steps=steps)
+        evaluate_step = ExecutorStep(
+            name=f"evaluation/fw-small-{step_name}",
+            fn=evaluate,
+            config=EvaluationConfig(
+                evaluator="helm",
+                model_name=versioned(step_name),
+                model_path=output_path_of(fw_100b_model),
+                evaluation_path=this_output_path(),
+                evals=["mmlu"],
+            ),
+        )
+
+        total_steps.extend([
+            step,
+            fw_tokenized, 
+            fw_100b_model, 
+            evaluate_step
+        ])
+
+    return total_steps
+
+
+if __name__ == "__main__":
+    steps = create_steps()
+    executor_main(steps=steps)
