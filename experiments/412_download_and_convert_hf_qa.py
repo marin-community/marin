5a85bbbced1f0a28ff68eca0acca45328da18b6d
J38 <jebolton@stanford.edu>
2024-10-16 01:41:26 -0700

    update script to have explicit download step

--- experiments/412_download_and_convert_hf_qa.py
+++ experiments/412_download_and_convert_hf_qa.py
@@ -1,4 +1,5 @@
-from marin.execution.executor import ExecutorStep, executor_main, this_output_path
+from marin.execution.executor import ExecutorStep, executor_main, output_path_of, this_output_path
+from operations.download.huggingface.download import DownloadConfig, download
 from operations.huggingface.qa.raw2json import DatasetConversionConfig, OutputFormatOptions, raw2json
 
 """
@@ -7,7 +8,21 @@ Downloads the following datasets
 and saves internal eval and decontamination formats to GCS.
 """
 ############################################################
-# Convert mmlu to evaluation format
+# download mmlu dataset
+mmlu_download_step = ExecutorStep(
+    name="raw/cais/mmlu",
+    fn=download,
+    config=DownloadConfig(
+        hf_dataset_id="cais/mmlu",
+        revision=versioned("c30699e"),
+        gcs_output_path=this_output_path(),
+        wait_for_completion=False,
+    ),
+    override_output_path="gs://marin-us-central2/raw/cais/mmlu",  # no versioned path; this had already been downloaded
+)
+############################################################
+# Convert mmlu to evaluation format (i.e. JSON with "prompt", "response" fields)
+# This is the input for internal evaluation which measures PPL model gives to correct responses to prompts
 mmlu_convert_eval_aux = ExecutorStep(
     name="evaluation/mmlu-eval_aux",
     fn=raw2json,
@@ -15,7 +30,7 @@ mmlu_convert_eval_aux = ExecutorStep(
         dataset_name="cais/mmlu",
         subsets=["all"],
         splits=["auxiliary_train"],
-        input_path="cais/mmlu",
+        input_path=output_path_of(mmlu_download_step),
         hf_path="cais/mmlu",
         output_path=this_output_path(),
         output_format=OutputFormatOptions("evaluation"),
@@ -33,7 +48,7 @@ mmlu_convert_eval_subject = ExecutorStep(
         dataset_name="cais/mmlu",
         subsets=["*"],
         splits=["dev", "validation"],
-        input_path="cais/mmlu",
+        input_path=output_path_of(mmlu_download_step),
         hf_path="cais/mmlu",
         output_path=this_output_path(),
         output_format=OutputFormatOptions("evaluation"),
@@ -44,8 +59,9 @@ mmlu_convert_eval_subject = ExecutorStep(
         exclude_subsets=["all"],
     ),
 )
-
-# Convert mmlu to dolma format
+############################################################
+# Convert mmlu to dolma format (i.e. JSON with "text" field)
+# This is used as input to the decontamination pipeline so documents with MMLU content are removed
 mmlu_convert_dolma = ExecutorStep(
     name="decontamination/mmlu-dolma",
     fn=raw2json,
@@ -53,7 +69,7 @@ mmlu_convert_dolma = ExecutorStep(
         dataset_name="cais/mmlu",
         subsets=["all"],
         splits=["dev", "test", "validation"],
-        input_path="cais/mmlu",
+        input_path=output_path_of(mmlu_download_step),
         hf_path="cais/mmlu",
         output_path=this_output_path(),
         output_format=OutputFormatOptions("decontamination"),
@@ -69,6 +85,7 @@ mmlu_convert_dolma = ExecutorStep(
 if __name__ == "__main__":
     executor_main(
         steps=[
+            mmlu_download_step,
             mmlu_convert_eval_aux,
             mmlu_convert_eval_subject,
             mmlu_convert_dolma,
