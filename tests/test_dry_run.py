c1efbb7697fefebeea4749d9625b7077b7af0f12
Abhinav Garg <abhinavg@stanford.edu>
2024-10-19 19:38:19 -0700

    Changes to test_dry_run

--- tests/test_dry_run.py
+++ tests/test_dry_run.py
@@ -1,5 +1,6 @@
 import subprocess
 import tempfile
+import time
 import unittest
 from pathlib import Path
 
@@ -14,24 +15,30 @@ def ray_start():
     ray.shutdown()  # teardown
 
 
-def create_temp_dir():
-    with tempfile.TemporaryDirectory(prefix="executor-", delete=False) as temp_dir:
-        return temp_dir
-
-
 def test_run_dry_runs():
     """Test the dry runs of experiment scripts"""
     experiments_dir = Path("experiments")
+    skip_marker = "# SKIP_DRY_RUN_TEST"
     if experiments_dir.exists() and experiments_dir.is_dir():
+
         for script in experiments_dir.glob("*.py"):
-            temp_dir = create_temp_dir()
-            result = subprocess.run(
-                ["python", script, "--dry_run", "True", "--executor_info_base_path", temp_dir, "--prefix", temp_dir],
-                capture_output=True,
-                text=True,
-            )
+            start = time.time()  # Start time for performance measurement
+            # Check if the script contains the skip marker
+            with open(script, "r") as file:
+                content = file.read()
+                if skip_marker in content:
+                    print(f"Skipping {script} (contains skip marker)")
+                    continue  # Skip this file
+
+            with tempfile.TemporaryDirectory(prefix="executor-") as temp_dir:
+                result = subprocess.run(
+                    ["python", script, "--dry_run", "True", "--executor_info_base_path", temp_dir, "--prefix", temp_dir],
+                    capture_output=True,
+                    text=True,
+                )
+
             assert result.returncode == 0, f"Dry run failed for {script}"
-            print(f"Output for {script}: {result.stdout}")
+            print(f"Execution time for {script}: {time.time() - start} seconds")
     else:
         print("No experiments directory found")
 
