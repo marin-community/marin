03430c049d10a3949b3506c350d0b7240bd446d9
Nelson Liu <nfliu@nelsonliu.me>
2024-10-24 15:35:18 -0700

    Add experiment to download open-web-math from HF

---
+++ experiments/open-web-math/open-web-math_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Downloads the open-web-math dataset
+(https://huggingface.co/datasets/open-web-math/open-web-math) to GCS.
+"""
+
+from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
+from operations.download.huggingface.download import DownloadConfig, download
+
+############################################################
+# download open-web-math dataset
+open_web_math_download_step = ExecutorStep(
+    name="raw/open-web-math",
+    fn=download,
+    config=DownloadConfig(
+        hf_dataset_id="open-web-math/open-web-math",
+        revision=versioned("fde8ef8"),
+        gcs_output_path=this_output_path(),
+        wait_for_completion=False,
+    ),
+    override_output_path="gs://marin-us-central2/raw/open-web-math-fde8ef8",
+)
+
+############################################################
+
+if __name__ == "__main__":
+    executor_main(
+        steps=[
+            open_web_math_download_step,
+        ]
+    )
