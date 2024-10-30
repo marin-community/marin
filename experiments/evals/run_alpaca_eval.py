43d9f7650fa5c97c784802065587748aff59c3dd
Tony Lee <tonyh.lee@yahoo.com>
2024-10-29 22:01:43 -0700

    run on gpu experiment

--- experiments/evals/run_alpaca_eval.py
+++ experiments/evals/run_alpaca_eval.py
@@ -1,14 +1,13 @@
 from marin.execution.executor import ExecutorMainConfig, executor_main
-from .evals import evaluate_helm
+from .evals import evaluate_alpaca_eval
 
 
 executor_main_config = ExecutorMainConfig()
 steps = [
-    evaluate_helm(
+    evaluate_alpaca_eval(
         model_name="pf5pe4ut/step-600",
         model_path="gs://marin-us-central2/checkpoints/quickstart_single_script_docker_test_09_18/"
         "pf5pe4ut/hf/pf5pe4ut/step-600",
-        evals=["mmlu"],
     ),
 ]
 
