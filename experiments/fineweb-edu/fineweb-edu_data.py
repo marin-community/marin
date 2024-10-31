fb736ae176368a98d2228d98a0aa70e28e7659cc
Nelson Liu <nfliu@nelsonliu.me>
2024-10-31 12:22:39 -0700

    Add script to download fineweb-edu to GCS

---
+++ experiments/fineweb-edu/fineweb-edu_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Downloads the fineweb-edu dataset
+(https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu) to GCS.
+"""
+
+from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
+from operations.download.huggingface.download import DownloadConfig, download
+
+############################################################
+# download fineweb-edu dataset
+fineweb_edu_raw = ExecutorStep(
+    name="raw/fineweb-edu",
+    fn=download,
+    config=DownloadConfig(
+        hf_dataset_id="HuggingFaceFW/fineweb-edu",
+        revision=versioned("651a648"),
+        gcs_output_path=this_output_path(),
+        wait_for_completion=False,
+    ),
+    override_output_path="gs://marin-us-central2/raw/fineweb-edu-fde8ef8",
+)
+
+############################################################
+
+if __name__ == "__main__":
+    executor_main(
+        steps=[
+            fineweb_edu_raw,
+        ]
+    )
