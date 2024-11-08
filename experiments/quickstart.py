a6c89643b5b03ea86a6624e883eaa947114883a2
Kamyar Salahi <kamyar@stanford.edu>
2024-11-08 12:15:01 -0800

    Adding internal evals to marin (#495)

--- experiments/quickstart.py
+++ experiments/quickstart.py
@@ -4,9 +4,11 @@ import os
 import sys
 
 import draccus
+from levanter.data.text import LMSupervisedDatasetConfig
 from levanter.models.gpt2 import Gpt2Config
 from levanter.trainer import TrainerConfig
 
+from experiments.raw2json import mmlu_convert_eval_aux, mmlu_convert_eval_subject
 from marin.classifiers.utils import DatasetConfig
 from marin.execution.executor import (
     ExecutorMainConfig,
@@ -23,7 +25,7 @@ from marin.processing.classification.fasttext.train_fasttext import (
     train,
 )
 from marin.processing.classification.inference import InferenceConfig, run_inference
-from marin.processing.tokenize import TokenizeConfig, lm_data_config, tokenize
+from marin.processing.tokenize import TokenizeConfig, levanter_tokenize_supervised, lm_data_config, tokenize
 from marin.schemas.web.convert import HtmlToMarkdownConfig
 from marin.training.training import TrainLmOnPodConfig, run_levanter_train_lm
 from marin.utilities.ray_utils import is_local_ray_cluster
@@ -158,6 +160,7 @@ def create_steps(prefix: str, synth_data: str) -> list[ExecutorStep]:
 
     ############################################################
     # Tokenize
+    tokenizer = "gpt2"
 
     tokenize_step = ExecutorStep(
         name=os.path.join(prefix, "tokenized"),
@@ -166,8 +169,33 @@ def create_steps(prefix: str, synth_data: str) -> list[ExecutorStep]:
             train_paths=output_path_of(consolidate_step),
             validation_paths=[],
             cache_path=this_output_path(),
-            tokenizer=versioned("gpt2"),
+            tokenizer=versioned(tokenizer),
         ),
+    )
+
+    evaluation_data_cache = ExecutorStep(
+        name="tokenized/evaluation/mmlu",
+        fn=levanter_tokenize_supervised,
+        config=TokenizeConfig(
+            train_paths=[],
+            validation_paths=[
+                output_path_of(mmlu_convert_eval_subject).cd("cais/mmlu-virology-dev-evaluation.jsonl.gz"),
+            ],
+            cache_path=this_output_path(),
+            input_field="prompt",
+            output_field="response",
+            tokenizer=versioned(tokenizer),
+        ),
+    )
+
+    evaluation_data_config = LMSupervisedDatasetConfig(
+        validation_urls=[
+            output_path_of(mmlu_convert_eval_aux).cd("cais/*.jsonl.gz"),
+            output_path_of(mmlu_convert_eval_subject).cd("cais/*.jsonl.gz"),
+        ],
+        cache_dir=output_path_of(evaluation_data_cache),
+        input_field="prompt",
+        output_field="response",
     )
 
     ############################################################
@@ -183,6 +211,7 @@ def create_steps(prefix: str, synth_data: str) -> list[ExecutorStep]:
         config=TrainLmOnPodConfig(
             output_path=this_output_path(),
             data=lm_data_config(tokenize_step),
+            supervised_data=evaluation_data_config,
             env={
                 "WANDB_API_KEY": None,
                 "WANDB_MODE": "disabled",
