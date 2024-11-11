0202d9468c5a17c16d47b7f7f07d1c898b873439
Nikil Ravi <nravi@stanford.edu>
2024-11-10 23:56:39 -0800

    add lm_eval_harness_step

--- experiments/evals/run_on_gpu.py
+++ experiments/evals/run_on_gpu.py
@@ -6,13 +6,23 @@ For evals that need to be run on GPUs (e.g. LM Evaluation Harness).
 """
 
 executor_main_config = ExecutorMainConfig()
+
+quickstart_eval_step = evaluate_lm_evaluation_harness(
+    model_name="pf5pe4ut/step-600",
+    model_path="gs://marin-us-central2/checkpoints/quickstart_single_script_docker_test_09_18/"
+    "pf5pe4ut/hf/pf5pe4ut/step-600",
+    evals=["mmlu"],
+)
+
+exp433_dclm_1b_1x_eval = evaluate_lm_evaluation_harness(
+    model_name="dclm_1b_1x_replication_oct26-a28b1e",
+    model_path="gs://marin-us-central2/checkpoints/dclm_1b_1x_replication_oct26-a28b1e/hf/dclm_1b_1x_replication_oct26-a28b1e/step-54931",
+    evals=["mmlu"],
+)
+
 steps = [
-    evaluate_lm_evaluation_harness(
-        model_name="pf5pe4ut/step-600",
-        model_path="gs://marin-us-central2/checkpoints/quickstart_single_script_docker_test_09_18/"
-        "pf5pe4ut/hf/pf5pe4ut/step-600",
-        evals=["mmlu"],
-    ),
+    quickstart_eval_step,
+    exp433_dclm_1b_1x_eval,
 ]
 
 if __name__ == "__main__":
