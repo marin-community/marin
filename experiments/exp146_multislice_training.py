b47c9d1815c11232e36d4bc393346a9e49d6acc6
Jason Wang <blahblahj.wsy@gmail.com>
2024-11-10 23:18:37 -0800

    only keep fineweb exp. rollback 300m config

--- experiments/exp146_multislice_training.py
+++ experiments/exp146_multislice_training.py
@@ -3,44 +3,14 @@ Train 1.4B models on standard datasets (e.g., SlimPajama) using multislice.
 https://github.com/stanford-crfm/marin/issues/146
 """
 
-import os
-
 from experiments.defaults import default_tokenize, default_train
 from experiments.llama import (
     llama3_tokenizer,
     llama_1_4b,
     llama_1_4b_multislice_train_config,
-    llama_300m,
-    llama_300m_multislice_train_config,
 )
-from experiments.pretraining_datasets import fineweb_edu, slimpajama, slimpajama_6b
-from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
-from marin.processing.tokenize import TokenizeConfig, tokenize
-
-slimpajama_6b_tokenized = default_tokenize(name="SlimPajama-6B", dataset=slimpajama_6b, tokenizer=llama3_tokenizer)
-slimpajama_6b_model = default_train(
-    name="SlimPajama-6B-1.4b-multislice",
-    tokenized=slimpajama_6b_tokenized,
-    model_config=llama_300m,
-    train_config=llama_300m_multislice_train_config,
-)
-
-slimpajama_tokenized = ExecutorStep(
-    name=os.path.join("tokenized", "SlimPajama-627B"),
-    fn=tokenize,
-    config=TokenizeConfig(
-        train_paths=[slimpajama.cd("train")],
-        validation_paths=[slimpajama.cd("validation")],
-        cache_path=this_output_path(),
-        tokenizer=versioned(llama3_tokenizer),
-    ),
-)
-slimpajama_model = default_train(
-    name="SlimPajama-627B-1.4b-multislice",
-    tokenized=slimpajama_tokenized,
-    model_config=llama_1_4b,
-    train_config=llama_1_4b_multislice_train_config,
-)
+from experiments.pretraining_datasets import fineweb_edu
+from marin.execution.executor import executor_main
 
 fineweb_edu_tokenized = default_tokenize(name="fineweb-edu", dataset=fineweb_edu, tokenizer=llama3_tokenizer)
 fineweb_edu_model = default_train(
@@ -54,10 +24,6 @@ fineweb_edu_model = default_train(
 
 if __name__ == "__main__":
     executor_main(
-        steps=[
-            slimpajama_6b_model,
-            slimpajama_model,
-            fineweb_edu_model,
-        ],
+        steps=[fineweb_edu_model],
         description="Train 1.4B models on standard datasets (SlimPajama 6B, SlimPajama, FineWebEdu).",
     )
--- experiments/llama.py
+++ experiments/llama.py
@@ -73,18 +73,9 @@ llama_8b = LlamaConfig(
 )
 
 llama_300m_train_config = SimpleTrainConfig(
-    tpu_type="v4-128",
+    tpu_type="v4-32",
     train_batch_size=1024,
     num_train_steps=2000,  # 4096 * 1024 * 2000 = 8.4B tokens
-    learning_rate=3e-4,
-    weight_decay=0.1,
-)
-
-llama_300m_multislice_train_config = SimpleTrainConfig(
-    tpu_type="v4-128",
-    node_count=2,
-    train_batch_size=1024,
-    num_train_steps=1000,  # 4096 * 1024 * 1000 * 2 = 8.4B tokens
     learning_rate=3e-4,
     weight_decay=0.1,
 )
