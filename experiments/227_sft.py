370aabbbb2564c9584647e2e0c7f60591ab9299c
Ahmed Ahmed <ahmedah@stanford.edu>
2024-11-11 16:58:39 -0800

    clean up sft

--- experiments/227_sft.py
+++ experiments/227_sft.py
@@ -8,21 +8,19 @@ export HF_TOKEN=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  && python -m levanter.mai
 --config_path levanter/config/llama_sft_hf_ckpt.yaml
 """
 
-import dataclasses
 from datetime import timedelta
 
 import draccus
 import jmp
 from instruction_datasets import get_instruction_dataset
 from levanter.checkpoint import CheckpointerConfig
+from levanter.data.text import LMSupervisedDatasetConfig
 from levanter.tracker.wandb import WandbConfig
+from levanter.trainer import TrainerConfig
 
 from marin.execution.executor import Executor, ExecutorStep, executor_main, output_path_of, this_output_path
 from marin.processing.tokenize.tokenize import TokenizeConfig, levanter_tokenize_sft
 from marin.training.training import TrainSFTOnPodConfig, run_levanter_sft
-
-# Load base config
-training_config = draccus.load(TrainSFTOnPodConfig, open("config/training/standard_sft.yaml"))
 
 # Get instruction dataset
 instruction_dataset = get_instruction_dataset("allenai/tulu-v2-sft-mixture")
@@ -41,31 +39,31 @@ tokenize_step = ExecutorStep(
     config=TokenizeConfig(
         train_paths=[f"{actual_gcs_path}/**/*.jsonl.gz"],
         validation_paths=[],
-        cache_path=training_config.supervised_data.cache_dir,
-        tokenizer=training_config.tokenizer,
-        input_field=training_config.input_role,
-        output_field=training_config.output_role,  # Or whatever tokenizer you're using
-        seq_len=training_config.max_seq_len,
+        cache_path="gs://marin-us-central2/tokenized/sft_cache/tulu-v2",
+        tokenizer="EleutherAI/gpt-neox-20b",
+        # fixed to OAI chat format
+        input_field="user",
+        output_field="assistant",  # Or whatever tokenizer you're using
+        # OLMO SFT uses 2048 as max length
+        seq_len=2048,
     ),
     description="Tokenize chat SFT data",
 )
 
+
 train_step = ExecutorStep(
     name="checkpoints/tulu_sft_3eps",
     fn=run_levanter_sft,
-    config=dataclasses.replace(
-        training_config,
+    config=TrainSFTOnPodConfig(
         output_path=this_output_path(),
-        tpu_type="v4-128",
+        tpu_type="v4-64",
         # number of epochs over the dataset set to reproduce Olmo SFT
         epoch=3,
-        supervised_data=dataclasses.replace(
-            training_config.supervised_data,
-            cache_dir=training_config.supervised_data.cache_dir,
+        supervised_data=LMSupervisedDatasetConfig(
+            cache_dir="gs://marin-us-central2/tokenized/sft_cache/tulu-v2", input_field="user", output_field="assistant"
         ),
         # Modify the nested trainer config by creating a new one
-        trainer=dataclasses.replace(
-            training_config.trainer,  # Start with the existing trainer config
+        trainer=TrainerConfig(
             tracker=WandbConfig(
                 project="marin",
             ),
@@ -79,7 +77,6 @@ train_step = ExecutorStep(
         ),
     ),
 )
-
 
 
 if __name__ == "__main__":
