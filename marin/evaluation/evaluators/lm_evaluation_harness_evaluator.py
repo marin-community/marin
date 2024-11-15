8f84487f9aa3ba66529e4eb7dbaf302487b21217
Nikil Ravi <nravi@stanford.edu>
2024-11-14 23:04:37 -0800

    modify eval code to be more configurable

--- marin/evaluation/evaluators/lm_evaluation_harness_evaluator.py
+++ marin/evaluation/evaluators/lm_evaluation_harness_evaluator.py
@@ -3,6 +3,7 @@ import shutil
 import traceback
 from typing import ClassVar
 
+from marin.evaluation.evaluation_config import EvalTaskConfig
 from marin.evaluation.evaluators.evaluator import Dependency, ModelConfig
 from marin.evaluation.evaluators.vllm_tpu_evaluator import VllmTpuEvaluator
 from marin.evaluation.utils import is_remote_path, run_bash_command, set_cuda_visible_devices, upload_to_gcs
@@ -23,14 +24,14 @@ class LMEvaluationHarnessEvaluator(VllmTpuEvaluator):
     ]
 
     def evaluate(
-        self, model: ModelConfig, evals: list[str], output_path: str, max_eval_instances: int | None = None
+        self, model: ModelConfig, evals: list[EvalTaskConfig], output_path: str, max_eval_instances: int | None = None
     ) -> None:
         """
         Runs EleutherAI's lm-eval harness on the specified model and set of  tasks.
 
         Args:
             model (ModelConfig): The model configuration of the model we want to evaluate
-            evals (List[str]): The list of evaluations to run.
+            evals (List[EvalTaskConfig]): The list of evaluations to run.
             output_path (str): The path to save the evaluation results.
             max_eval_instances (int | None): The maximum number of evaluation instances to run.
         """
@@ -42,30 +43,35 @@ class LMEvaluationHarnessEvaluator(VllmTpuEvaluator):
             # Download the model from GCS or HuggingFace
             model_name_or_path: str = self.download_model(model)
 
-            # Construct the command to run lm-eval
-            command: list[str] = [
-                "lm_eval",
-                "--model",
-                "vllm",
-                "--tasks",
-                ",".join(evals),
-                "--model_args",
-                f"pretrained={model_name_or_path}",
-                "--batch_size",
-                "auto",
-                "--output_path",
-                self.RESULTS_PATH,
-            ]
-            if max_eval_instances is not None:
-                # According lm-eval-harness, --limit should only be used for testing purposes
-                command.extend(["--limit", str(max_eval_instances)])
+            for eval_task in evals:
 
-            run_bash_command(command, check=False)
-            assert os.path.exists(self.RESULTS_PATH), f"Results path {self.RESULTS_PATH} does not exist."
+                command = [
+                    "lm_eval",
+                    "--model",
+                    "vllm",
+                    "--tasks",
+                    eval_task.name,
+                    "--num_fewshot",
+                    int(eval_task.num_fewshot),
+                    "--model_args",
+                    f"pretrained={model_name_or_path}",
+                    "--batch_size",
+                    "auto",
+                    "--output_path",
+                    os.path.join(self.RESULTS_PATH, eval_task.name, "_", eval_task.num_fewshot, "shot.jsonl"),
+                ]
 
-            # Upload the results to GCS
-            if is_remote_path(output_path):
-                upload_to_gcs(self.RESULTS_PATH, output_path)
+                if max_eval_instances is not None:
+                    # According lm-eval-harness, --limit should only be used for testing purposes
+                    command.extend(["--limit", str(max_eval_instances)])
+
+                run_bash_command(command, check=False)
+                assert os.path.exists(self.RESULTS_PATH), f"Results path {self.RESULTS_PATH} does not exist."
+
+                # Upload the results to GCS
+                if is_remote_path(output_path):
+                    upload_to_gcs(self.RESULTS_PATH, output_path)
+
         except Exception as e:
             traceback.print_exc()
             raise RuntimeError("lm-eval failed. Please check the logs for more information.") from e
