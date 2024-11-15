e15279d2d5cc403343d6e075d5a7b1d57ba342fe
Herumb Shandilya <herumbshandilya123@gmail.com>
2024-11-15 15:29:52 -0800

    comment resolves

--- experiments/exp355_resiliparse_mardownify_extraction.py
+++ experiments/exp355_resiliparse_mardownify_extraction.py
@@ -1,40 +1,34 @@
 """
-Test different html->text transformation methods (on FineWeb, train 1.4B models).
-https://github.com/stanford-crfm/marin/issues/246
+Test the Resiliparse custom fork on the fineweb-small dataset.
+https://github.com/stanford-crfm/marin/issues/355
 """
 
 import logging
 
 from experiments.defaults import default_tokenize, default_train
 from experiments.llama import llama3_tokenizer, llama_1_4b, llama_1_4b_train_config
-from marin.execution.executor import (
-    ExecutorStep,
-    executor_main,
-)
+from marin.execution.executor import executor_main
 
 logger = logging.getLogger("ray")
+step_name = "fineweb-small-resiliparse-custom-fork"
 
-
-def create_steps() -> list[ExecutorStep]:
-    step_name = "fineweb-small-resiliparse-custom-fork"
-
-    fw_tokenized = default_tokenize(
-        name=step_name,
-        dataset="gs://marin-us-central2/documents/fineweb-small-resiliparse-custom-fork-ca2156/md/CC-MAIN-2024-18",
-        tokenizer=llama3_tokenizer,
-    )
-    fw_100b_model = default_train(
-        name=f"{step_name}-1.4b",
-        tokenized=fw_tokenized,
-        model_config=llama_1_4b,
-        train_config=llama_1_4b_train_config,
-    )
-
-    return [
-        fw_tokenized,
-        fw_100b_model,
-    ]
+fineweb_resiliparse_custom_fork_tokenized = default_tokenize(
+    name=step_name,
+    dataset="gs://marin-us-central2/documents/fineweb-small-resiliparse-custom-fork-ca2156/md/CC-MAIN-2024-18",
+    tokenizer=llama3_tokenizer,
+)
+fineweb_resiliparse_custom_fork_1_4b_model = default_train(
+    name=f"{step_name}-1.4b",
+    tokenized=fineweb_resiliparse_custom_fork_tokenized,
+    model_config=llama_1_4b,
+    train_config=llama_1_4b_train_config,
+)
 
 
 if __name__ == "__main__":
-    executor_main(steps=create_steps())
+    executor_main(
+        steps=[
+            fineweb_resiliparse_custom_fork_tokenized,
+            fineweb_resiliparse_custom_fork_1_4b_model,
+        ]
+    )
