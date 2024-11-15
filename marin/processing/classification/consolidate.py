7ed410b2e9f0b26b455bcf18069ffb48d6b35ef6
Christopher Chou <emu4yolife@gmail.com>
2024-11-15 21:48:41 +0000

    Refactor consolidate

--- marin/processing/classification/consolidate.py
+++ marin/processing/classification/consolidate.py
@@ -7,16 +7,17 @@ the attributes.  Handles two cases:
 - Deduplication produces attributes (e.g., duplicate_text).  Remove duplicates.
 """
 
-import json
 import logging
 import os
 from dataclasses import dataclass, replace
 from typing import Any
 
 import draccus
-import fsspec
+import numpy as np
 import ray
 
+from marin.core.runtime import cached_or_construct_output
+from marin.processing.classification.inference import read_dataset, write_dataset
 from marin.utils import (
     fsspec_exists,
     fsspec_glob,
@@ -48,6 +49,9 @@ class FilterConfig:
     threshold: float | None = 0.5
     """Keep documents where the value is above this."""
 
+    keep_fraction: float | None = None
+    """Keep documents where the score is in the top percentile. Calculates the threshold from the entire dataset."""
+
 
 @dataclass(frozen=True)
 class ConsolidateConfig:
@@ -62,7 +66,13 @@ class ConsolidateConfig:
     filters: list[FilterConfig]
     """List of filters to apply to the documents."""
 
+    filetype: str = "jsonl.gz"
+    """The filetype of the input data."""
+
     max_tasks_in_flight: int = 1000  # The maximum number of flights in a task
+
+    memory_limit_gb: float = 0.5
+    """The memory limit for the task in GB."""
 
 
 def remove_spans(text: str, spans: list[list[int]]) -> str:
@@ -80,12 +90,26 @@ def remove_spans(text: str, spans: list[list[int]]) -> str:
     return text
 
 
-def apply_filter(input_data: dict, doc_filter: FilterConfig, attributes: dict[str, Any]) -> dict | None:
+def apply_filter(input_data: dict, doc_filter: FilterConfig, id_to_attributes: dict[str, Any]) -> bool | dict:
+    """Apply a filter to a single document
+
+    Currently, we support two types of filters:
+    - classify: filter based on a classification score. This is a filter-based operation where we return
+    True if the document should be kept and False otherwise.
+    - remove_spans: remove spans from the text - used in deduplication. This is a map-based operation where
+    we return a new document with the spans removed. However, if we don't want to keep the document,
+    we set the "keep" key to False. If we want to keep the document, we set the "keep" key to True.
+    """
+    attributes = id_to_attributes[input_data["id"]]
     if doc_filter.type == FILTER_TYPE_CLASSIFY:
         # Check attribute >= threshold?
         scores = attributes[doc_filter.name]
         score = scores[doc_filter.label]
-        return input_data if score >= doc_filter.threshold else None
+
+        if score >= doc_filter.threshold:
+            return True
+
+        return False
 
     elif doc_filter.type == FILTER_TYPE_REMOVE_SPANS:
 
@@ -95,16 +119,47 @@ def apply_filter(input_data: dict, doc_filter: FilterConfig, attributes: dict[st
         # if the deduped text doesn't have actual content, we can skip this document
         # this is to avoid cases where there are just newlines or spaces
         if new_text.strip() == "":
-            return None
+            return dict(input_data, keep=False)
 
-        return dict(input_data, text=new_text)
+        return dict(input_data, text=new_text, keep=True)
 
     else:
         raise ValueError(f"Unknown filter type: {doc_filter.type}")
 
 
+def apply_filter_batch_func(doc_filter, id_to_attributes):
+    # The huggingface dataset map API passes a batch of data to the function and returns a batch of data.
+    # We have additional data to pass in such as doc_filter and id_to_attributes, so we define a wrapper
+    # function here.
+    def apply_filter_func(batch):
+        return apply_filter(batch, doc_filter, id_to_attributes)
+
+    return apply_filter_func
+
+
+def read_attributes_as_dict(attribute_filename: str) -> dict[str, Any]:
+    """Given some attribute filename, return a dictionary mapping from id to attributes
+
+    Inputs:
+        attribute_filename: str
+            The path to the attribute file
+        filetype: str
+            The filetype of the attribute file
+    """
+    table = read_dataset(attribute_filename)
+    data = {}
+    for row_id, attr in zip(table["id"], table["attributes"], strict=True):
+        data[row_id] = attr
+    return data
+
+
 @ray.remote
-def process_file(input_path: str, filters: list[FilterConfig], output_path: str):
+@cached_or_construct_output(success_suffix="SUCCESS")
+def process_file(
+    input_path: str,
+    output_path: str,
+    filters: list[FilterConfig],
+):
     """
     Read documents from `input_path`, apply the `filters` (involves reading the
     attributes paths) and writes the subset of documents to `output_path`.
@@ -113,58 +168,86 @@ def process_file(input_path: str, filters: list[FilterConfig], output_path: str)
     logger.info(f"Processing {input_path} and {[doc_filter.attribute_path for doc_filter in filters]}")
 
     # Open all files simultaneously, and read in parallel.
-    input_file = fsspec.open(input_path, "rt", compression="gzip").open()
     attribute_files = []
     for doc_filter in filters:
         if fsspec_exists(doc_filter.attribute_path):
-            attribute_files.append(fsspec.open(doc_filter.attribute_path, compression="gzip").open())
+            attribute_files.append(read_attributes_as_dict(doc_filter.attribute_path))
         else:
             logger.warning(f"Attribute file not found: {doc_filter.attribute_path}")
             attribute_files.append(None)
-    output_file = fsspec.open(output_path, "wt", compression="gzip").open()
 
-    num_kept = 0
-    num_total = 0
-    for input_line in input_file:
-        num_total += 1
+    dataset = read_dataset(input_path)
 
-        # Read document and attributes for that document
-        input_data = json.loads(input_line)
-        all_attributes = [json.loads(attr_file.readline()) if attr_file else None for attr_file in attribute_files]
+    total_examples = len(dataset)
 
-        # Apply filters
-        for doc_filter, attributes in zip(filters, all_attributes, strict=True):
-            if attributes is None:
-                continue
-            try:
-                assert attributes["id"] == input_data["id"]
-                input_data = apply_filter(input_data, doc_filter, attributes["attributes"])
-            except Exception as e:
-                logger.error(f"Error applying filter {doc_filter} to line {num_total}: {e}")
-                input_data = None  # Skip this example
+    for doc_filter, id_to_attributes in zip(filters, attribute_files, strict=True):
+        print(f"Applying filter {doc_filter.name} with label {doc_filter.label} with dataset length {len(dataset)}")
+        if id_to_attributes is None:
+            continue
 
-            if input_data is None:
-                break
+        apply_filter_func = apply_filter_batch_func(doc_filter, id_to_attributes)
 
-        # Write output
-        if input_data is not None:
-            num_kept += 1
-            print(json.dumps(input_data), file=output_file)
+        if doc_filter.type == FILTER_TYPE_CLASSIFY:
+            dataset = dataset.filter(apply_filter_func)
+        elif doc_filter.type == FILTER_TYPE_REMOVE_SPANS:
+            dataset = dataset.map(apply_filter_func)
+        else:
+            raise ValueError(f"Unknown filter type: {doc_filter.type}")
 
-    logger.info(f"Kept {num_kept}/{num_total} from {input_path}")
+    # In the previous map step, we set the "keep" key for documents that should be removed
+    dataset = dataset.filter(lambda x: x["keep"])
 
-    # Close all files
-    input_file.close()
-    for attr_file in attribute_files:
-        if attr_file:
-            attr_file.close()
-    output_file.close()
+    write_dataset(dataset, output_path)
+
+    total_kept = len(dataset)
+
+    logger.info(f"Kept {total_kept}/{total_examples} from {input_path}")
+
+
+@ray.remote
+def get_scores(attribute_filename: str, attribute_name: str, label: str) -> list[float]:
+    scores = []
+    attributes = read_attributes_as_dict(attribute_filename)
+    for _, attr in attributes.items():
+        scores.append(attr[attribute_name][label])
+    return scores
+
+
+def calculate_percentile_threshold(scores: list[list[float]], keep_fraction: float) -> float:
+    percentile = 100 - keep_fraction * 100
+    scores = np.concatenate(scores)
+    return np.percentile(scores, percentile)
 
 
 @ray.remote
 def consolidate(config: ConsolidateConfig):
-    input_paths = fsspec_glob(os.path.join(config.input_path, "**/*.jsonl.gz"))
+    input_paths = fsspec_glob(os.path.join(config.input_path, f"**/*.{config.filetype}"))
     logger.info(f"Consolidating {len(input_paths)} documents")
+
+    updated_filters = []
+    for doc_filter in config.filters:
+        assert (
+            doc_filter.keep_fraction is None or doc_filter.threshold is None
+        ), "Cannot specify both percentile threshold and threshold. Please specify only one."
+
+        if doc_filter.keep_fraction is not None:
+            assert doc_filter.keep_fraction > 0 and doc_filter.keep_fraction < 1, "Keep fraction must be between 0 and 1"
+
+        # Calculate the minimum threshold required to keep `keep_fraction` of the documents
+        if doc_filter.keep_fraction is not None and doc_filter.type == FILTER_TYPE_CLASSIFY:
+            attribute_paths = [
+                rebase_file_path(config.input_path, input_path, doc_filter.attribute_path) for input_path in input_paths
+            ]
+            scores = ray.get(
+                [
+                    get_scores.remote(attribute_path, doc_filter.name, doc_filter.label)
+                    for attribute_path in attribute_paths
+                ]
+            )
+            threshold = calculate_percentile_threshold(scores, doc_filter.keep_fraction)
+            updated_filters.append(replace(doc_filter, threshold=threshold, keep_fraction=None))
+        else:
+            updated_filters.append(doc_filter)
 
     tasks = []
     ready_refs = []
@@ -177,11 +260,13 @@ def consolidate(config: ConsolidateConfig):
             replace(
                 doc_filter, attribute_path=rebase_file_path(config.input_path, input_path, doc_filter.attribute_path)
             )
-            for doc_filter in config.filters
+            for doc_filter in updated_filters
         ]
         output_path = rebase_file_path(config.input_path, input_path, config.output_path)
 
-        task = process_file.remote(input_path, filters, output_path)
+        task = process_file.options(memory=config.memory_limit_gb * 1024 * 1024 * 1024, num_cpus=2).remote(
+            input_path, output_path, filters
+        )
         tasks.append(task)
 
     try:
