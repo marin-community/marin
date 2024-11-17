84bd246cc641b7c3cff31b0e96ba7ef4d476f58c
Nikil Ravi <nravi@sc.stanford.edu>
2024-11-16 16:18:10 -0800

    add all DCLM evals to run_on_gpu.py

--- experiments/evals/run_on_gpu.py
+++ experiments/evals/run_on_gpu.py
@@ -20,10 +20,51 @@ exp433_dclm_1b_1x_eval = evaluate_lm_evaluation_harness(
     evals=["mmlu"],
 )
 
+exp433_dclm_1b_1x_eval_nov12 = evaluate_lm_evaluation_harness(
+    model_name="dclm_baseline_1b_1x_replication_nov12-b182e8",
+    model_path="gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12-b182e8/hf/step-54930",
+    evals=["mmlu"],
+)
+
+
+# gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_3404462497seed-b68241
+exp433_dclm_1b_1x_eval_nov12_3404462497seed = evaluate_lm_evaluation_harness(
+    model_name="dclm_baseline_1b_1x_replication_nov12_3404462497seed-b68241",
+    model_path="gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_3404462497seed-b68241/hf/step-54930",
+    evals=["mmlu"],
+)
+
+#gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_1639465881seed-c0c9ea
+exp433_dclm_1b_1x_eval_nov12_1639465881seed = evaluate_lm_evaluation_harness(
+    model_name="dclm_baseline_1b_1x_replication_nov12_1639465881seed-c0c9ea",
+    model_path="gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_1639465881seed-c0c9ea/hf/step-54930",
+    evals=["mmlu"],
+)
+
+#gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_3883584121seed-f9be46
+exp433_dclm_1b_1x_eval_nov12_3883584121seed = evaluate_lm_evaluation_harness(
+    model_name="dclm_baseline_1b_1x_replication_nov12_3883584121seed-f9be46",
+    model_path="gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_3883584121seed-f9be46/hf/step-54930",
+    evals=["mmlu"],
+)
+
+#gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_2940257722seed-b05777
+exp433_dclm_1b_1x_eval_nov12_2940257722seed = evaluate_lm_evaluation_harness(
+    model_name="dclm_baseline_1b_1x_replication_nov12_2940257722seed-b05777",
+    model_path="gs://marin-us-central2/checkpoints/dclm_baseline_1b_1x_replication_nov12_2940257722seed-b05777/hf/step-54930",
+    evals=["mmlu"],
+)
+
 steps = [
     quickstart_eval_step,
     exp433_dclm_1b_1x_eval,
+    exp433_dclm_1b_1x_eval_nov12,
+    exp433_dclm_1b_1x_eval_nov12_3404462497seed,
+    exp433_dclm_1b_1x_eval_nov12_1639465881seed,
+    exp433_dclm_1b_1x_eval_nov12_3883584121seed,
+    exp433_dclm_1b_1x_eval_nov12_2940257722seed,
 ]
 
 if __name__ == "__main__":
     executor_main(executor_main_config, steps=steps)
+
