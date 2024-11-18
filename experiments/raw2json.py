92b9173b9792d9478d20fd40cacebf523903ff84
Kamyar Salahi <kam.salahi@berkeley.edu>
2024-11-17 23:01:53 -0800

    Using download_hf

--- experiments/raw2json.py
+++ experiments/raw2json.py
@@ -1,5 +1,6 @@
 from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
-from operations.download.huggingface.download import DownloadConfig, download
+from operations.download.huggingface.download import DownloadConfig
+from operations.download.huggingface.download_hf import download_hf
 from operations.raw2json.huggingface.qa.raw2json import DatasetConversionConfig, OutputFormatOptions, raw2json
 
 """
@@ -10,7 +11,7 @@ Downloads the following datasets
 # download mmlu dataset
 mmlu_download_step = ExecutorStep(
     name="raw/cais/mmlu",
-    fn=download,
+    fn=download_hf,
     config=DownloadConfig(
         hf_dataset_id="cais/mmlu",
         revision=versioned("c30699e"),
@@ -18,7 +19,7 @@ mmlu_download_step = ExecutorStep(
         wait_for_completion=True,
     ),
     override_output_path="gs://marin-us-central2/raw/cais/mmlu",
-).cd("c30699e/huggingface.co/datasets/cais/mmlu/resolve/c30699e")
+)
 
 """
 Converts raw to JSON for:
