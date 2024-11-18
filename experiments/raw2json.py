8300df1b12d647098e3e1f778b25c70712c406c1
Kamyar Salahi <kam.salahi@berkeley.edu>
2024-11-18 14:51:49 -0800

    Fixing arc

--- experiments/raw2json.py
+++ experiments/raw2json.py
@@ -18,10 +18,10 @@ mmlu_download_step = ExecutorStep(
         revision=versioned("c30699e"),
         gcs_output_path=this_output_path(),
         wait_for_completion=True,
-        hf_url_glob="**/*.parquet",
+        hf_urls_glob=["**/*.parquet"],
     ),
     override_output_path="gs://marin-us-central2/raw/cais/mmlu",
-)
+).cd("c30699e")
 
 ############################################################
 # download arc dataset
@@ -33,10 +33,10 @@ arc_download_step = ExecutorStep(
         revision=versioned("210d026"),
         gcs_output_path=this_output_path(),
         wait_for_completion=True,
-        hf_url_glob="**/*.parquet",
+        hf_urls_glob=["**/*.parquet"],
     ),
     override_output_path="gs://marin-us-central2/raw/allenai/ai2_arc",
-)
+).cd("210d026")
 
 """
 Converts raw to JSON for:
