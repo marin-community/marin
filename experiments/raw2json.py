ddb6f8ef82cae68fc8ad2f14f02af194c22944f3
Kamyar Salahi <kam.salahi@berkeley.edu>
2024-11-18 12:52:10 -0800

    Switching to download_hf

--- experiments/raw2json.py
+++ experiments/raw2json.py
@@ -1,5 +1,6 @@
 from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
-from operations.download.huggingface.download import DownloadConfig, download
+from operations.download.huggingface.download import DownloadConfig
+from operations.download.huggingface.download_hf import download_hf
 from operations.raw2json.huggingface.qa.raw2json import DatasetConversionConfig, OutputFormatOptions, raw2json
 
 """
@@ -11,28 +12,30 @@ Downloads the following datasets
 # download mmlu dataset
 mmlu_download_step = ExecutorStep(
     name="raw/cais/mmlu",
-    fn=download,
+    fn=download_hf,
     config=DownloadConfig(
         hf_dataset_id="cais/mmlu",
         revision=versioned("c30699e"),
         gcs_output_path=this_output_path(),
         wait_for_completion=True,
+        hf_url_glob="**/*.parquet",
     ),
     override_output_path="gs://marin-us-central2/raw/cais/mmlu",
-).cd("c30699e/huggingface.co/datasets/cais/mmlu/resolve/c30699e")
+)
 
 # download boolq dataset
 boolq_download_step = ExecutorStep(
     name="raw/google/boolq",
-    fn=download,
+    fn=download_hf,
     config=DownloadConfig(
         hf_dataset_id="google/boolq",
         revision=versioned("35b264d"),
         gcs_output_path=this_output_path(),
         wait_for_completion=True,
+        hf_url_glob="**/*.parquet",
     ),
     override_output_path="gs://marin-us-central2/raw/google/boolq",
-).cd("35b264d/huggingface.co/datasets/google/boolq/resolve/35b264d")
+)
 
 
 """
