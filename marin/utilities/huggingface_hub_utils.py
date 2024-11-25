8a2bfe62d16af98b01aed980b27063dfbbfb5332
David Hall <dlwh@stanford.edu>
2024-11-25 14:52:05 -0800

    fix huggingface_hub_utils.py for new globbing

--- marin/utilities/huggingface_hub_utils.py
+++ marin/utilities/huggingface_hub_utils.py
@@ -4,6 +4,8 @@ huggingface_hub_utils.py
 Helpful functions for facilitating downloads/verification of datasets/artifacts hosted on the HuggingFace Hub.
 """
 
+import os
+
 import fsspec
 from huggingface_hub import hf_hub_url
 from huggingface_hub.utils import GatedRepoError
@@ -13,9 +15,11 @@ from marin.utilities.storage_transfer_utils import create_gcs_transfer_job_from_
 from marin.utilities.validation_utils import write_provenance_json
 
 
-def get_hf_dataset_urls(hf_dataset_id: str, revision: str, hf_url_glob: str) -> list[str]:
+def get_hf_dataset_urls(hf_dataset_id: str, revision: str, hf_url_globs: list[str]) -> list[str]:
     """Walk through Dataset Repo using the `hf://` fsspec built-ins."""
-    fs = fsspec.filesystem("hf")
+    # get the token from the environment
+    hf_token = os.environ.get("HF_TOKEN")
+    fs = fsspec.filesystem("hf", token=hf_token)
 
     # Check if Dataset is Public or Gated
     try:
@@ -24,16 +28,19 @@ def get_hf_dataset_urls(hf_dataset_id: str, revision: str, hf_url_glob: str) -> 
         raise NotImplementedError(f"Unable to automatically download gated dataset `{hf_dataset_id}`") from err
 
     try:
-        url_list = []
         base_dir = f"hf://datasets/{hf_dataset_id}"
-        for fpath in fs.find(base_dir, revision=revision):
-            if ".git" in fpath:
-                continue
+        if not hf_url_globs:
+            # We get all the files using find
+            files = fs.find(base_dir, revision=revision)
+        else:
+            files = []
+            # Get list of files directly from HfFileSystem matching the pattern
+            for hf_url_glob in hf_url_globs:
+                pattern = os.path.join(base_dir, hf_url_glob)
+                files += fs.glob(pattern, revision=revision)
 
-            # Continue if the file does not match the hf_url_glob pattern
-            if not fs.glob(f"{base_dir}/{hf_url_glob}"):
-                continue
-
+        url_list = []
+        for fpath in files:
             # Resolve to HF Path =>> grab URL
             resolved_fpath = fs.resolve_path(fpath)
             url_list.append(
@@ -45,16 +52,16 @@ def get_hf_dataset_urls(hf_dataset_id: str, revision: str, hf_url_glob: str) -> 
                 )
             )
     except Exception as err:
-        raise ValueError(f"Unable to download dataset `{hf_dataset_id}`. Problem with glob {hf_url_glob}") from err
+        raise ValueError(f"Unable to download dataset `{hf_dataset_id}`") from err
 
     return url_list
 
 
 def download_hf_dataset(
-    hf_dataset_id: str, revision: str, hf_url_glob: str, gcs_output_path: str, public_gcs_path: str
+    hf_dataset_id: str, revision: str, hf_url_globs: list[str], gcs_output_path: str, public_gcs_path: str
 ) -> tuple[str, str]:
     """Create & Launch a Google Cloud Storage Transfer Job to Download a (Public) HuggingFace Dataset."""
-    hf_urls = get_hf_dataset_urls(hf_dataset_id, revision, hf_url_glob)
+    hf_urls = get_hf_dataset_urls(hf_dataset_id, revision, hf_url_globs)
 
     # Parse GCS Bucket, Relative Path from `gcs_output_path`
     gcs_bucket, gcs_relative_path = split_gcs_path(gcs_output_path)
