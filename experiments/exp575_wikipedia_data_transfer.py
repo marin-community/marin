e99c499f059f9854b8fd62d29e6c5aeb48b9b6e3
Herumb Shandilya <herumbshandilya123@gmail.com>
2024-12-03 13:58:12 -0800

    add script to process wiki dump

--- experiments/exp575_wikipedia_data_transfer.py
+++ experiments/exp575_wikipedia_data_transfer.py
@@ -1,16 +1,42 @@
 from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
+from marin.schemas.web.convert import ResiliparseConfig
 from operations.download.wikipedia.download import DownloadConfig, download
+from operations.download.wikipedia.extract_html import WikiExtractionConfig, process_wiki_dump
 
-raw_wikipedia_dump = ExecutorStep(
+wikipedia_dump_raw = ExecutorStep(
     name="raw/wikipedia",
     fn=download,
     config=DownloadConfig(
-        input_path=versioned(
-            "https://dumps.wikimedia.org/enwiki/20241101/enwiki-20241101-pages-articles-multistream.xml.bz2"
+        input_urls=versioned(
+            [
+                "https://dumps.wikimedia.org/other/enterprise_html/runs/20241201/enwiki-NS0-20241201-ENTERPRISE-HTML.json.tar.gz",
+            ]
         ),
-        output_path=this_output_path("enwiki-latest-pages-articles.xml"),
+        revision="20241201",
+        output_path=this_output_path(),
+    ),
+)
+
+wikipedia_text_extracted = ExecutorStep(
+    name="documents/wikipedia-resiliparse-with-preserving-formatting",
+    fn=process_wiki_dump,
+    config=WikiExtractionConfig(
+        input_path=versioned("gs://marin-us-central2/raw/wikipedia/v2024-05-01"),
+        revision="20241201",
+        output_path=this_output_path(),
+        extract_method="resiliparse",
+        extract_config=ResiliparseConfig(
+            preserve_formatting=versioned(True),
+            main_content=versioned(True),
+            links=versioned(True),
+        ),
     ),
 )
 
 if __name__ == "__main__":
-    executor_main(steps=[raw_wikipedia_dump])
+    executor_main(
+        steps=[
+            # wikipedia_dump_raw,
+            wikipedia_text_extracted,
+        ]
+    )
