27bb97321306e34b293db73f5f9cb6e6f619b194
Abhinav Garg <abhinavg@stanford.edu>
2024-12-04 22:30:44 -0800

    PR changes

--- experiments/metrics/calculate_cumulative_metric.py
+++ experiments/metrics/calculate_cumulative_metric.py
@@ -1,58 +1,40 @@
 import os
 from datetime import datetime, timedelta
 
-from experiments.metrics.gcp_related import NUM_RESTART_CONFIG, get_number_of_restarts
+from experiments.metrics.gcp_related import NumRestartConfig, get_gcp_restart_events
 from experiments.metrics.github_related import (
-    GITHUB_API_CONFIG,
-    GITHUB_ISSUE_CONFIG,
+    GithubApiConfig,
+    GithubIssueConfig,
     get_average_duration_for_all_workflows,
     get_closed_issues_with_label,
 )
-from marin.execution.executor import ExecutorMainConfig, ExecutorStep, executor_main, output_path_of, this_output_path
-from marin.utilities.metrics_utils import MergeConfig, merge
 
-average_duration = ExecutorStep(
-    name=os.path.join("metrics", "github", "average_duration"),
-    fn=get_average_duration_for_all_workflows,
-    config=GITHUB_API_CONFIG(
-        GITHUB_TOKEN=os.getenv("GITHUB_TOKEN"),
-        output_path=this_output_path(),
-        TIME_SINCE=(datetime.now() - timedelta(days=7)).isoformat(),
-    ),
-)
 
-closed_issues = ExecutorStep(
-    name=os.path.join("metrics", "github", "closed_issues"),
-    fn=get_closed_issues_with_label,
-    config=GITHUB_ISSUE_CONFIG(
-        GITHUB_TOKEN=os.getenv("GITHUB_TOKEN"),
-        output_path=this_output_path(),
-        TIME_SINCE=(datetime.now() - timedelta(days=7)).isoformat(),
-        LABEL="experiments",
-    ),
-)
+def main():
 
-number_of_restarts = ExecutorStep(
-    name=os.path.join("metrics", "gcp", "number_of_restarts"),
-    fn=get_number_of_restarts,
-    config=NUM_RESTART_CONFIG(output_path=this_output_path()),
-    pip_dependency_groups=["google-cloud-logging"],
-)
-
-merge = ExecutorStep(
-    name=os.path.join("metrics", "merge"),
-    fn=merge,
-    config=MergeConfig(
-        output_path=this_output_path(),
-        merge_paths=[
-            output_path_of(average_duration),
-            output_path_of(closed_issues),
-            output_path_of(number_of_restarts),
-        ],
-    ),
-)
-if __name__ == "__main__":
-    executor_main(
-        ExecutorMainConfig(force_run=[closed_issues.name, number_of_restarts.name, merge.name, average_duration.name]),
-        [average_duration, closed_issues, number_of_restarts, merge],
+    final_metrics = {}
+    final_metrics["Workflow Times per workflow"] = get_average_duration_for_all_workflows(
+        GithubApiConfig(
+            github_token=os.getenv("GITHUB_TOKEN"), time_since=(datetime.now() - timedelta(days=7)).isoformat()
+        )
     )
+
+    label = "experiments"
+    final_metrics[f"Closed Issues with label {label}"] = get_closed_issues_with_label(
+        GithubIssueConfig(
+            github_token=os.getenv("GITHUB_TOKEN"),
+            time_since=(datetime.now() - timedelta(days=7)).isoformat(),
+            label=label,
+        ),
+    )
+
+    events = get_gcp_restart_events(
+        NumRestartConfig(time_since=((datetime.now() - timedelta(days=7)).isoformat("T") + "Z"))
+    )
+    final_metrics["Number of Ray cluster restart"] = len(events)
+    final_metrics["Ray restart events"] = events
+    return final_metrics
+
+
+if __name__ == "__main__":
+    print(main())
