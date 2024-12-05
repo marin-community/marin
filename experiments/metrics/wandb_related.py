d4cfb4da4a312c6a0cf44c9550f2b9e1e173a0b1
Nikil Ravi <nravi@stanford.edu>
2024-12-04 20:15:46 -0800

    address comments

--- experiments/metrics/wandb_related.py
+++ experiments/metrics/wandb_related.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
@@ -6,6 +7,8 @@ from typing import Any
 
 import fsspec
 import wandb
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -47,19 +50,19 @@ def get_wandb_run_metrics(
             # Retrieve the metric value for the run
             value = run.summary.get(metric, None)
             if value is None:
-                print(f"Metric '{metric}' not found for run {run_id}")
+                logger.info(f"Metric '{metric}' not found for run {run_id}")
             metrics_dict[metric] = value
 
-        print("Run metrics: ", metrics_dict)
+        logger.info("Run metrics: ", metrics_dict)
 
         return metrics_dict
 
     except wandb.errors.CommError as e:
-        print("Failed to retrieve run data:", e)
+        logger.error("Failed to retrieve run data:", e)
         return None
 
     except Exception as e:
-        print(f"An unexpected error occurred when trying to retrieve run data: {e}")
+        logger.error(f"An unexpected error occurred when trying to retrieve run data: {e}")
         return None
 
 
@@ -82,27 +85,25 @@ def get_all_runs_over_period(num_days=7, entity="stanford-mercury", project="mar
         try:
             api.project(entity, project)
         except wandb.errors.CommError:
-            print(f"Project '{project}' not found in entity '{entity}'")
+            logger.error(f"Project '{project}' not found in entity '{entity}'")
             return None
 
         # Fetch all runs for the project
         runs = api.runs(f"{entity}/{project}")
 
-        print(runs)
-
         # Filter runs by creation date
         runs_list = [run for run in runs if datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S%z") >= time_window]
 
-        print(f"Successfully retrieved {len(runs_list)} runs from the past {num_days} days")
+        logger.info(f"Successfully retrieved {len(runs_list)} runs from the past {num_days} days")
 
         return runs_list
 
     except wandb.errors.CommError as e:
-        print("Failed to retrieve run data:", e)
+        logger.error("Failed to retrieve run data:", e)
         return None
 
     except Exception as e:
-        print(f"An unexpected error occurred when trying to get runs from WandB: {e}")
+        logger.error(f"An unexpected error occurred when trying to get runs from WandB: {e}")
         return None
 
 
@@ -111,6 +112,7 @@ def count_params_for_run(run_id: str, entity="stanford-mercury", project="marin"
     Retrieves the number of parameters for a specific WandB run.
     """
     from experiments.llama import LlamaConfig, compute_num_parameters
+    from marin.processing.tokenize.utils import calculate_vocab_size
 
     # Initialize the WandB API
     api = wandb.Api()
@@ -121,24 +123,11 @@ def count_params_for_run(run_id: str, entity="stanford-mercury", project="marin"
 
         assert run is not None, f"Run {run_id} not found."
 
-        # Calculate the number of parameters for the run
-        # TODO: actually can this just be replaced by getting the parameter_count from the run?
-        # there seems be some discrepancy in the parameter count between the two methods
-        tokenizer = run.config.get("data", {}).get("tokenizer")
-        print("Tokenizer: ", tokenizer)
-        if tokenizer == "EleutherAI/gpt-neox-20b":
-            vocab_size = 50_257
-        elif tokenizer == "meta-llama/Meta-Llama-3.1-8B":
-            vocab_size = 128_256
-        elif tokenizer == "meta-llama/Llama-2-7b":
-            vocab_size = 32_000
-        elif tokenizer == "gpt2":
-            vocab_size == 50_257
-
-        print("Vocab size: ", vocab_size)
-
+        tokenizer = run.config.get("data", {}).get("tokenizer", None)
+        vocab_size = calculate_vocab_size(tokenizer)
         model_dict = run.config.get("model", {})
-        print("Model dict: ", model_dict)
+        logger.info("Model dict: ", model_dict)
+
         llama_config = LlamaConfig(
             hidden_dim=model_dict.get("hidden_dim"),
             num_heads=model_dict.get("num_heads"),
@@ -147,17 +136,20 @@ def count_params_for_run(run_id: str, entity="stanford-mercury", project="marin"
             num_layers=model_dict.get("num_layers"),
         )
 
+        # Calculate the number of parameters for the run
+        # TODO: actually can this just be replaced by getting the parameter_count from the run?
+        # there seems be some discrepancy in the parameter count between the two methods
         num_parameters = compute_num_parameters(llama_config, vocab_size=vocab_size)
-        print(f"Number of parameters for run {run_id}: {num_parameters}\n")
+        logger.info(f"Number of parameters for run {run_id}: {num_parameters}\n")
 
         return num_parameters
 
     except wandb.errors.CommError as e:
-        print("Failed to retrieve run data:", e)
+        logger.error("Failed to retrieve run data:", e)
         return None
 
     except Exception as e:
-        print(f"An unexpected error occurred when trying to retrieve run data: {e}")
+        logger.error(f"An unexpected error occurred when trying to retrieve run data: {e}")
         return None
 
 
@@ -181,38 +173,40 @@ def calculate_wandb_metrics(config: WANDB_METRICS_CONFIG) -> tuple[dict[str, Any
         # get parameter count for the run and add to metrics
         run_metrics[run_id]["num_parameters"] = count_params_for_run(run_id)
 
+    # Define parameter scale thresholds (exclusive upper bounds)
+    parameter_scales = {
+        "<1B": 1_000_000_000,  # for runs in the millions of params (300M, etc.)
+        "<2B": 2_000_000_000,  # for 1B scale models or smaller
+        "<9B": 9_000_000_000,  # for 7 or 8B scale models or smaller
+        "<23B": 23_000_000_000,  # for 22B scale models or smaller
+    }
+
+    # track best BPB for each scale
+    best_bpb_per_scale = {scale: {"bpb": None, "run_id": None} for scale in parameter_scales}
+
     # get the model with best bpb eval for each group in 1b parameter scale
-    best_bpb_1b, best_bpb_7b = None, None
-    best_bpb1b_run_id, best_bpb7b_run_id = None, None
+    # best_bpb_1b, best_bpb_7b = None, None
+    # best_bpb1b_run_id, best_bpb7b_run_id = None, None
     for run_id, metrics in run_metrics.items():
         if metrics["eval/paloma/c4_en/bpb"] is not None:
             num_parameters = metrics["num_parameters"]
-
-            # if num_parameters is below 2 billion and bpb is better than current best, update best_bpb
-            if num_parameters < 2_000_000_000 and (
-                best_bpb_1b is None or metrics["eval/paloma/c4_en/bpb"] < best_bpb_1b
-            ):
-                best_bpb_1b = metrics["eval/paloma/c4_en/bpb"]
-                best_bpb1b_run_id = run_id
-
-            # if num_parameters is above 6 billion and bpb is better than current best, update best_bpb
-            if num_parameters > 6_000_000_000 and (
-                best_bpb_7b is None or metrics["eval/paloma/c4_en/bpb"] < best_bpb_7b
-            ):
-                best_bpb_7b = metrics["eval/paloma/c4_en/bpb"]
-                best_bpb7b_run_id = run_id
+            for scale_label, threshold in parameter_scales.items():
+                if num_parameters < threshold:
+                    current_best_bpb = best_bpb_per_scale[scale_label]["bpb"]
+                    if current_best_bpb is None or metrics["eval/paloma/c4_en/bpb"] < current_best_bpb:
+                        best_bpb_per_scale[scale_label] = {
+                            "bpb": metrics["eval/paloma/c4_en/bpb"],
+                            "run_id": run_id,
+                        }
 
     metrics = {
         "num_runs": len(run_metrics),
         "best_c4_en_bpb": {
-            "1b": {
-                "run_id": best_bpb1b_run_id,
-                "run_metrics": run_metrics[best_bpb1b_run_id] if best_bpb1b_run_id else None,
-            },
-            "7b": {
-                "run_id": best_bpb7b_run_id,
-                "run_metrics": run_metrics[best_bpb7b_run_id] if best_bpb7b_run_id else None,
-            },
+            scale: {
+                "run_id": data["run_id"],
+                "run_metrics": run_metrics[data["run_id"]] if data["run_id"] else None,
+            }
+            for scale, data in best_bpb_per_scale.items()
         },
         "total_gflops_across_runs": sum(
             [
@@ -224,11 +218,11 @@ def calculate_wandb_metrics(config: WANDB_METRICS_CONFIG) -> tuple[dict[str, Any
     }
 
     with fsspec.open(os.path.join(config.output_path, "metric.json"), "w") as f:
-        print(json.dumps(metrics), file=f)
+        logger.info(json.dumps(metrics), file=f)
 
 
 if __name__ == "__main__":
     config = WANDB_METRICS_CONFIG(entity="stanford-mercury", project="marin", num_days=7, output_path=".")
-    #calculate_wandb_metrics(config)
+    # calculate_wandb_metrics(config)
 
     count_params_for_run(run_id="sweep474-150m-hs=128-ce=64000-bs=256-step=4000-lr=0.001-wd0.1-e08ba6")
