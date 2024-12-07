03aede73229cffc7527f0ed6b569fb28eea37867
Nikil Ravi <nravi@stanford.edu>
2024-12-06 20:40:29 -0800

    minor changes

--- experiments/evals/task_configs.py
+++ experiments/evals/task_configs.py
@@ -12,18 +12,18 @@ CORE_TASKS = [
     EvalTaskConfig("boolq", 10),  # answer yes/no questions based on a passage
     EvalTaskConfig("commonsense_qa", 10),  # 5-way multiple-choice questions based on common-sense, everyday scenarios
     EvalTaskConfig("copa", 0),  # use causal reasoning to predict the correct outcome of a given scenario
-    EvalTaskConfig("hellaswag", 0),  # 4-way multiple choice commonsense reasoning dataset
-    EvalTaskConfig("hellaswag", 10),  # 4-way multiple choice commonsense reasoning dataset
+    EvalTaskConfig("hellaswag", 0, task_alias="hellaswag_0shot"),  # 4-way multiple choice commonsense reasoning dataset
+    EvalTaskConfig("hellaswag", 10, task_alias="hellaswag_10shot"),  # 4-way MCQ commonsense reasoning dataset
     EvalTaskConfig("lambada", 0),  # predict the endings of text passages
     EvalTaskConfig("openbookqa", 0),  # 4-way multiple choice question answering task that requires multi-step reasoning
     EvalTaskConfig("piqa", 10),  # answer questions based on a passage
-    EvalTaskConfig("squadv2", 10),  # reading comprehension benchmark
+    # EvalTaskConfig("squadv2", 10),  # reading comprehension benchmark
     EvalTaskConfig("wsc273", 0),  # Winograd Schema Challenge
     EvalTaskConfig("winogrande", 0),  # Winograd challenge, extended to more domains
 ]
 
 
-CORE_TASKS = [
+LEVANTER_LM_EVAL_CORE_TASKS = [
     TaskConfig(task="agieval_lsat_ar", num_fewshot=3),  # 3-shot tests in legal domain
     TaskConfig(task="arc_easy", num_fewshot=10),  # 10-shot, four-way MCQs involving grade 3-9 basic science
     TaskConfig(task="arc_challenge", num_fewshot=10),  # a (harder) version of arc_easy
