a0f6445efcac32386737553c6cad0474feca82cd
Herumb Shandilya <herumbshandilya123@gmail.com>
2024-12-06 18:02:32 -0800

    final changes

--- experiments/exp579_ar5iv_data_transfer.py
+++ experiments/exp579_ar5iv_data_transfer.py
@@ -1,5 +1,7 @@
-from marin.execution.executor import ExecutorStep, executor_main, this_output_path
+from marin.execution.executor import ExecutorStep, executor_main, output_path_of, this_output_path, versioned
+from marin.schemas.web.convert import ResiliparseConfig
 from operations.download.ar5iv.download import DownloadConfig, download
+from operations.transform.ar5iv.transform_ar5iv import Ar5ivExtractionConfig, process_ar5iv_dump
 
 ar5iv_no_problem_raw = ExecutorStep(
     name="raw/ar5iv/ar5iv-04-2024-no-problem",
@@ -19,10 +21,46 @@ ar5iv_warnings_raw = ExecutorStep(
     ),
 )
 
+ar5iv_no_problem_extracted = ExecutorStep(
+    name="documents/ar5iv/ar5iv-04-2024-no-problem",
+    fn=process_ar5iv_dump,
+    config=Ar5ivExtractionConfig(
+        input_path=output_path_of(ar5iv_no_problem_raw),
+        revision="042024",
+        output_path=this_output_path("resiliparse-with-preserving-formatting"),
+        extract_method="resiliparse",
+        extract_config=ResiliparseConfig(
+            preserve_formatting=versioned(True),
+            main_content=versioned(True),
+            links=versioned(True),
+        ),
+    ),
+    pip_dependency_groups=["download_transform"],
+)
+
+ar5iv_warnings_extracted = ExecutorStep(
+    name="documents/ar5iv/ar5iv-04-2024-warning",
+    fn=process_ar5iv_dump,
+    config=Ar5ivExtractionConfig(
+        input_path=output_path_of(ar5iv_warnings_raw),
+        revision="042024",
+        output_path=this_output_path("resiliparse-with-preserving-formatting"),
+        extract_method="resiliparse",
+        extract_config=ResiliparseConfig(
+            preserve_formatting=versioned(True),
+            main_content=versioned(True),
+            links=versioned(True),
+        ),
+    ),
+    pip_dependency_groups=["download_transform"],
+)
+
 if __name__ == "__main__":
     executor_main(
         steps=[
             ar5iv_no_problem_raw,
             ar5iv_warnings_raw,
+            ar5iv_no_problem_extracted,
+            ar5iv_warnings_extracted,
         ]
     )
