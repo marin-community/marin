3c75e9ef1eeb3b5728bbae4f337c8dc5eabf5c47
Christopher Chou <emu4yolife@gmail.com>
2024-12-09 02:22:41 +0000

    Tokenize stackexchange

--- experiments/exp636_stackexchange_vs_hqwebpages.py
+++ experiments/exp636_stackexchange_vs_hqwebpages.py
@@ -1,21 +1,31 @@
 from experiments.defaults import default_tokenize, default_train
-from experiments.exp102_classifier_ablations import create_steps
 from experiments.exp596_stackexchange_classifier import stackexchange_experiment_config
 from experiments.llama import llama3_tokenizer, llama_1_4b, llama_1_4b_train_config
 from experiments.pretraining_datasets import dolmino
-from marin.execution.executor import executor_main
+from experiments.quality_classifier_experiment_utils import create_steps
+from marin.execution.executor import ExecutorStep, executor_main, this_output_path
 from marin.processing.tokenize.data_configs import lm_mixture_data_config
+from operations.transform.dolmino.filter_dolmino import FilterDolminoConfig, filter_dolmino
 
-dolmino_stackexchange = dolmino.cd("stackexchange")
+dolmino_stackexchange_jsonl = ExecutorStep(
+    name="documents/dolmino_stackexchange",
+    fn=filter_dolmino,
+    config=FilterDolminoConfig(
+        input_path=dolmino,
+        output_path=this_output_path(),
+        split="stackexchange",
+        min_length=0,
+    ),
+)
 
 dolmino_stackexchange_tokenized = default_tokenize(
-    name="quality_filtering/dolmino_stackexchange_model",
-    dataset=dolmino_stackexchange,
+    name="quality_filtering/dolmino_stackexchange",
+    dataset=dolmino_stackexchange_jsonl,
     tokenizer=llama3_tokenizer,
 )
 
 dolmino_stackexchange_model = default_train(
-    name="quality_filtering/dolmino_stackexchange_model",
+    name="quality_filtering/dolmino_stackexchange",
     tokenized=dolmino_stackexchange_tokenized,
     model_config=llama_1_4b,
     train_config=llama_1_4b_train_config,
@@ -42,7 +52,7 @@ data_config = lm_mixture_data_config(
 )
 
 half_stackexchange_half_hqwebpages_model = default_train(
-    name="quality_filtering/half_stackexchange_half_hqwebpages_model",
+    name="quality_filtering/half_stackexchange_half_hqwebpages",
     tokenized=data_config,
     model_config=llama_1_4b,
     train_config=llama_1_4b_train_config,
@@ -52,7 +62,7 @@ if __name__ == "__main__":
     executor_main(
         steps=[
             dolmino_stackexchange_model,
-            stackexchange_filtered_hq_webpages_model,
-            half_stackexchange_half_hqwebpages_model,
+            # stackexchange_filtered_hq_webpages_model,
+            # half_stackexchange_half_hqwebpages_model,
         ]
     )
