f2406eb1ca891bad43d4609da9fd629ac5f0c5f4
David Hall <dlwh@stanford.edu>
2024-12-09 22:57:58 -0800

    actually run pre-commit --all-files

--- scripts/ar5iv/transform.py
+++ scripts/ar5iv/transform.py
@@ -9,9 +9,10 @@ from marin import markdown
 from bs4 import BeautifulSoup
 import pathlib
 
+
 def remove_authors(html):
     # Remove authors since we only care about information after first section
-    authors = html.findAll('div', {'class': 'ltx_authors'})
+    authors = html.findAll("div", {"class": "ltx_authors"})
     for author in authors:
         author.decompose()
         section = author.previous_sibling
@@ -21,72 +22,81 @@ def remove_authors(html):
             section = new_section
     return html
 
+
 def remove_title_page(html):
     # Remove title page since we only care about information after first section
-    title_page = html.findAll('div', {'class': 'ltx_titlepage'})
+    title_page = html.findAll("div", {"class": "ltx_titlepage"})
     for tp in title_page:
         tp.decompose()
 
+
 def clean_li(html):
     # Remove the li tags since they repeat the same information (eg 1. 1.)
-    tags = html.findAll('span', {'class': 'ltx_tag_item'})
+    tags = html.findAll("span", {"class": "ltx_tag_item"})
     for tag in tags:
         tag.decompose()
-    tags = html.findAll('span', {'class': 'ltx_tag_listingline'})
+    tags = html.findAll("span", {"class": "ltx_tag_listingline"})
     for tag in tags:
         tag.decompose()
+
 
 def remove_biblio(html):
     # Remove the biblio since there is a lot of noise
-    biblio = html.findAll('section', {'id': 'bib'})
+    biblio = html.findAll("section", {"id": "bib"})
     for bib in biblio:
         bib.decompose()
 
+
 def remove_footnotes(html):
     # Remove footnotes since they are plopped in the middle of the text
-    footnotes = html.findAll('div', {'class': 'ltx_role_footnote'})
+    footnotes = html.findAll("div", {"class": "ltx_role_footnote"})
     for fn in footnotes:
         fn.decompose()
 
+
 def remove_biblinks(html):
     # Remove the biblinks since we are removing the biblio
-    biblinks = html.findAll('a', {'class': 'ltx_ref'})
+    biblinks = html.findAll("a", {"class": "ltx_ref"})
     for biblink in biblinks:
         # Removes reference links
         # biblink.decompose()
         # Removes linking but keeps text
         biblink.unwrap()
 
+
 def linelisting_to_newline(html):
     # Turn new line listings into new lines
-    linelisting = html.findAll('div', {'class': 'ltx_listingline'})
+    linelisting = html.findAll("div", {"class": "ltx_listingline"})
     for fn in linelisting:
         fn.append(BeautifulSoup("<br>", "html.parser"))
 
+
 def unwrap_eqn(html):
     # Unwrap equation tables to ensure math mode is not in a table
-    eqntables = html.findAll('table', {'class': 'ltx_eqn_table'})
+    eqntables = html.findAll("table", {"class": "ltx_eqn_table"})
     for eqn in eqntables:
         eqn.append(BeautifulSoup("<br>", "html.parser"))
         eqn.unwrap()
-    eqnrows = html.findAll('tr', {'class': 'ltx_eqn_row'})
+    eqnrows = html.findAll("tr", {"class": "ltx_eqn_row"})
     for eqn in eqnrows:
         eqn.append(BeautifulSoup("<br>", "html.parser"))
         eqn.unwrap()
-    
-    eqncell = html.findAll('td', {'class': 'ltx_eqn_cell'})
+
+    eqncell = html.findAll("td", {"class": "ltx_eqn_cell"})
     for eqn in eqncell:
         eqn.unwrap()
 
+
 def remove_ar5iv_footer(html):
     # This is the ar5iv footer generated on xyz date
-    footer = html.findAll('footer')
+    footer = html.findAll("footer")
     for fn in footer:
         fn.decompose()
 
+
 def remove_before_section(html):
     # We only care about information after the first section
-    section = html.find('section')
+    section = html.find("section")
     if section:
         section = section.previous_sibling
         while section:
@@ -94,11 +104,13 @@ def remove_before_section(html):
             section.extract()
             section = new_section
 
+
 def remove_title(html):
     # Title is added by markdown parser
-    title = html.find('title')
+    title = html.find("title")
     if title:
         title.decompose()
+
 
 def clean_html(html):
     if isinstance(html, str):
@@ -116,6 +128,7 @@ def clean_html(html):
     remove_title(html)
     return str(html)
 
+
 @ray.remote(memory=512 * 1024 * 1024)  # 512 MB
 def clean_ar5iv_html(file, prefix_path, output_path, file_size):
     """
@@ -127,19 +140,24 @@ def clean_ar5iv_html(file, prefix_path, output_path, file_size):
 
     try:
         outs = ""
-        with fsspec.open(file, 'rb', compression='gzip') as outputf:
+        with fsspec.open(file, "rb", compression="gzip") as outputf:
             for _ in range(file_size):
                 line = outputf.readline()
                 if not line:
                     break
                 html_blob = json.loads(line)
                 content = clean_html(html_blob["text"])
-                outs += json.dumps({
-                    "id": html_blob["id"],             # MANDATORY: source-specific identifier
-                    "text": content,           # MANDATORY: textual content of the document
-                    "source": "ar5iv",         # MANDATORY: source of the data, such as peS2o, common-crawl, etc.
-                    "added": datetime.datetime.now().isoformat(),          # OPTIONAL: timestamp ai2 acquired this data
-                }) + "\n"
+                outs += (
+                    json.dumps(
+                        {
+                            "id": html_blob["id"],  # MANDATORY: source-specific identifier
+                            "text": content,  # MANDATORY: textual content of the document
+                            "source": "ar5iv",  # MANDATORY: source of the data, such as peS2o, common-crawl, etc.
+                            "added": datetime.datetime.now().isoformat(),  # OPTIONAL: timestamp ai2 acquired this data
+                        }
+                    )
+                    + "\n"
+                )
         _file_path = pathlib.Path(file.replace("gs://", ""))
         _output_path = pathlib.Path(output_path.replace("gs://", ""))
         if _file_path.is_relative_to(prefix_path.replace("gs://", "")):
@@ -148,8 +166,8 @@ def clean_ar5iv_html(file, prefix_path, output_path, file_size):
                 out_file = "gs://" + str(out_file)
         else:
             raise Exception(f"File {file} is not in the prefix path {prefix_path}")
-        with fsspec.open(out_file, 'wb', compression='gzip') as outputf:
-            outputf.write(outs.encode('utf-8'))
+        with fsspec.open(out_file, "wb", compression="gzip") as outputf:
+            outputf.write(outs.encode("utf-8"))
         print(f"Wrote to file {out_file}")
     except FileNotFoundError as e:
         print(f"Error reading the zip file: {e}")
@@ -157,8 +175,9 @@ def clean_ar5iv_html(file, prefix_path, output_path, file_size):
 
     return True
 
+
 @ray.remote(memory=1024 * 1024 * 1024)  # 1 GB
-def markdownify_ar5iv_html(file,  prefix_path, output_path, file_size):
+def markdownify_ar5iv_html(file, prefix_path, output_path, file_size):
     """
     Takes in the input file and processes it to get the html content.
     Args:
@@ -169,7 +188,7 @@ def markdownify_ar5iv_html(file,  prefix_path, output_path, file_size):
     try:
         outs = ""
         print(f"Starting Processing for the ar5iv file: {file}")
-        with fsspec.open(file, 'rb', compression='gzip') as outputf:
+        with fsspec.open(file, "rb", compression="gzip") as outputf:
             for _ in range(file_size):
                 line = outputf.readline()
                 if not line:
@@ -185,12 +204,17 @@ def markdownify_ar5iv_html(file,  prefix_path, output_path, file_size):
                 # cleanup: replace nbsp as space
                 # this isn't quite right if we preserve html in places, but we currently are not doing that
                 content = content.replace("\xa0", " ").strip()
-                outs += json.dumps({
-                    "id": html_blob["id"],             # MANDATORY: source-specific identifier
-                    "text": content,           # MANDATORY: textual content of the document
-                    "source": "ar5iv",         # MANDATORY: source of the data, such as peS2o, common-crawl, etc.
-                    "added": datetime.datetime.now().isoformat(),          # OPTIONAL: timestamp ai2 acquired this data
-                }) + "\n"
+                outs += (
+                    json.dumps(
+                        {
+                            "id": html_blob["id"],  # MANDATORY: source-specific identifier
+                            "text": content,  # MANDATORY: textual content of the document
+                            "source": "ar5iv",  # MANDATORY: source of the data, such as peS2o, common-crawl, etc.
+                            "added": datetime.datetime.now().isoformat(),  # OPTIONAL: timestamp ai2 acquired this data
+                        }
+                    )
+                    + "\n"
+                )
         _file_path = pathlib.Path(file.replace("gs://", ""))
         _output_path = pathlib.Path(output_path.replace("gs://", ""))
         if _file_path.is_relative_to(prefix_path.replace("gs://", "")):
@@ -199,8 +223,8 @@ def markdownify_ar5iv_html(file,  prefix_path, output_path, file_size):
                 out_file = "gs://" + str(out_file)
         else:
             raise Exception(f"File {file} is not in the prefix path {prefix_path}")
-        with fsspec.open(out_file, 'wb', compression='gzip') as outputf:
-            outputf.write(outs.encode('utf-8'))
+        with fsspec.open(out_file, "wb", compression="gzip") as outputf:
+            outputf.write(outs.encode("utf-8"))
         print(f"Wrote to file {out_file}")
     except FileNotFoundError as e:
         print(f"Error reading the zip file: {e}")
@@ -208,11 +232,12 @@ def markdownify_ar5iv_html(file,  prefix_path, output_path, file_size):
 
     return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert ar5iv to markdown.")
-    parser.add_argument('--input_path', type=str, help='Path to the ar5iv jsonl folder', required=True)
-    parser.add_argument('--output_path', type=str, help='Path to the ar5iv output folder', required=True)
-    parser.add_argument('--file_size', type=int, help='Number of ar5iv documents in a file', required=False, default=256)
+    parser.add_argument("--input_path", type=str, help="Path to the ar5iv jsonl folder", required=True)
+    parser.add_argument("--output_path", type=str, help="Path to the ar5iv output folder", required=True)
+    parser.add_argument("--file_size", type=int, help="Number of ar5iv documents in a file", required=False, default=256)
 
     args = parser.parse_args()
     if args.input_path.startswith("gs://"):
@@ -226,7 +251,7 @@ if __name__ == '__main__':
     MAX_NUM_PENDING_TASKS = 600  # Max number of html files we want to process in pending state
     ray.init()
     result_refs = []
-    
+
     for html in files:
         if len(result_refs) > MAX_NUM_PENDING_TASKS:
             # update result_refs to only
@@ -244,15 +269,14 @@ if __name__ == '__main__':
         ray.get(result_refs)
     except Exception as e:
         print(f"Error processing the group: {e}")
-    
+
     result_refs = []
 
-    
     if args.input_path.startswith("gs://"):
         fs = fsspec.filesystem("gcs")
     else:
         fs = fsspec.filesystem("file")
-        
+
     clean_html_folder = pathlib.Path(output_path.replace("gs://", "")) / "html_clean"
     files = fs.find(clean_html_folder)
 
