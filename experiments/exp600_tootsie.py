6589e8a5edeaf061f01376ceebe4b6d9045bc2a4
David Hall <dlwh@stanford.edu>
2024-12-11 11:24:08 -0800

    adjustments to tootsie

--- experiments/exp600_tootsie.py
+++ experiments/exp600_tootsie.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 from experiments.dclm.exp433_dclm_run import DCLM_MIXTURE_WEIGHTS
 from experiments.defaults import default_tokenize, default_train
 from experiments.llama import llama3_tokenizer, llama_1_4b, llama_8b
@@ -39,8 +41,8 @@ llama_8b_train_config = SimpleTrainConfig(
     learning_rate=1e-3,  # we get divergence with 2e-3
     weight_decay=0.05,
     # WSD-S
-    cycle_length=5000,
-    steps_per_eval=5000,
+    cycle_length=10000,
+    steps_per_eval=10000,
     steps_per_export=20000,
     warmup=1000,  # initial warmup
     # TODO: do we need rewarmup
@@ -48,19 +50,22 @@ llama_8b_train_config = SimpleTrainConfig(
     lr_schedule="inv",
 )
 
-llama_8b_tootsie = default_train(
-    name="llama-8b-tootsie-0.001",
-    tokenized=dclm_mixture_config,
-    model_config=llama_8b,
-    train_config=llama_8b_train_config,
-    tags=["llama", "8b", "wsd-s", "exp600"],
+llama_8b_tootsie = dataclasses.replace(
+    default_train(
+        name="llama-8b-tootsie-0.001",
+        tokenized=dclm_mixture_config,
+        model_config=llama_8b,
+        train_config=llama_8b_train_config,
+        tags=["llama", "8b", "wsd-s", "exp600"],
+    ),
+    override_output_path="checkpoints/llama-8b-tootsie-0.001-19ad63",
 )
 
-llama_8b_simple_train_config = SimpleTrainConfig(
-    tpu_type="v4-256",
+llama_1b_simple_train_config = SimpleTrainConfig(
+    tpu_type="v5litepod-256",
     node_count=1,
     train_batch_size=1024,
-    num_train_steps=1_000_000_000,  # using wsd-s so this doesn't really matter
+    num_train_steps=1_000,  # using wsd-s so this doesn't really matter
     # these hypers from Table 12 in https://arxiv.org/html/2406.11794v1#A6
     learning_rate=2e-3,
     weight_decay=0.05,
@@ -78,8 +83,10 @@ llama_8b_simple = default_train(
     name="llama-1b-simple",
     tokenized=dclm_mixture_config,
     model_config=llama_1_4b,
-    train_config=llama_8b_simple_train_config,
-    tags=["llama", "8b", "wsd-s", "exp600"],
+    train_config=llama_1b_simple_train_config,
+    tags=["llama", "1b", "wsd-s", "exp600"],
+    eval_harness_tasks=[],
+    use_default_evaluation=False,
 )
 
 if __name__ == "__main__":
