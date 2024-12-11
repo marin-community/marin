07c112cc5fdc3dd77fa35624badf901f554ed353
Nikil Ravi <nravi@stanford.edu>
2024-12-11 04:32:09 -0800

    change README

--- experiments/dclm/exp433_dclm_run.py
+++ experiments/dclm/exp433_dclm_run.py
@@ -87,7 +87,7 @@ EXPERIMENT_TAG_MIXTURE = ["433_dclm_1b_1x"]
 EXPERIMENT_TAG_BASELINE_ONLY = ["433_dclm_baseline_1b_1x"]
 
 dclm_mixture_model = default_train(
-    name="dclm_1b_1x_replication_oct26",
+    name="dclm_1b_1x_replication",
     tokenized=dclm_mixture_config,
     model_config=llama_1_4b_dclm,
     train_config=training_config,
@@ -109,12 +109,12 @@ dclm_baseline_only_eval = default_eval(step=dclm_baseline_only_model)
 if __name__ == "__main__":
     executor_main(
         steps=[
-            # dclm_baseline_tokenized,
-            # starcoderdata_tokenized,
-            # proofpile_2_tokenized,
+            dclm_baseline_tokenized,
+            sstarcoderdata_tokenized,
+            proofpile_2_tokenized,
             dclm_mixture_model,
-            # dclm_baseline_only_model,
-            # dclm_mixture_eval,
-            # dclm_baseline_only_eval,
+            dclm_baseline_only_model,
+            dclm_mixture_eval,
+            dclm_baseline_only_eval,
         ]
     )
