ea1d2d600ba1bd94f33a89cb7353e70fb0d1c985
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2024-12-14 15:39:46 -0800

    address comments

--- marin/processing/classification/demos/custom_attribute.py
+++ marin/processing/classification/demos/custom_attribute.py
@@ -7,6 +7,7 @@ Demo for creating custom attributes.
 from collections.abc import Callable
 from dataclasses import dataclass
 
+from marin.classifiers.types import Attribute, Document
 from marin.classifiers.utils import label_documents
 
 
@@ -16,23 +17,24 @@ class CustomAttributeConfig:
     Configuration class for main process.
 
     Attributes:
-        input_doc_path (str): Path to documents (i.e., gs://$BUCKET/documents/...).
         output_attr_path (str): Path to write attributes (i.e., gs://$BUCKET/attributes/...).
-        attribute_func (Callable[[dict],dict]): Function to construct attributes from documents.
+        input_doc_path (str): Path to documents (i.e., gs://$BUCKET/documents/...).
+        label_func (Callable[[Document, list[Attribute]], dict]): Generates attribute dict from
+            document and other input attributes.
         input_attr_paths (list[str]): Path to attributes needed to determine new attribute.
     """
 
-    input_doc_path: str
     output_attr_path: str
-    attribute_func: Callable[[dict, list[dict] | None], dict]
+    input_doc_path: str
+    label_func: Callable[[Document, list[Attribute]], dict]
     input_attr_paths: list[str] | None = None
 
 
 def create_custom_attribute(cfg: CustomAttributeConfig):
     label_documents(
-        input_doc_path=cfg.input_doc_path,
         output_attr_path=cfg.output_attr_path,
-        attribute_func=cfg.attribute_func,
+        input_doc_path=cfg.input_doc_path,
+        label_func=cfg.label_func,
         input_attr_paths=cfg.input_attr_paths,
     )
 
@@ -46,16 +48,16 @@ def main():
     """
     create_custom_attribute(
         CustomAttributeConfig(
-            input_doc_path="gs://marin-us-central2/documents/instruct/tulu_v2_mix/text",
             output_attr_path="gs://marin-us-central2/custom_attribute_demo/part1",
-            attribute_func=lambda doc, attrs: {"label": len(doc["text"])},
+            input_doc_path="gs://marin-us-central2/documents/instruct/tulu_v2_mix/text",
+            label_func=lambda doc, attrs: {"label": len(doc["text"])},
         )
     )
     create_custom_attribute(
         CustomAttributeConfig(
-            input_doc_path="gs://marin-us-central2/documents/instruct/tulu_v2_mix/text/",
             output_attr_path="gs://marin-us-central2/custom_attribute_demo/part2",
-            attribute_func=lambda doc, attrs: {"label": attrs[0]["label"] + 1},
+            input_doc_path="gs://marin-us-central2/documents/instruct/tulu_v2_mix/text/",
+            label_func=lambda doc, attrs: {"label": attrs[0]["label"] + 1},
             input_attr_paths=["gs://marin-us-central2/scratch/custom_attribute_demo/part1"],
         )
     )
