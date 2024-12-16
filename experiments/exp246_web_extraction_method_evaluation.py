75ff58fdb6039f7d59c0cd17fd0e829f78670220
Herumb Shandilya <herumbshandilya123@gmail.com>
2024-12-16 14:32:17 -0800

    parallelism reduction

--- experiments/exp246_web_extraction_method_evaluation.py
+++ experiments/exp246_web_extraction_method_evaluation.py
@@ -1,11 +1,10 @@
-from marin.evaluation.run import evaluate
 from experiments.evals.task_configs import CORE_TASKS
 from marin.evaluation.evaluation_config import EvaluationConfig
+from marin.evaluation.run import evaluate
 from marin.execution.executor import ExecutorStep, executor_main, this_output_path
 
-
 evaluate_readability = ExecutorStep(
-    name=f"evaluation/levanter_lm_evaluation_harness/fineweb-small-readability-1.4b",
+    name="evaluation/lm_evaluation_harness/fineweb-small-readability-1.4b",
     fn=evaluate,
     config=EvaluationConfig(
         evaluator="levanter_lm_evaluation_harness",
@@ -19,7 +18,7 @@ evaluate_readability = ExecutorStep(
 
 
 evaluate_trafilatura_default = ExecutorStep(
-    name=f"evaluation/levanter_lm_evaluation_harness/fineweb-small-trafilatura-default-1.4b",
+    name="evaluation/lm_evaluation_harness/fineweb-small-trafilatura-default-1.4b",
     fn=evaluate,
     config=EvaluationConfig(
         evaluator="levanter_lm_evaluation_harness",
@@ -33,7 +32,7 @@ evaluate_trafilatura_default = ExecutorStep(
 
 
 evaluate_trafilatura_favor_precision = ExecutorStep(
-    name=f"evaluation/levanter_lm_evaluation_harness/fineweb-small-trafilatura-favor-precision-1.4b",
+    name="evaluation/lm_evaluation_harness/fineweb-small-trafilatura-favor-precision-1.4b",
     fn=evaluate,
     config=EvaluationConfig(
         evaluator="levanter_lm_evaluation_harness",
@@ -47,7 +46,7 @@ evaluate_trafilatura_favor_precision = ExecutorStep(
 
 
 evaluate_resiliparse_default = ExecutorStep(
-    name=f"evaluation/levanter_lm_evaluation_harness/fineweb-small-resiliparse-default-1.4b",
+    name="evaluation/lm_evaluation_harness/fineweb-small-resiliparse-default-1.4b",
     fn=evaluate,
     config=EvaluationConfig(
         evaluator="levanter_lm_evaluation_harness",
@@ -61,7 +60,7 @@ evaluate_resiliparse_default = ExecutorStep(
 
 
 evaluate_resiliparse_preserve_formatting = ExecutorStep(
-    name=f"evaluation/levanter_lm_evaluation_harness/fineweb-small-resiliparse-preserve-formatting-1.4b",
+    name="evaluation/lm_evaluation_harness/fineweb-small-resiliparse-preserve-formatting-1.4b",
     fn=evaluate,
     config=EvaluationConfig(
         evaluator="levanter_lm_evaluation_harness",
@@ -78,9 +77,9 @@ if __name__ == "__main__":
     executor_main(
         steps=[
             evaluate_readability,
-            # evaluate_trafilatura_default,
-            # evaluate_trafilatura_favor_precision,
-            # evaluate_resiliparse_default,
-            # evaluate_resiliparse_preserve_formatting,
+            evaluate_trafilatura_default,
+            evaluate_trafilatura_favor_precision,
+            evaluate_resiliparse_default,
+            evaluate_resiliparse_preserve_formatting,
         ]
     )
