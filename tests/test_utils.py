6c5b7e2b8ccb171dee51e06ae96cb482ffab93f2
Abhinav Garg <abhinavg@stanford.edu>
2024-12-20 13:04:55 -0600

    let ray figure out the cluster

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,7 +10,7 @@ from marin.utils import remove_tpu_lockfile_on_exit
 
 
 def setup_module(module):
-    ray.init("local", num_cpus=8)
+    ray.init(num_cpus=8, namespace="marin")
 
 
 def teardown_module(module):
