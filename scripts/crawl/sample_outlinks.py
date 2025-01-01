4571ed2da9db1d48479778e4b1c9358a907aaec9
Nelson Liu <nfliu@nelsonliu.me>
2025-01-01 14:52:00 -0800

    Lower memory requirements on remote functions

--- scripts/crawl/sample_outlinks.py
+++ scripts/crawl/sample_outlinks.py
@@ -60,7 +60,7 @@ class Outlink:
     in_main_content: bool
 
 
-@ray.remote(memory=8 * 1024 * 1024 * 1024)
+@ray.remote(memory=2 * 1024 * 1024 * 1024)
 def count_examples_in_shard(shard_path: str) -> tuple[str, int]:
     with fsspec.open(shard_path, "rt", compression="gzip") as fin:
         num_lines = 0
@@ -69,7 +69,7 @@ def count_examples_in_shard(shard_path: str) -> tuple[str, int]:
     return shard_path, num_lines
 
 
-@ray.remote(memory=8 * 1024 * 1024 * 1024)
+@ray.remote(memory=4 * 1024 * 1024 * 1024)
 def get_examples_from_offsets(shard_path: str, offsets: list[int]):
     extracted_examples = set()
     offsets = sorted(offsets)  # ensure ascending order
