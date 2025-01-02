8660049e1ba4b8a2a579fba84fe13993f4baab8d
Nelson Liu <nfliu@nelsonliu.me>
2025-01-01 20:32:32 -0800

    Log all the failed fetches too

--- scripts/crawl/fetch_links.py
+++ scripts/crawl/fetch_links.py
@@ -12,7 +12,8 @@ python marin/run/ray_run.py \
     python scripts/crawl/fetch_links.py \
     --urls_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-1M/links.99.parquet \
     --warc_output_path gs://marin-us-central2/scratch/nfliu/fetched_outlinks/fineweb-edu-1M/links.99.warc.gz \
-    --robots_output_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-1M/links.99.json
+    --robots_output_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-1M/links.99_robots.json \
+    --errors_output_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-1M/links.99_errors.json
 ```
 """
 import io
@@ -35,15 +36,15 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 
-def fetch_url(session: requests.Session, url: str) -> requests.Response | None:
+def fetch_url(session: requests.Session, url: str) -> tuple[requests.Response, None] | tuple[None, str]:
     """Fetch the content of a URL."""
     try:
         response = session.get(url, timeout=30)
         response.raise_for_status()
-        return response
+        return response, None
     except requests.RequestException as e:
         logger.info(f"Failed to fetch {url}: {e}")
-        return None
+        return None, str(e)
 
 
 @dataclass
@@ -51,10 +52,11 @@ class FetchLinksConfig:
     urls_path: str
     warc_output_path: str
     robots_output_path: str
+    errors_output_path: str
 
 
 @ray.remote(memory=64 * 1024 * 1024 * 1024)
-def fetch_links(urls_path: str, warc_output_path: str, robots_output_path: str):
+def fetch_links(urls_path: str, warc_output_path: str, robots_output_path: str, errors_output_path: str):
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     with fsspec.open(urls_path) as f:
         df = pd.read_parquet(f)
@@ -63,22 +65,19 @@ def fetch_links(urls_path: str, warc_output_path: str, robots_output_path: str):
     # Extract the URLs from the "link_target" column
     urls = df["link_target"].tolist()
 
-    fetch_to_warc(urls, warc_output_path, robots_output_path)
+    fetch_to_warc(urls, warc_output_path, robots_output_path, errors_output_path)
     logger.info(f"WARC file created at: {warc_output_path}, robots.txt data written to {robots_output_path}")
 
 
-def fetch_to_warc(urls: list[str], warc_output_path: str, robots_output_path: str):
+def fetch_to_warc(urls: list[str], warc_output_path: str, robots_output_path: str, errors_output_path: str):
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     domains_to_robots: dict[str, str] = {}
+    fetch_errors: dict[str, str] = {}
 
     warc_buffer = io.BytesIO()
 
     with requests.Session() as session:
-        session.headers.update(
-            {
-                "User-Agent": "CCBot",
-            }
-        )
+        session.headers.update({"User-Agent": "CCBot"})
         writer = WARCWriter(warc_buffer, gzip=True)
 
         for url in tqdm(urls, desc="Fetching URLs"):
@@ -89,30 +88,27 @@ def fetch_to_warc(urls: list[str], warc_output_path: str, robots_output_path: st
                 # Construct the robots.txt URL
                 base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
                 robots_url = f"{base_url}/robots.txt"
-                # Fetch the robots.txt for this domain
-                logger.info(f"Getting robots.txt: {url}")
-                response = fetch_url(session, robots_url)
-                if response is not None:
-                    domains_to_robots[url_domain] = response.text
+
+                logger.info(f"Getting robots.txt: {robots_url}")
+                robots_response, _ = fetch_url(session, robots_url)
+                if robots_response is not None:
+                    domains_to_robots[url_domain] = robots_response.text
 
             logger.info(f"Processing: {url}")
-            response = fetch_url(session, url)
+            response, err = fetch_url(session, url)
             if response is None:
+                if err:
+                    fetch_errors[url] = err
                 continue
 
             # Prepare HTTP headers for WARC
             http_headers = []
-            # Add status line
             status_line = f"{response.status_code} {response.reason}"
             http_headers.append(("Status", status_line))
-            # Add other headers
             for header, value in response.headers.items():
                 http_headers.append((header, value))
 
-            # Create StatusAndHeaders object
             status_headers = StatusAndHeaders(status_line, http_headers, protocol="HTTP/1.1")
-
-            # Wrap the response content in a BytesIO object
             payload_io = io.BytesIO(response.content)
 
             # Create WARC record
@@ -121,20 +117,25 @@ def fetch_to_warc(urls: list[str], warc_output_path: str, robots_output_path: st
                 writer.write_record(warc_record)
             except Exception as e:
                 logger.info(f"Failed to write WARC record for {url}: {e}")
+                fetch_errors[url] = f"WARC write error: {e}"
 
     # Get the WARC data from our in-memory buffer and write to disk
     warc_data = warc_buffer.getvalue()
     with fsspec.open(warc_output_path, "wb") as fout:
         fout.write(warc_data)
 
+    # Write domains_to_robots data
     with fsspec.open(robots_output_path, "w", compression="gzip") as fout:
         json.dump(domains_to_robots, fout)
+
+    with fsspec.open(errors_output_path, "w", compression="gzip") as fout:
+        json.dump(fetch_errors, fout)
 
 
 @draccus.wrap()
 def main(cfg: FetchLinksConfig):
     # Do all the processing in a remote function
-    _ = ray.get(fetch_links.remote(cfg.urls_path, cfg.warc_output_path, cfg.robots_output_path))
+    _ = ray.get(fetch_links.remote(cfg.urls_path, cfg.warc_output_path, cfg.robots_output_path, cfg.errors_output_path))
 
 
 if __name__ == "__main__":
