5e27812da30b5db3d92031c8a506e25a81488359
Nikil Ravi <nravi@stanford.edu>
2025-01-08 12:20:00 -0800

    add a default function for scaling laws

--- marin/scaling_laws/scaling_law_config.py
+++ marin/scaling_laws/scaling_law_config.py
@@ -1,19 +1,27 @@
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True)
 class ScalingLawConfig:
 
-    input_run_ids: list[str]
+    ladder_model_steps: list[str]
     """list of (smaller model) wandb run ids to be used as input for scaling laws"""
 
     pred_run_id: str
     """wandb run id for the larger model to make predictions for"""
 
-    task_loss: str = "eval/paloma/c4_en/bpb"
+    intermediate_task_loss: str = "eval/paloma/c4_en/bpb"
     """intermediate task loss to be used for scaling laws (eg. c4en bpb)"""
 
     task_accuracy: str = "lm_eval/hellaswag_10shot/acc"
     """task accuracy to predict for the larger model (eg. hellaswag accuracy)"""
+
+
+def get_wandb_run_id_from_step(step: ExecutorStep) -> str:
+    """
+    Get the wandb run id from a given ExecutorStep.
+    """
+    return step.config.trainer.tracker.id
 
 
 def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
@@ -21,15 +29,18 @@ def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
     Analyze scaling laws for a given task loss and accuracy.
     """
     from utils import *
-    
+
+    input_run_ids = [get_wandb_run_id_from_step(step) for step in config.ladder_model_steps]
+    pred_run_id = get_wandb_run_id_from_step(config.pred_model_step)
+
     # first predict task loss from (N, D) pairs
     actual, predicted = fit_task_loss_from_ladder_models(
-        runs=config.input_run_ids,
+        runs=input_run_ids,
         entity="stanford-mercury",
         project="marin",
         metrics=[config.task_loss, "throughput/total_tokens", task_accuracy],
-        pred_run=config.pred_run_id,
-        task_loss=config.task_loss,
+        pred_run=pred_run_id,
+        task_loss=config.intermediate_task_loss,
         aggregation="all",
         tokens_col="throughput/total_tokens",
         param_col="parameter_count",
@@ -37,8 +48,6 @@ def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
         use_log_for_ND=True,
         normalize_ND=True,
     )
-
-    # plot_actual_vs_predicted(actual, predicted, title="Prediction on 7B Run using 5-sub 1.4B ladder models")
 
     # then predict task accuracy from task loss
     actual_acc, pred_acc = fit_accuracy_from_task_loss(
@@ -57,4 +66,3 @@ def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
     # produce plots and write to wandb
 
     return actual_acc, pred_acc
-
