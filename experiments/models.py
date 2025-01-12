8ad5c488d8de01c19f0c55f98a3e36a528829765
Christopher Chou <emu4yolife@gmail.com>
2025-01-12 22:41:04 +0000

    Create hard link

--- experiments/models.py
+++ experiments/models.py
@@ -1,3 +1,4 @@
+import subprocess
 from dataclasses import dataclass
 
 from experiments.instruction_datasets import get_directory_friendly_dataset_name
@@ -16,12 +17,17 @@ class ModelConfig:
 # This means that for certain large models (e.g. Llama 70B), we will not be able
 # to fit the models on local disk. We use GCSFuse to mount the GCS bucket to the local filesystem
 # to be able to download and use these large models.
+LOCAL_PREFIX = "/opt"
 GCS_FUSE_MOUNT_PATH = "gcsfuse_mount/models"
 
 MODEL_NAME_TO_CONFIG = {
     "HuggingFaceTB/SmolLM2-1.7B-Instruct": ModelConfig(
         hf_repo_id="HuggingFaceTB/SmolLM2-1.7B-Instruct",
         hf_revision="450ff1f",
+    ),
+    "Qwen/Qwen2.5-7B-Instruct": ModelConfig(
+        hf_repo_id="Qwen/Qwen2.5-7B-Instruct",
+        hf_revision="a09a354",
     ),
 }
 
@@ -39,8 +45,21 @@ def download_model_step(model_config: ModelConfig) -> ExecutorStep:
             hf_repo_type_prefix="",
         ),
     )
+    # While the above operation downloads the model to the GCS, the model
+    # is not available on the local filesystem since GCS does not handle
+    # implicitly defined directories that are not created through GCSFuse.
+    # The previous operation was created using Cloud Storage but not FUSE
+    # so it will no appear to be available. The following operation creates
+    # the directory on the local filesystem.
+    # Source: https://arc.net/l/quote/dcgeuozs
+    create_local_filesystem_link(model_name)
 
     return download_step
+
+
+def create_local_filesystem_link(model_name: str):
+    # Create /opt/gcsfuse_mount/models
+    subprocess.run(["mkdir", f"{LOCAL_PREFIX}/{GCS_FUSE_MOUNT_PATH}"], capture_output=True, text=True)
 
 
 def get_model(model_name: str) -> ExecutorStep:
