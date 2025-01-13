4af4d726c00be39e05d29b65d46cf2171fd2449b
Nikil Ravi <nravi@stanford.edu>
2025-01-13 03:12:12 -0800

    wip

---
+++ experiments/scaling_law_simple.py
@@ -0,0 +1,29 @@
+from experiments.defaults import default_scaling_law_analysis
+from experiments.exp600_tootsie import dclm_mixture_config_llama3, llama_8b_tootsie
+from marin.execution.executor import executor_main
+from marin.training.scaling_laws import scaling_law_suite
+
+RUNS = [
+    "tootsie-scaling-512-81c36c",
+    "tootsie-scaling-768-d17a90",
+    "tootsie-scaling-1024-f4e4be",
+    "tootsie-scaling-1536-e2a6d8",
+    "tootsie-scaling-2048-72c648",
+]
+
+PRED_RUN = "llama-8b-tootsie-0.001-19ad63"
+
+scaling_law_8b_performance_pred = default_scaling_law_analysis(
+    ladder_runs=RUNS,
+    pred_run=PRED_RUN,
+    intermediate_task_loss="eval/paloma/c4_en/bpb",
+    task_accuracy="lm_eval/hellaswag_0shot/acc",
+)
+
+if __name__ == "__main__":
+    executor_main(
+        steps=[
+            scaling_law_8b_performance_pred,
+        ],
+        description="scaling law suite to predict performance of 8B model on DCLM mix",
+    )
--- marin/scaling_laws/scaling_laws.py
+++ marin/scaling_laws/scaling_laws.py
@@ -1,19 +1,18 @@
 from dataclasses import dataclass
 
-import matplotlib.pyplot as plt
 import wandb
-from utils import plot_actual_vs_predicted, plot_fit
 
 from marin.execution.executor import ExecutorStep
+from marin.scaling_laws.utils import plot_actual_vs_predicted, plot_fit
 
 
 @dataclass(frozen=True)
 class ScalingLawConfig:
 
-    ladder_model_steps: list[ExecutorStep]
+    ladder_model_steps: list[ExecutorStep | str]
     """list of (smaller model) wandb run ids to be used as input for scaling laws"""
 
-    pred_model_step: ExecutorStep
+    pred_model_step: ExecutorStep | str
     """wandb run id for the larger model to make predictions for"""
 
     intermediate_task_loss: str = "eval/paloma/c4_en/bpb"
@@ -34,20 +33,32 @@ def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
     """
     Analyze scaling laws for a given task loss and accuracy.
     """
-    from utils import (
+    from marin.scaling_laws.utils import (
         fit_accuracy_from_task_loss,
         fit_task_loss_from_ladder_models,
     )
 
-    input_run_ids = [get_wandb_run_id_from_step(step) for step in config.ladder_model_steps]
-    pred_run_id = get_wandb_run_id_from_step(config.pred_model_step)
+    input_run_ids = [
+        get_wandb_run_id_from_step(step) if isinstance(step, ExecutorStep) else step
+        for step in config.ladder_model_steps
+    ]
+
+    if isinstance(config.pred_model_step, ExecutorStep):
+        pred_run_id = get_wandb_run_id_from_step(config.pred_model_step)
+    else:
+        pred_run_id = config.pred_model_step
+
+    print(f"Input runs: {input_run_ids}")
+    print(f"Prediction run: {pred_run_id}")
+    print(f"Intermediate task loss: {config.intermediate_task_loss}")
+    print(f"Task accuracy: {config.task_accuracy}")
 
     # first predict task loss from (N, D) pairs
     actual_loss, predicted_loss = fit_task_loss_from_ladder_models(
         runs=input_run_ids,
         entity="stanford-mercury",
         project="marin",
-        metrics=[config.task_loss, "throughput/total_tokens", config.task_accuracy],
+        metrics=[config.intermediate_task_loss, "throughput/total_tokens", config.task_accuracy],
         pred_run=pred_run_id,
         task_loss=config.intermediate_task_loss,
         aggregation="all",
@@ -61,16 +72,18 @@ def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
     # then predict task accuracy from task loss
     actual_acc, pred_acc = fit_accuracy_from_task_loss(
         pred_task_losses=predicted_loss,
-        runs=config.input_run_ids,
+        runs=input_run_ids,
         entity="stanford-mercury",
         project="marin",
         x_axis="throughput/total_gflops",
         tokens_col="throughput/total_tokens",
-        pred_run=config.pred_run_id,
+        pred_run=pred_run_id,
         aggregation="all",
         task_loss_col=config.intermediate_task_loss,
         accuracy_col=config.task_accuracy,
     )
+
+    print("Made predictions on accuracy and loss. Going to create a report.")
 
     # Log and create a report
     log_and_create_report(
@@ -84,8 +97,6 @@ def run_scaling_law_analysis(config: ScalingLawConfig) -> None:
         wandb_project="marin-scaling-laws",
         wandb_entity="stanford-mercury",
     )
-
-    return actual_acc, pred_acc
 
 
 def log_and_create_report(
@@ -131,14 +142,12 @@ def log_and_create_report(
         actual_loss, predicted_loss, title=f"Prediction on larger run using {len(input_run_ids)} smaller models"
     )
     wandb.log({"Task Loss Comparison": wandb.Image(task_loss_plot)})
-    plt.close(task_loss_plot)
 
     # Task Loss fit plot
     task_loss_fit_plot = plot_fit(
-        actual_acc, predicted_acc, title=f"Prediction on larger run using {len(input_run_ids)} smaller models"
+        actual_loss, predicted_loss, title=f"Prediction on larger run using {len(input_run_ids)} smaller models"
     )
     wandb.log({"Task Loss Fit": wandb.Image(task_loss_fit_plot)})
-    plt.close(task_loss_fit_plot)
 
     # Task Accuracy Comparison Plot
     task_accuracy_plot = plot_actual_vs_predicted(
@@ -148,7 +157,6 @@ def log_and_create_report(
         task_metric=scaling_law_config.task_accuracy,
     )
     wandb.log({"Task Accuracy Comparison": wandb.Image(task_accuracy_plot)})
-    plt.close(task_accuracy_plot)
 
     # Task Accuracy fit plot
     task_accuracy_fit_plot = plot_fit(
@@ -157,7 +165,6 @@ def log_and_create_report(
         title=f"Prediction on larger run using {len(input_run_ids)} smaller models",
     )
     wandb.log({"Task Accuracy Fit": wandb.Image(task_accuracy_fit_plot)})
-    plt.close(task_accuracy_fit_plot)
 
     # Info about runs and links
     input_run_links = [f"https://wandb.ai/stanford-mercury/marin/runs/{run_id}" for run_id in input_run_ids]
@@ -166,18 +173,8 @@ def log_and_create_report(
         {
             "Input Runs": input_run_links,
             "Prediction Run": prediction_run_link,
-        }
-    )
-
-    # log the config
-    run.log(
-        {
-            "Config Information": {
-                "Input Runs": input_run_links,
-                "Prediction Run": prediction_run_link,
-                "Task Loss Metric": scaling_law_config.intermediate_task_loss,
-                "Task Accuracy Metric": scaling_law_config.task_accuracy,
-            }
+            "Task Loss Metric": scaling_law_config.intermediate_task_loss,
+            "Task Accuracy Metric": scaling_law_config.task_accuracy,
         }
     )
 
