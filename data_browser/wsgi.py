d8d29faaad6219323ebb62f96ee1a2489924dcb8
Ashwin Ramaswami <aramaswamis@gmail.com>
2025-01-15 02:07:02 +0000

    Fixed gunicorn deploy

---
+++ data_browser/wsgi.py
@@ -0,0 +1,17 @@
+import yaml
+from server import app, ServerConfig, standardize_config, Server, server
+
+# Read config from gcp.conf
+with open('conf/gcp.conf', 'r') as f:
+    config_dict = yaml.safe_load(f)
+
+# Initialize server configuration
+config = ServerConfig(**config_dict)
+config = standardize_config(config)
+
+# Initialize global server instance
+import server as server_module
+server_module.server = Server(config)
+
+# This is what Gunicorn will import
+application = app 
