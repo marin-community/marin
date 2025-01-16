a2bf51fc1733823a5a1496b3d332b4606a943afa
Nelson Liu <nfliu@nelsonliu.me>
2025-01-16 14:38:52 -0800

    Hash URLs to try to reduce memory usage

--- scripts/crawl/count_outlinks.py
+++ scripts/crawl/count_outlinks.py
@@ -7,6 +7,7 @@ Running on OpenWebMath:
 
 ```
 python marin/run/ray_run.py \
+    --pip_deps 'xxhash' \
     --no_wait -- \
     python scripts/crawl/count_outlinks.py \
     --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8/*_links.jsonl.gz'
@@ -16,11 +17,13 @@ Running on FineWeb-Edu:
 
 ```
 python marin/run/ray_run.py \
+    --pip_deps 'xxhash' \
     --no_wait -- \
     python scripts/crawl/count_outlinks.py \
     --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu/CC-MAIN*/*_links.jsonl.gz'
 ```
 """
+import json
 import logging
 from dataclasses import dataclass
 
@@ -28,6 +31,7 @@ import draccus
 import fsspec
 import ray
 from tqdm_loggable.auto import tqdm
+import xxhash
 
 from marin.utils import fsspec_glob
 
@@ -41,12 +45,14 @@ class OutlinksCountingConfig:
 
 
 @ray.remote(memory=1 * 1024 * 1024 * 1024)
-def count_examples_in_shard(shard_path: str) -> tuple[str, int]:
+def count_examples_in_shard(shard_path: str) -> tuple[int, set[int]]:
+    unique_outlink_target_hashes = set()
+    num_lines = 0
     with fsspec.open(shard_path, "rt", compression="gzip") as fin:
-        num_lines = 0
-        for _ in fin:
+        for line in fin:
+            unique_outlink_target_hashes.add(xxhash.xxh64(json.loads(line)["link_target"]).intdigest())
             num_lines += 1
-    return shard_path, num_lines
+    return num_lines, unique_outlink_target_hashes
 
 
 @ray.remote(memory=64 * 1024 * 1024 * 1024)
@@ -60,6 +66,7 @@ def count_outlinks(input_pattern: str):
         refs.append(count_examples_in_shard.remote(shard_path))
 
     num_outlinks = 0
+    all_link_target_hashes = set()
     with tqdm(total=len(refs), desc="Counting records") as pbar:
         while refs:
             # Process results in the finish order instead of the submission order.
@@ -67,10 +74,11 @@ def count_outlinks(input_pattern: str):
             # The node only needs enough space to store
             # a batch of objects instead of all objects.
             results = ray.get(ready_refs)
-            for shard_path, num_examples in results:
+            for num_examples, shard_link_target_hashes in results:
                 num_outlinks += num_examples
+                all_link_target_hashes.update(shard_link_target_hashes)
                 pbar.update(1)
-    logger.info(f"Found {num_outlinks} outlinks in the input pattern")
+    logger.info(f"Found {num_outlinks} outlinks ({len(all_link_target_hashes)} unique)")
 
 
 @draccus.wrap()
