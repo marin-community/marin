3bc97f68db0636d1fe448d5fa3baf058c7d22c4a
Nelson Liu <nfliu@nelsonliu.me>
2025-01-16 21:36:00 -0800

    Try using ray.data groupby to count unique outlinks

--- scripts/crawl/count_outlinks.py
+++ scripts/crawl/count_outlinks.py
@@ -7,7 +7,6 @@ Running on OpenWebMath:
 
 ```
 python marin/run/ray_run.py \
-    --pip_deps 'xxhash' \
     --no_wait -- \
     python scripts/crawl/count_outlinks.py \
     --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8/*_links.jsonl.gz'
@@ -17,21 +16,18 @@ Running on FineWeb-Edu:
 
 ```
 python marin/run/ray_run.py \
-    --pip_deps 'xxhash' \
     --no_wait -- \
     python scripts/crawl/count_outlinks.py \
     --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu/CC-MAIN*/*_links.jsonl.gz'
 ```
 """
-import json
 import logging
 from dataclasses import dataclass
 
 import draccus
 import fsspec
 import ray
-from tqdm_loggable.auto import tqdm
-import xxhash
+from ray.data.aggregate import AggregateFn
 
 from marin.utils import fsspec_glob
 
@@ -44,63 +40,24 @@ class OutlinksCountingConfig:
     input_pattern: str
 
 
-@ray.remote(memory=4 * 1024 * 1024 * 1024)
-def count_examples_in_shard(shard_path: str) -> tuple[int, set[int]]:
-    unique_outlink_target_hashes = set()
-    num_lines = 0
-    with fsspec.open(shard_path, "rt", compression="gzip") as fin:
-        for line in fin:
-            unique_outlink_target_hashes.add(xxhash.xxh64(json.loads(line)["link_target"]).intdigest())
-            num_lines += 1
-    return num_lines, unique_outlink_target_hashes
-
-
 @ray.remote(memory=256 * 1024 * 1024 * 1024)
 def count_outlinks(input_pattern: str):
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-    shard_paths = list(fsspec_glob(input_pattern))
-    logger.info(f"Found {len(shard_paths)} shards to process")
-
-    num_shards_to_process = len(shard_paths)
-    MAX_CONCURRENT_TASKS = 100
-    num_shards_submitted = 0
-    unfinished = []
-
-    def submit_shard_task(shard_path):
-        nonlocal num_shards_submitted
-        unfinished.append(count_examples_in_shard.remote(shard_path))
-        num_shards_submitted += 1
-        if num_shards_submitted % 10 == 0:
-            logger.info(
-                f"Submitted {num_shards_submitted} / {num_shards_to_process} shards "
-                f"({num_shards_submitted / num_shards_to_process})"
-            )
-
-    for _ in range(min(MAX_CONCURRENT_TASKS, len(shard_paths))):
-        submit_shard_task(shard_paths.pop())
-
-    all_link_target_hashes = set()
-    num_outlinks = 0
-    with tqdm(total=num_shards_to_process, desc="Counting records") as pbar:
-        while unfinished:
-            finished, unfinished = ray.wait(unfinished, num_returns=len(unfinished), timeout=5)
-            try:
-                results = ray.get(finished)
-                for num_examples, shard_link_target_hashes in results:
-                    num_outlinks += num_examples
-                    all_link_target_hashes.update(shard_link_target_hashes)
-                    pbar.update(1)
-                    logger.info(f"So far, found {num_outlinks} outlinks ({len(all_link_target_hashes)} unique)")
-            except Exception as e:
-                logger.exception(f"Error processing shard: {e}")
-                raise
-
-            # If we have more shard paths left to process and we haven't hit the max
-            # number of concurrent tasks, add tasks to the unfinished queue.
-            while shard_paths and len(unfinished) < MAX_CONCURRENT_TASKS:
-                submit_shard_task(shard_paths.pop())
-
-    logger.info(f"In total, found {num_outlinks} outlinks ({len(all_link_target_hashes)} unique)")
+    logger.info(f"Getting shards from {input_pattern}")
+    shard_paths = sorted(list(fsspec_glob(input_pattern)))
+    logger.info(f"Reading data from {len(shard_paths)} shard paths")
+    ds = ray.data.read_json(shard_paths)
+    logger.info(f"Read data from {input_pattern}")
+    logger.info("Counting total number of outlinks")
+    count_total = ds.count()
+    logger.info("Deduplicating outlinks by link target")
+    # First, groupby link_target and compute some aggregation (e.g. count)
+    ds_grouped = ds.groupby("link_target").count()
+    logger.info("Counting number of deduplicated outlinks")
+    # Now each group is just one row; how many groups do we have?
+    count_unique = ds_grouped.count()
+    logger.info(f"Found {count_total} total outlinks")
+    logger.info(f"Found {count_unique} unique outlinks")
 
 
 @draccus.wrap()
