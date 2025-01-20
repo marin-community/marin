2182276347cd59f03d4cfd15b78012ef621aed91
Nelson Liu <nfliu@nelsonliu.me>
2025-01-19 17:12:34 -0800

    Don't print out full traceback when requests hits an exception

--- scripts/crawl/fetch_links.py
+++ scripts/crawl/fetch_links.py
@@ -117,11 +117,11 @@ def fetch_url(
             return truncated_response, r.status_code, None, False
     except requests.exceptions.HTTPError as e:
         # We got a response from the server, so the domain is reachable
-        logger.exception(f"Error fetching {url}: {e}")
+        logger.error(f"Error fetching {url}: {e}")
         return None, e.response.status_code, str(e), False
     except Exception as e:
         # Anything else is a connection-level error (DNS, SSL, etc.)
-        logger.exception(f"Error fetching {url}: {e}")
+        logger.error(f"Error fetching {url}: {e}")
         return None, None, str(e), True
 
 
