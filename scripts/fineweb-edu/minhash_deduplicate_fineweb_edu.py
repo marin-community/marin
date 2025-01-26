0ed3697fc274c0eeae801d04cf27161ba43234e8
Nelson Liu <nfliu@nelsonliu.me>
2025-01-25 23:09:06 -0800

    Add both document and file progress bars

--- scripts/fineweb-edu/minhash_deduplicate_fineweb_edu.py
+++ scripts/fineweb-edu/minhash_deduplicate_fineweb_edu.py
@@ -105,7 +105,9 @@ def minhash_deduplicate_fineweb_edu(
     # this is the original data that we want to deduplicate
     # NOTE: neither the base folder path or the paths in the pathfile should
     # include the leading "/"
-    INPUT_READER = ParquetReader("gs://marin-us-central2", paths_file=parquets_paths_file)
+    INPUT_READER = ParquetReader(
+        "gs://marin-us-central2", paths_file=parquets_paths_file, file_progress=True, doc_progress=True
+    )
     TOTAL_TASKS = 1000
     NUM_WORKERS = 100
 
