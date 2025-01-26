439af8ddaed0dffdd64f6ef364d58da199922471
Herumb Shandilya <herumbshandilya123@gmail.com>
2025-01-26 00:16:46 -0800

    Modify decompression logic for zstd

---
+++ experiments/exp747_download_nemotron_cc.py
@@ -0,0 +1,10 @@
+from experiments.pretraining_datasets import nemotron_cc
+from marin.execution.executor import executor_main
+
+if __name__ == "__main__":
+    executor_main(
+        steps=[
+            nemotron_cc,
+        ],
+        pip_dependency_groups=["download_transform"],
+    )
