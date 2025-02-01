7c4aaa509fac6c2a933c1274fa87fa3c65165afb
Nelson Liu <nfliu@nelsonliu.me>
2025-01-31 22:02:25 -0800

    Add initial script to deduplicate outlinks against CC

---
+++ scripts/crawl/deduplicate_outlinks_against_cc.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+"""
+Running on OpenWebMath:
+
+```
+python scripts/crawl/deduplicate_outlinks_against_cc.py \
+    --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8/*_links.jsonl.gz' \
+    --bloom_filter_2013_2018_path 'gs://marin-us-central2/gcsfuse_mount/nfliu/deduplicate_outlinks/cc-urls-partitioned_2013_2018.bloom' \
+    --bloom_filter_2019_2024_path 'gs://marin-us-central2/gcsfuse_mount/nfliu/deduplicate_outlinks/cc-urls-partitioned_2019_2024.bloom' \
+    --output_path gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8-cc-deduplicated/
+```
+
+Running on FineWeb-Edu:
+
+```
+python scripts/crawl/deduplicate_outlinks_against_cc.py \
+    --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu/CC-MAIN*/*_links.jsonl.gz' \
+    --bloom_filter_2013_2018_path 'gs://marin-us-central2/gcsfuse_mount/nfliu/deduplicate_outlinks/cc-urls-partitioned_2013_2018.bloom' \
+    --bloom_filter_2019_2024_path 'gs://marin-us-central2/gcsfuse_mount/nfliu/deduplicate_outlinks/cc-urls-partitioned_2019_2024.bloom' \
+    --output_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-cc-deduplicated/
+```
+"""
+import json
+import concurrent.futures
+import logging
+import os
+from dataclasses import dataclass
+from hashlib import sha256
+
+import draccus
+import fsspec
+from marin.utils import fsspec_exists, fsspec_glob
+from rbloom import Bloom
+from tqdm_loggable.auto import tqdm
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+bloom_filter_2013_2018: Bloom = None
+bloom_filter_2019_2024: Bloom = None
+
+
+@dataclass
+class DeduplicateOutlinksAgainstCCConfig:
+    input_pattern: str
+    bloom_filter_2013_2018_path: str
+    bloom_filter_2019_2024_path: str
+    output_path: str
+
+
+def hash_func(obj):
+    h = sha256(obj.encode("utf-8")).digest()
+    # use sys.byteorder instead of "big" for a small speedup when
+    # reproducibility across machines isn't a concern
+    return int.from_bytes(h[:16], "big", signed=True)
+
+
+def deduplicate_shard(shard_path: str, shard_output_path: str) -> tuple[int, int]:
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    success_path = shard_output_path + ".SUCCESS"
+    # If the success path exists, read its stats and return
+    if fsspec_exists(success_path):
+        logger.info(f"Found success file {success_path}, skipping...")
+        with fsspec.open(success_path) as f:
+            successful_stats = json.load(f)
+            return (successful_stats["num_outlinks"], successful_stats["num_deduplicated_outlinks"])
+
+    num_outlinks = 0
+    num_deduplicated_outlinks = 0
+
+    seen_link_targets = set()
+    with (
+        fsspec.open(shard_path, "rt", compression="infer") as fin,
+        fsspec.open(shard_output_path, "w", compression="infer") as fout,
+    ):
+        for line in tqdm(fin, desc=f"Deduplicating {shard_path}"):
+            parsed_line = json.loads(line)
+            link_target = parsed_line["link_target"]
+            if (
+                link_target not in bloom_filter_2013_2018
+                and link_target not in bloom_filter_2019_2024
+                and link_target not in seen_link_targets
+            ):
+                fout.write(line)
+                num_deduplicated_outlinks += 1
+            num_outlinks += 1
+            seen_link_targets.add(link_target)
+
+    with fsspec.open(success_path, "w", compression="infer") as f:
+        json.dump({"num_outlinks": num_outlinks, "num_deduplicated_outlinks": num_deduplicated_outlinks}, f)
+    logger.info(
+        f"Shard {shard_path} has {num_outlinks} total outlinks, {num_deduplicated_outlinks} deduplicated outlinks"
+    )
+    return (num_outlinks, num_deduplicated_outlinks)
+
+
+def deduplicate_outlinks_against_cc(
+    input_pattern: str,
+    bloom_filter_2013_2018_path: str,
+    bloom_filter_2019_2024_path: str,
+    output_path: str,
+):
+    """
+    Args:
+    input_pattern (str): Pattern to input outlinks to deduplicate.
+    bloom_filter_2013_2018_path (str): path to bloom filter for links from 2013-2018.
+    bloom_filter_2019_2024_path (str): path to bloom filter for links from 2019-2024.
+    output_path (str): Write deduplicated outlinks to this directory
+    """
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    # Sort for reproducibility
+    shard_paths = sorted(list(fsspec_glob(input_pattern)))
+    logger.info(f"Found {len(shard_paths)} shards to process")
+
+    # Load the bloom filter
+    global bloom_filter_2013_2018
+    logger.info("Loading 2013 - 2018 bloom filter")
+    bloom_filter_2013_2018 = Bloom.load(bloom_filter_2013_2018_path, hash_func)
+    logger.info("Loaded 2013 - 2018 bloom filter")
+    global bloom_filter_2019_2024
+    logger.info("Loading 2019 - 2024 bloom filter")
+    bloom_filter_2019_2024 = Bloom.load(bloom_filter_2019_2024_path, hash_func)
+    logger.info("Loaded 2019 - 2024 bloom filter")
+
+    num_outlinks = 0
+    num_deduplicated_outlinks = 0
+
+    with tqdm(total=len(shard_paths)) as pbar:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=64) as executor:
+            futures = []
+            for shard_path in shard_paths:
+                output_shard_path = os.path.join(output_path, os.path.basename(shard_path))
+                futures.append(executor.submit(deduplicate_shard, shard_path, output_shard_path))
+            for future in concurrent.futures.as_completed(futures):
+                (shard_num_outlinks, shard_num_deduplicated_outlinks) = future.result()
+                num_outlinks += shard_num_outlinks
+                num_deduplicated_outlinks += shard_num_deduplicated_outlinks
+                # Log count so far
+                logger.info(
+                    f"So far, found {num_outlinks} total outlinks, {num_deduplicated_outlinks} of which do not occur in the CC"
+                )
+                pbar.update(1)
+
+    logger.info(
+        f"In total, found {num_outlinks} total outlinks, {num_deduplicated_outlinks} of which do not occur in the CC"
+    )
+
+
+@draccus.wrap()
+def deduplicate_outlinks_against_cc_driver(cfg: DeduplicateOutlinksAgainstCCConfig):
+    deduplicate_outlinks_against_cc(
+        cfg.input_pattern,
+        cfg.bloom_filter_2013_2018_path,
+        cfg.bloom_filter_2019_2024_path,
+        cfg.output_path,
+    )
+
+
+if __name__ == "__main__":
+    deduplicate_outlinks_against_cc_driver()
