e1f4925b4ec267d095be5d56af20c277e53901a1
Nikil Ravi <nravi@stanford.edu>
2025-02-01 04:03:15 -0800

    lint, fixes

--- experiments/scaling_law_simple.py
+++ experiments/scaling_law_simple.py
@@ -18,17 +18,43 @@ RUNS = [
 # PRED_RUN = "llama-70b-tootsie-dummy-testing-986d5d" #llama-8b-tootsie-0.001-19ad63"
 PRED_RUN = "llama-8b-tootsie-0.001-19ad63"
 
+TASK_BPB_RUNS = [
+    "tootsie-scaling-soft-metrics-512-64dff5",
+    "tootsie-scaling-soft-metrics-768-e6d0bb",
+    "tootsie-scaling-soft-metrics-1024-ef3791",
+    "tootsie-scaling-soft-metrics-1536-b928e4",
+    "tootsie-scaling-soft-metrics-2048-c18a5c",
+]
+
+TASK_BPBS_TO_PREDICT = [
+    "lm_eval/hellaswag_10shot/bpb",
+    "lm_eval/hellaswag_0shot/bpb",
+    "lm_eval/boolq/bpb",
+    "lm_eval/copa/bpb",
+]
+
 scaling_law_8b_performance_pred = default_scaling_law_pred(
     ladder_runs=RUNS,
     pred_run=PRED_RUN,
-    task_losses=("eval/paloma/c4_en/bpb", "eval/bpb", "eval/loss"),
+    task_losses=(
+        "eval/paloma/c4_en/bpb",
+        "eval/bpb",
+        "eval/loss",
+    ),
     task_accuracies=CORE_TASKS[4:9],  # predict 5 metrics
+)
+
+scaling_law_bpb_projection = default_scaling_law_pred(
+    ladder_runs=TASK_BPB_RUNS,
+    pred_run=None,
+    task_losses=TASK_BPBS_TO_PREDICT,
 )
 
 if __name__ == "__main__":
     executor_main(
         steps=[
-            scaling_law_8b_performance_pred,
+            # scaling_law_8b_performance_pred,
+            scaling_law_bpb_projection,
         ],
         description="scaling law suite to predict performance of 8B model on DCLM mix",
     )
