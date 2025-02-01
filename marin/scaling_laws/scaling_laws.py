4ffd8847ee0031a82cdced7685b46c5e5e53676a
Nikil Ravi <nravi@stanford.edu>
2025-01-31 23:47:56 -0800

    wip

--- marin/scaling_laws/scaling_laws.py
+++ marin/scaling_laws/scaling_laws.py
@@ -118,7 +118,7 @@ def log_and_create_report(
     run = wandb.init(
         project=wandb_project,
         entity=wandb_entity,
-        name=f"""Scaling Law Report: {pred_run_id if pred_run_id else 'projection'}- bpb""",
+        name=f"""Scaling Law Report: {pred_run_id if pred_run_id else 'projection'}""",
         tags=["scaling_laws"],
         config={
             "input_runs": input_run_ids,
