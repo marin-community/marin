cfe0f1d5d00fb4c4ed4583925de3924fff4968b5
Nelson Liu <nfliu@nelsonliu.me>
2025-02-01 14:38:05 -0800

    Try loading bloom filter directly from GCS

--- scripts/crawl/deduplicate_outlinks_against_cc.py
+++ scripts/crawl/deduplicate_outlinks_against_cc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Requirements
+Before running, set the authentication key in the environment variable:
 
 ```
-pip install 'rbloom @ git+https://github.com/nelson-liu/rbloom@multiprocessing' orjson tqdm
+export AUTHENTICATION_JSON="$(jq -c . data_browser/gcs-key.json)"
 ```
 
 Running on OpenWebMath:
@@ -11,24 +11,23 @@ Running on OpenWebMath:
 ```
 python scripts/crawl/deduplicate_outlinks_against_cc.py \
     --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8/*_links.jsonl.gz' \
-    --bloom_filter_2013_2018_path '/mnt/disks/bloom_filters/cc-urls-partitioned_2013_2018.bloom' \
-    --bloom_filter_2019_2024_path '/mnt/disks/bloom_filters/cc-urls-partitioned_2019_2024.bloom' \
-    --output_path gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8-cc-deduplicated/ \
-    --num_workers 100
+    --bloom_filter_path '/mnt/disks/bloom_filters/cc-urls-partitioned_2013_2018.bloom' \
+    --output_path gs://marin-us-central2/scratch/nfliu/outlinks/open-web-math-fde8ef8-cc-deduplicated/
 ```
 
 Running on FineWeb-Edu:
 
 ```
-python scripts/crawl/deduplicate_outlinks_against_cc.py \
-    --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu/CC-MAIN*/*_links.jsonl.gz' \
-    --bloom_filter_2013_2018_path '/mnt/disks/bloom_filters/cc-urls-partitioned_2013_2018.bloom' \
-    --bloom_filter_2019_2024_path '/mnt/disks/bloom_filters/cc-urls-partitioned_2019_2024.bloom' \
-    --output_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-cc-deduplicated/ \
-    --num_workers 1
+python marin/run/ray_run.py \
+    --pip_deps 'rbloom @ git+https://github.com/nelson-liu/rbloom@multiprocessing,orjson' \
+    -e "GOOGLE_APPLICATION_CREDENTIALS" "$AUTHENTICATION_JSON" \
+    --no_wait -- \
+    python scripts/crawl/deduplicate_outlinks_against_cc.py \
+        --input_pattern 'gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu/CC-MAIN-2013-20/*_links.jsonl.gz' \
+        --bloom_filter_path 'gs://marin-us-central2/gcsfuse_mount/nfliu/deduplicate_outlinks/cc-urls-partitioned_2013_2018.bloom' \
+        --output_path gs://marin-us-central2/scratch/nfliu/outlinks/fineweb-edu-cc-deduplicated-2013_2018/CC-MAIN-2013-20/
 ```
 """
-import concurrent.futures
 import logging
 import os
 from dataclasses import dataclass
@@ -38,24 +37,19 @@ import draccus
 import orjson
 import fsspec
 from marin.utils import fsspec_exists, fsspec_glob
+import ray
 from rbloom import Bloom
-from tqdm import tqdm
+from tqdm_loggable.auto import tqdm
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 
-bloom_filter_2013_2018: Bloom = None
-bloom_filter_2019_2024: Bloom = None
-
-
 @dataclass
 class DeduplicateOutlinksAgainstCCConfig:
     input_pattern: str
-    bloom_filter_2013_2018_path: str
-    bloom_filter_2019_2024_path: str
+    bloom_filter_path: str
     output_path: str
-    num_workers: int
 
 
 def hash_func(s: str):
@@ -65,7 +59,12 @@ def hash_func(s: str):
     return int.from_bytes(h[:16], "big", signed=True)
 
 
-def deduplicate_shard(shard_path: str, shard_output_path: str, lock) -> tuple[int, int]:
+@ray.remote(memory=350 * 1024 * 1024 * 1024, num_cpus=8)
+def deduplicate_shard(
+    shard_path: str,
+    bloom_filter_path: str,
+    shard_output_path: str,
+) -> tuple[int, int]:
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     success_path = shard_output_path + ".SUCCESS"
     # If the success path exists, read its stats and return
@@ -74,6 +73,11 @@ def deduplicate_shard(shard_path: str, shard_output_path: str, lock) -> tuple[in
         with fsspec.open(success_path) as f:
             successful_stats = orjson.loads(f.read())
             return (successful_stats["num_outlinks"], successful_stats["num_deduplicated_outlinks"])
+
+    logger.info(f"Reading bloom filter {bloom_filter_path}...")
+    object_path = bloom_filter_path.removeprefix("gs://marin-us-central2/")
+    bloom_filter = Bloom.load_from_gcs(bucket="marin-us-central2", object_path=object_path)
+    logger.info(f"Read bloom filter {bloom_filter_path}...")
 
     logger.info(f"Reading links from {os.path.basename(shard_path)}...")
     num_deduplicated_outlinks = 0
@@ -95,11 +99,7 @@ def deduplicate_shard(shard_path: str, shard_output_path: str, lock) -> tuple[in
     hashed_link_targets = [hash_func(ex["link_target"]) for ex in parsed_examples]
     for parsed_example, hashed_link_target in zip(parsed_examples, hashed_link_targets):
         link_target = parsed_example["link_target"]
-        if (
-            hashed_link_target not in bloom_filter_2013_2018
-            and hashed_link_target not in bloom_filter_2019_2024
-            and link_target not in seen_link_targets
-        ):
+        if hashed_link_target not in bloom_filter and link_target not in seen_link_targets:
             deduplicated_examples.append(parsed_example)
             num_deduplicated_outlinks += 1
             seen_link_targets.add(link_target)
@@ -117,56 +117,71 @@ def deduplicate_shard(shard_path: str, shard_output_path: str, lock) -> tuple[in
     return (num_outlinks, num_deduplicated_outlinks)
 
 
+@ray.remote(memory=32 * 1024 * 1024 * 1024, num_cpus=8)
 def deduplicate_outlinks_against_cc(
     input_pattern: str,
-    bloom_filter_2013_2018_path: str,
-    bloom_filter_2019_2024_path: str,
+    bloom_filter_path: str,
     output_path: str,
-    num_workers: int,
 ):
     """
     Args:
     input_pattern (str): Pattern to input outlinks to deduplicate.
-    bloom_filter_2013_2018_path (str): path to bloom filter for links from 2013-2018.
-    bloom_filter_2019_2024_path (str): path to bloom filter for links from 2019-2024.
+    bloom_filter_path (str): path to bloom filter for links
     output_path (str): Write deduplicated outlinks to this directory.
-    num_workers (int): Number of parallel processes to use.
     """
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     # Sort for reproducibility
     shard_paths = sorted(list(fsspec_glob(input_pattern)))
     logger.info(f"Found {len(shard_paths)} shards to process")
 
-    # Load the bloom filter
-    global bloom_filter_2013_2018
-    logger.info("Loading 2013 - 2018 bloom filter")
-    bloom_filter_2013_2018 = Bloom.load(bloom_filter_2013_2018_path)
-    logger.info("Loaded 2013 - 2018 bloom filter")
+    num_shards_to_process = len(shard_paths)
+    MAX_CONCURRENT_TASKS = 10
+    num_shards_submitted = 0
+    unfinished = []
 
-    global bloom_filter_2019_2024
-    logger.info("Loading 2019 - 2024 bloom filter")
-    bloom_filter_2019_2024 = Bloom.load(bloom_filter_2019_2024_path)
-    logger.info("Loaded 2019 - 2024 bloom filter")
+    def submit_shard_task(shard_path):
+        nonlocal num_shards_submitted
+        output_shard_path = os.path.join(output_path, os.path.basename(shard_path))
+        unfinished.append(
+            deduplicate_shard.remote(
+                shard_path,
+                bloom_filter_path,
+                output_shard_path,
+            )
+        )
+        num_shards_submitted += 1
+        if num_shards_submitted % 10 == 0:
+            logger.info(
+                f"Submitted {num_shards_submitted} / {num_shards_to_process} shards "
+                f"({num_shards_submitted / num_shards_to_process})"
+            )
+
+    for _ in range(min(MAX_CONCURRENT_TASKS, len(shard_paths))):
+        submit_shard_task(shard_paths.pop())
 
     num_outlinks = 0
     num_deduplicated_outlinks = 0
+    with tqdm(total=num_shards_to_process, desc="Counting records") as pbar:
+        while unfinished:
+            finished, unfinished = ray.wait(unfinished, num_returns=len(unfinished), timeout=5)
+            try:
+                results = ray.get(finished)
+                for shard_num_outlinks, shard_num_deduplicated_outlinks in results:
+                    num_outlinks += shard_num_outlinks
+                    num_deduplicated_outlinks += shard_num_deduplicated_outlinks
+                    pbar.update(1)
+                    logger.info(
+                        f"So far, found {num_outlinks} total outlinks, {num_deduplicated_outlinks} of "
+                        f"which do not occur in the CC ({num_deduplicated_outlinks/num_outlinks})"
+                    )
+            except Exception as e:
+                logger.exception(f"Error processing shard: {e}")
+                raise
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
-        futures = []
-        for shard_path in tqdm(shard_paths, desc="Submitting futures"):
-            output_shard_path = os.path.join(output_path, os.path.basename(shard_path))
-            futures.append(executor.submit(deduplicate_shard, shard_path, output_shard_path, lock))
-        with tqdm(total=len(shard_paths)) as pbar:
-            for future in concurrent.futures.as_completed(futures):
-                (shard_num_outlinks, shard_num_deduplicated_outlinks) = future.result()
-                num_outlinks += shard_num_outlinks
-                num_deduplicated_outlinks += shard_num_deduplicated_outlinks
-                # Log count so far
-                logger.info(
-                    f"So far, found {num_outlinks} total outlinks, {num_deduplicated_outlinks} of "
-                    f"which do not occur in the CC ({num_deduplicated_outlinks/num_outlinks})"
-                )
-                pbar.update(1)
+            # If we have more shard paths left to process and we haven't hit the max
+            # number of concurrent tasks, add tasks to the unfinished queue.
+            while shard_paths and len(unfinished) < MAX_CONCURRENT_TASKS:
+                submit_shard_task(shard_paths.pop())
 
     logger.info(
         f"In total, found {num_outlinks} total outlinks, {num_deduplicated_outlinks:.1%} of which "
@@ -176,12 +191,12 @@ def deduplicate_outlinks_against_cc(
 
 @draccus.wrap()
 def deduplicate_outlinks_against_cc_driver(cfg: DeduplicateOutlinksAgainstCCConfig):
-    deduplicate_outlinks_against_cc(
-        cfg.input_pattern,
-        cfg.bloom_filter_2013_2018_path,
-        cfg.bloom_filter_2019_2024_path,
-        cfg.output_path,
-        cfg.num_workers,
+    ray.get(
+        deduplicate_outlinks_against_cc.remote(
+            cfg.input_pattern,
+            cfg.bloom_filter_path,
+            cfg.output_path,
+        )
     )
 
 
