5be9f6b6c55d87aa09580c1ff38b65ed67606fd4
Suhas Kotha <kotha.suhas@gmail.com>
2025-02-05 23:00:20 -0800

    config

--- experiments/curriculum/exp702_targeted_curriculum.py
+++ experiments/curriculum/exp702_targeted_curriculum.py
@@ -365,27 +365,6 @@ if __name__ == "__main__":
 
     # Chinchilla scaling 
 
-    stage_pairs = [
-        full_training_varying_mixture(
-            data1_name="flan",
-            data2_name="c4",
-            total_data1_portion=0.005,
-            duration_frac_stage2=duration_frac_stage2,
-            data1_frac_alloc_stage2=1.0,
-            schedule_type="linear",
-            cooldown_frac=0.05,
-            model_size=correct_model_size(model_size),
-            num_train_steps=chinchilla_steps[model_size],
-            learning_rate=learning_rate_dict[model_size],
-            additional_tags=["flan-c4-eu-chinchilla-model-scaling"],
-            version_tag=version_tag(learning_rate_dict[model_size])
-        )
-        for model_size in ["1_9b"]
-        for duration_frac_stage2 in [0.02]
-    ]
-
-    # Token scaling
-
     # stage_pairs = [
     #     full_training_varying_mixture(
     #         data1_name="flan",
@@ -396,15 +375,36 @@ if __name__ == "__main__":
     #         schedule_type="linear",
     #         cooldown_frac=0.05,
     #         model_size=correct_model_size(model_size),
-    #         num_train_steps=num_train_steps,
+    #         num_train_steps=chinchilla_steps[model_size],
     #         learning_rate=learning_rate_dict[model_size],
-    #         additional_tags=["flan-c4-eu-token-scaling"],
+    #         additional_tags=["flan-c4-eu-chinchilla-model-scaling"],
     #         version_tag=version_tag(learning_rate_dict[model_size])
     #     )
-    #     for model_size in ["600m_0.001"]
-    #     for num_train_steps in [48000]
-    #     for duration_frac_stage2 in [0.02]
+    #     for model_size in ["1_9b"]
+    #     for duration_frac_stage2 in [0.01, 0.02, 0.05, 0.1, 0.2, 0.4]
     # ]
+
+    # Token scaling
+
+    stage_pairs = [
+        full_training_varying_mixture(
+            data1_name="flan",
+            data2_name="c4",
+            total_data1_portion=0.005,
+            duration_frac_stage2=duration_frac_stage2,
+            data1_frac_alloc_stage2=1.0,
+            schedule_type="linear",
+            cooldown_frac=0.05,
+            model_size=correct_model_size(model_size),
+            num_train_steps=num_train_steps,
+            learning_rate=learning_rate_dict[model_size],
+            additional_tags=["flan-c4-eu-token-scaling"],
+            version_tag=version_tag(learning_rate_dict[model_size])
+        )
+        for model_size in ["600m_0.001"]
+        for num_train_steps in [48000]
+        for duration_frac_stage2 in [0.01, 0.02, 0.05, 0.1, 0.2, 0.4]
+    ]
 
     # steps = list(chain(*stage_pairs))
 
