c36780be8dfc6b7ff2d80af249bd873a66459ff1
Suhas Kotha <kotha.suhas@gmail.com>
2025-02-07 16:03:14 -0800

    config

--- experiments/curriculum/exp702_targeted_curriculum.py
+++ experiments/curriculum/exp702_targeted_curriculum.py
@@ -386,34 +386,10 @@ if __name__ == "__main__":
     #         version_tag=version_tag(learning_rate_dict[model_size])
     #     )
     #     for model_size in ["1_9b"]
-    #     for duration_frac_stage2 in [0.01, 0.02, 0.05, 0.1, 0.2, 0.4]
+    #     for duration_frac_stage2 in [0.1]
     # ]
 
     # Token scaling
-
-    stage_pairs = [
-        full_training_varying_mixture(
-            data1_name="flan",
-            data2_name="c4",
-            total_data1_portion=0.005,
-            duration_frac_stage2=duration_frac_stage2,
-            data1_frac_alloc_stage2=1.0,
-            schedule_type="linear",
-            cooldown_frac=0.05,
-            model_size=correct_model_size(model_size),
-            num_train_steps=num_train_steps,
-            learning_rate=learning_rate_dict[model_size],
-            additional_tags=["flan-c4-eu-token-scaling"],
-            version_tag=version_tag(learning_rate_dict[model_size])
-        )
-        for model_size in ["600m_0.001"]
-        for num_train_steps in [48000]
-        for duration_frac_stage2 in [0.1]
-    ]
-
-    # steps = list(chain(*stage_pairs))
-
-    # Confirm optimal learning rate schedule
 
     # stage_pairs = [
     #     full_training_varying_mixture(
@@ -423,18 +399,42 @@ if __name__ == "__main__":
     #         duration_frac_stage2=duration_frac_stage2,
     #         data1_frac_alloc_stage2=1.0,
     #         schedule_type="linear",
-    #         cooldown_frac=cooldown_frac,
+    #         cooldown_frac=0.05,
     #         model_size=correct_model_size(model_size),
     #         num_train_steps=num_train_steps,
     #         learning_rate=learning_rate_dict[model_size],
-    #         additional_tags=["flan-c4-eu-confirming-0.05-lr-decay"],
+    #         additional_tags=["flan-c4-eu-token-scaling"],
     #         version_tag=version_tag(learning_rate_dict[model_size])
     #     )
-    #     for model_size in ["600m_0.003"]
-    #     for num_train_steps in [1200]
-    #     for cooldown_frac in [0.02, 0.05, 0.1, 0.2]
-    #     for duration_frac_stage2 in [0.02, 0.05, 0.1, 0.2]
+    #     for model_size in ["600m_0.001"]
+    #     for num_train_steps in [48000]
+    #     for duration_frac_stage2 in [0.01, 0.02, 0.05, 0.1, 0.2, 0.4]
     # ]
+
+    # steps = list(chain(*stage_pairs))
+
+    # Confirm optimal learning rate schedule
+
+    stage_pairs = [
+        full_training_varying_mixture(
+            data1_name="flan",
+            data2_name="c4",
+            total_data1_portion=0.005,
+            duration_frac_stage2=duration_frac_stage2,
+            data1_frac_alloc_stage2=1.0,
+            schedule_type="linear",
+            cooldown_frac=cooldown_frac,
+            model_size=correct_model_size(model_size),
+            num_train_steps=num_train_steps,
+            learning_rate=learning_rate_dict[model_size],
+            additional_tags=["flan-c4-usc2-confirming-0.05-lr-decay"],
+            version_tag=version_tag(learning_rate_dict[model_size])
+        )
+        for model_size in ["600m_0.003"]
+        for num_train_steps in [1200]
+        for cooldown_frac in [0.02, 0.05, 0.1, 0.2]
+        for duration_frac_stage2 in [0.02, 0.05, 0.1, 0.2]
+    ]
 
     steps = list(chain(*stage_pairs))
 
