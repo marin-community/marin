cbaca8ef7c2bb9fc3ac29c21a157402a2b69c3c1
Nikil Ravi <nravi@stanford.edu>
2025-02-08 02:19:17 -0800

    wip

--- experiments/exp780_dolma_suite.py
+++ experiments/exp780_dolma_suite.py
@@ -1,14 +1,12 @@
 """
 This script runs a suite of scaling laws on the Dolma mix.
 
-Link to issue: https://github.com/stanford-crfm/marin/issues/780 
+Link to issue: https://github.com/stanford-crfm/marin/issues/780
 """
 
-from marin.scaling_laws.create_ladder_suite import scaling_law_suite, WS_EMA_DEFAULT_TRAIN_CONFIG
-from marin.execution.executor import executor_main
-from experiments.defaults import default_tokenize
-from experiments.dolma.tokenize_dolma import tokenize_dolma_steps
 from experiments.dolma.exp442_dolma import dolma_llama3_tokenized
+from marin.execution.executor import executor_main
+from marin.scaling_laws.create_ladder_suite import scaling_law_suite
 
 dolma_suite = scaling_law_suite(
     sweep_name="dolma-suite",
