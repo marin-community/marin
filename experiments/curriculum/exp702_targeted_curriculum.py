04cba766c52f3246ef576289e329de4c7e545880
Suhas Kotha <kotha.suhas@gmail.com>
2025-02-10 13:09:55 -0800

    config

--- experiments/curriculum/exp702_targeted_curriculum.py
+++ experiments/curriculum/exp702_targeted_curriculum.py
@@ -415,27 +415,48 @@ if __name__ == "__main__":
 
     # Confirm optimal learning rate schedule
 
+    # stage_pairs = [
+    #     full_training_varying_mixture(
+    #         data1_name="flan",
+    #         data2_name="c4",
+    #         total_data1_portion=0.005,
+    #         duration_frac_stage2=duration_frac_stage2,
+    #         data1_frac_alloc_stage2=1.0,
+    #         schedule_type="linear",
+    #         cooldown_frac=cooldown_frac,
+    #         model_size=correct_model_size(model_size),
+    #         num_train_steps=num_train_steps,
+    #         learning_rate=learning_rate_dict[model_size],
+    #         additional_tags=["flan-c4-usc2-confirming-0.05-lr-decay"],
+    #         version_tag=version_tag(learning_rate_dict[model_size])
+    #     )
+    #     for model_size in ["600m_0.003"]
+    #     for num_train_steps in [12000]
+    #     for cooldown_frac in [0.02, 0.05, 0.1, 0.2]
+    #     for duration_frac_stage2 in [0.1]
+    #     # for cooldown_frac in [0.02, 0.05, 0.1, 0.2]
+    #     # for duration_frac_stage2 in [0.05, 0.1, 0.2]
+    # ]
+
     stage_pairs = [
         full_training_varying_mixture(
             data1_name="flan",
             data2_name="c4",
             total_data1_portion=0.005,
             duration_frac_stage2=duration_frac_stage2,
-            data1_frac_alloc_stage2=1.0,
+            data1_frac_alloc_stage2=data1_frac_alloc_stage2,
             schedule_type="linear",
-            cooldown_frac=cooldown_frac,
+            cooldown_frac=0.05,
             model_size=correct_model_size(model_size),
             num_train_steps=num_train_steps,
             learning_rate=learning_rate_dict[model_size],
-            additional_tags=["flan-c4-usc2-confirming-0.05-lr-decay"],
+            additional_tags=["flan-c4-usc2-confirming-all-stage2"],
             version_tag=version_tag(learning_rate_dict[model_size])
         )
         for model_size in ["600m_0.003"]
         for num_train_steps in [12000]
-        # for cooldown_frac in [0.02, 0.05, 0.1, 0.2]
-        # for duration_frac_stage2 in [0.02]
-        for cooldown_frac in [0.02, 0.05, 0.1, 0.2]
-        for duration_frac_stage2 in [0.05, 0.1, 0.2]
+        for data1_frac_alloc_stage2 in [0.25, 0.5, 0.75]
+        for duration_frac_stage2 in [0.02, 0.05, 0.1, 0.2]
     ]
 
     steps = list(chain(*stage_pairs))
