a4c13ad8295e6618d87275159525f1f919d5d05f
chiheem <15081354+chiheem@users.noreply.github.com>
2025-02-11 16:43:37 -0800

    ruff

--- experiments/instruction_datasets.py
+++ experiments/instruction_datasets.py
@@ -154,7 +154,10 @@ def download_dataset_step(dataset: InstructionDatasetConfig) -> ExecutorStep:
     return download_step
 
 
-def transform_dataset_step(dataset_cfg: InstructionDatasetConfig, download_step: ExecutorStep) -> ExecutorStep:
+def transform_dataset_step(
+    dataset_cfg: InstructionDatasetConfig,
+    download_step: ExecutorStep
+    ) -> ExecutorStep:
     """ExecutorStep that preprocesses and shards the input dataset.
 
     ===========================================================================
@@ -206,7 +209,10 @@ def transform_dataset_step(dataset_cfg: InstructionDatasetConfig, download_step:
     return transform_step
 
 
-def get_instruction_dataset(hf_dataset_id: str, splits: list[str] = ["train"]) -> ExecutorStep:
+def get_instruction_dataset(
+    hf_dataset_id: str,
+    splits: list[str] = field(default_factory=lambda: ["train"])
+    ) -> ExecutorStep:
     # Check that config exists
     assert hf_dataset_id in INSTRUCTION_DATASET_NAME_TO_CONFIG, f"Unknown instruction dataset: {hf_dataset_id}"
 
