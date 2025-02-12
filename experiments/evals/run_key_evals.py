bfc0910718940471cf211276c529d1b87b6be98e
Christopher Chou <emu4yolife@gmail.com>
2025-02-12 02:17:27 +0000

    Add comments

--- experiments/evals/run_key_evals.py
+++ experiments/evals/run_key_evals.py
@@ -2,6 +2,7 @@ from experiments.evals.evals import default_key_evals
 from experiments.evals.resource_configs import SINGLE_TPU_V6E_8
 from marin.execution.executor import executor_main
 
+# Insert your model path here
 model_path = "gs://marin-us-central2/checkpoints/llama3.1_8b_tulu_3_seed0_alldocs-379cf0/hf/step-7334"
 
 key_evals = default_key_evals(
