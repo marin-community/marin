d69e408f6765793193ad354bf0d7711b605274db
Christopher Chou <emu4yolife@gmail.com>
2025-02-12 05:49:30 +0000

    Ease constraint on the max model length

--- experiments/evals/engine_configs.py
+++ experiments/evals/engine_configs.py
@@ -1,5 +1,5 @@
 """Engine configuration for vLLM used for evals."""
 
 DEFAULT_VLLM_ENGINE_KWARGS = {
-    "max_model_len": 8192,
+    "max_model_len": 4096,
 }
--- experiments/evals/run_key_evals.py
+++ experiments/evals/run_key_evals.py
@@ -3,11 +3,12 @@ from experiments.evals.resource_configs import SINGLE_TPU_V6E_8
 from marin.execution.executor import executor_main
 
 # Insert your model path here
-model_path = "gs://marin-us-central2/checkpoints/llama3.1_8b_tulu_3_seed0_alldocs-379cf0/hf/step-7334"
+model_path = "gs://marin-us-central2/checkpoints/llama-8b-control-00f31b/hf/step-210388"
 
 key_evals = default_key_evals(
     step=model_path,
     resource_config=SINGLE_TPU_V6E_8,
+    model_name="llama-8b-control-00f31b",
 )
 
 if __name__ == "__main__":
