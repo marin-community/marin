e4063cad3b458f49777a0f52a97e748dba4a6724
David Hall <dlwh@stanford.edu>
2025-02-14 16:17:20 -0800

    wip

--- experiments/exp750_tootsie70b.py
+++ experiments/exp750_tootsie70b.py
@@ -197,7 +197,7 @@ llama_70b_tootsie_warmstart = dataclasses.replace(
 # warmstarted from llama_22b_train_config at 200,000
 llama_22b_train_config_ema = SimpleTrainConfig(
     tpu_type="v6e-128",
-    node_count=6,
+    node_count=4,
     # train_batch_size=1024,
     train_batch_size=[ScheduleStep(until=200_000, value=1024), ScheduleStep(until=-1, value=3072)],
     num_train_steps=1_000_000,
