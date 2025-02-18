afe98061cfbb3d17aa47afeadfeeae0e2cb1dbfe
Christopher Chou <emu4yolife@gmail.com>
2025-02-18 21:35:48 +0000

    Save

--- experiments/test_xla_infer.py
+++ experiments/test_xla_infer.py
@@ -2,19 +2,15 @@ import ray
 import torch_xla.distributed.xla_multiprocessing as xmp
 
 
-def _mp_fn(rank: int):
-    print(f"Hello, world! from rank {rank}")
+def _mp_fn(rank):
+    print(f"Hello from rank {rank}")
 
 
-@ray.remote(
-    num_cpus=8,
-    resources={"TPU": 8, "TPU-v6e-8-head": 1},
-    runtime_env={"env_vars": {"TPU_NUM_DEVICES": "8", "PJRT_DEVICE": "TPU"}},
-)
-def test_xla_infer():
-
-    xmp.spawn(_mp_fn, args=())
+@ray.remote(num_cpus=10, resources={"TPU": 8, "TPU-v6e-8-head": 1})
+def run_on_tpu():
+    xmp.spawn(_mp_fn)
 
 
 if __name__ == "__main__":
-    ray.get(test_xla_infer.remote())
+    future = run_on_tpu.remote()
+    ray.get(future)
