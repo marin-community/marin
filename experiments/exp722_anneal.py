cacac1ed5311f17efb224619c957c7283764a26f
Christopher Chou <emu4yolife@gmail.com>
2025-02-19 19:32:31 +0000

    Remove commented code

--- experiments/exp722_anneal.py
+++ experiments/exp722_anneal.py
@@ -32,13 +32,11 @@ control_model = default_anneal(name="llama-8b-anneal-dclm", anneal_config=contro
 annealed_model = default_anneal(name="llama-8b-anneal-finemath-dclm", anneal_config=finemath_anneal_config)
 
 eval_anneal_on_finemath_with_fineweb_edu = default_eval(
-    # step=anneal_on_finemath_with_fineweb_edu, # not working
     step="gs://marin-us-central2/checkpoints/llama-8b-anneal-finemath-fd2597/hf/step-210388",
     evals=MMLU_TASKS,
 )
 
 eval_control_model = default_eval(
-    # step=anneal_on_finemath_with_fineweb_edu, # not working
     step="gs://marin-us-central2/checkpoints/llama-8b-control-00f31b/hf/step-210388",
     evals=MMLU_TASKS,
 )
