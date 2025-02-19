500689a0e919edd39bdfc61f0123754c062e944c
Christopher Chou <emu4yolife@gmail.com>
2025-02-19 19:32:14 +0000

    Remove commented code

--- experiments/exp722_anneal.py
+++ experiments/exp722_anneal.py
@@ -1,6 +1,8 @@
 from experiments.anneal_config import AnnealConfig
 from experiments.defaults import default_anneal
 from experiments.dolmino.tokenize_dolmino import get_dolmino_step
+from experiments.evals.evals import default_eval
+from experiments.evals.task_configs import MMLU_TASKS
 from experiments.midtraining_datasets import finemath_3_plus_tokenized
 from marin.execution.executor import executor_main
 from marin.processing.tokenize.data_configs import lm_mixture_data_config
@@ -29,35 +31,25 @@ control_model = default_anneal(name="llama-8b-anneal-dclm", anneal_config=contro
 
 annealed_model = default_anneal(name="llama-8b-anneal-finemath-dclm", anneal_config=finemath_anneal_config)
 
+eval_anneal_on_finemath_with_fineweb_edu = default_eval(
+    # step=anneal_on_finemath_with_fineweb_edu, # not working
+    step="gs://marin-us-central2/checkpoints/llama-8b-anneal-finemath-fd2597/hf/step-210388",
+    evals=MMLU_TASKS,
+)
 
-# fineweb_edu_finemath_anneal_config = AnnealConfig(
-#     target_dataset=finemath_3_plus_tokenized,
-#     high_quality_web_text_dataset=fineweb_edu_tokenized,
-# )
-# anneal_on_finemath_with_fineweb_edu = default_anneal(
-#     name="llama-8b-anneal-finemath",
-#     anneal_config=fineweb_edu_finemath_anneal_config,
-# )
-
-# eval_anneal_on_finemath_with_fineweb_edu = default_eval(
-#     # step=anneal_on_finemath_with_fineweb_edu, # not working
-#     step="gs://marin-us-central2/checkpoints/llama-8b-anneal-finemath-fd2597/hf/step-210388",
-#     evals=MMLU_TASKS,
-# )
-
-# eval_control_model = default_eval(
-#     # step=anneal_on_finemath_with_fineweb_edu, # not working
-#     step="gs://marin-us-central2/checkpoints/llama-8b-control-00f31b/hf/step-210388",
-#     evals=MMLU_TASKS,
-# )
+eval_control_model = default_eval(
+    # step=anneal_on_finemath_with_fineweb_edu, # not working
+    step="gs://marin-us-central2/checkpoints/llama-8b-control-00f31b/hf/step-210388",
+    evals=MMLU_TASKS,
+)
 
 if __name__ == "__main__":
     executor_main(
         steps=[
             annealed_model,
             control_model,
-            # eval_anneal_on_finemath_with_fineweb_edu,
-            # eval_control_model,
+            eval_anneal_on_finemath_with_fineweb_edu,
+            eval_control_model,
         ],
         description="Train 8B model on DCLM using WSD-S.",
     )
