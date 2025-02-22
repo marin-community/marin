06b84335466427718f1ba4502e57f121aa4b6977
Herumb Shandilya <herumbshandilya123@gmail.com>
2025-02-22 01:37:44 -0800

    lint fix and comment resolves

--- experiments/exp647_wikipedia_training.py
+++ experiments/exp647_wikipedia_training.py
@@ -5,32 +5,35 @@ https://github.com/stanford-crfm/marin/issues/647
 
 import logging
 
-from experiments.defaults import default_train
+from experiments.defaults import default_tokenize, default_train
 from experiments.dolma.tokenize_dolma import DOLMA_OLMO_MIXTURE_WEIGHTS, tokenize_dolma_steps
 from experiments.evals.evals import default_eval
-from experiments.llama import llama_1_4b, llama_1_4b_train_config
+from experiments.exp575_wikipedia_markdownify import wikipedia_readability, wikipedia_resiliparse_with_pf
+from experiments.llama import llama3_tokenizer, llama_1_4b, llama_1_4b_train_config
 from marin.execution.executor import executor_main
 from marin.processing.tokenize.data_configs import lm_mixture_data_config
-from marin.utils import fsspec_glob
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("ray")
 
 EXPERIMENT_TAG = ["wiki_subbed_dolma"]
 
-weights = DOLMA_OLMO_MIXTURE_WEIGHTS.pop("dolma/wiki")
-DOLMA_OLMO_MIXTURE_WEIGHTS["wiki-subbed-readability-no-references"] = weights
+tokenized_dolma_steps = tokenize_dolma_steps()
 
-wiki_readability_path = "gs://marin-us-central2/documents/wikipedia-readability-a03369/20241201"
-wiki_readability_files = fsspec_glob(f"{wiki_readability_path}/*.jsonl.gz")
-logger.info(f"Found {len(wiki_readability_files)} files in {wiki_readability_files}")
-wiki_readability_subbed_dolma_tokenized = tokenize_dolma_steps(
-    substitute={"wiki": wiki_readability_files},
-    prefix="readability-no-references",
+wiki_readability_tokenized = default_tokenize(
+    name="dolma-wiki-readability-tokenized",
+    dataset=wikipedia_readability,
+    tokenizer=llama3_tokenizer,
 )
+
+dolma_wiki_readability_tokenization_steps = dict(tokenized_dolma_steps, {"wiki-readability": wiki_readability_tokenized})
+wiki_readability_weights = dict(
+    DOLMA_OLMO_MIXTURE_WEIGHTS, {"wiki-readability": DOLMA_OLMO_MIXTURE_WEIGHTS["dolma/wiki"]}
+)
+
 wiki_readability_subbed_dolma_llama3_tokenized = lm_mixture_data_config(
-    components=wiki_readability_subbed_dolma_tokenized,
-    weights=DOLMA_OLMO_MIXTURE_WEIGHTS,
+    components=dolma_wiki_readability_tokenization_steps,
+    weights=wiki_readability_weights,
 )
 
 wiki_readablity_1_4b_subbed_dolma_model = default_train(
@@ -43,22 +46,23 @@ wiki_readablity_1_4b_subbed_dolma_model = default_train(
 
 wiki_readablity_1_4b_subbed_dolma_evals = default_eval(step=wiki_readablity_1_4b_subbed_dolma_model)
 
+wiki_resiliparse_with_preserve_formatting_tokenized = default_tokenize(
+    name="dolma-wiki-resiliparse-with-preserving-formatting-tokenized",
+    dataset=wikipedia_resiliparse_with_pf,
+    tokenizer=llama3_tokenizer,
+)
 
-weights = DOLMA_OLMO_MIXTURE_WEIGHTS.pop("wiki-subbed-readability-no-references")
-DOLMA_OLMO_MIXTURE_WEIGHTS["wiki-subbed-resiliparse-with-preserving-formatting-no-references"] = weights
+dolma_wiki_resiliparse_with_preserve_formatting_tokenization_steps = dict(
+    tokenized_dolma_steps,
+    {"wiki-resiliparse-with-preserving-formatting": wiki_resiliparse_with_preserve_formatting_tokenized},
+)
+wiki_resiliparse_with_preserve_formatting_weights = dict(
+    DOLMA_OLMO_MIXTURE_WEIGHTS, {"wiki-resiliparse-with-preserving-formatting": DOLMA_OLMO_MIXTURE_WEIGHTS["dolma/wiki"]}
+)
 
-wiki_resiliparse_with_preserve_formatting_path = (
-    "gs://marin-us-central2/documents/wikipedia-resiliparse-with-preserving-formatting-0cccb4/20241201"
-)
-wiki_resiliparse_with_preserve_formatting_files = fsspec_glob(
-    f"{wiki_resiliparse_with_preserve_formatting_path}/*.jsonl.gz"
-)
-wiki_resiliparse_with_preserve_formatting_subbed_dolma_tokenized = tokenize_dolma_steps(
-    substitute={"wiki": wiki_resiliparse_with_preserve_formatting_files},
-    prefix="resiliparse-with-preserving-formatting-no-references",
-)
 wiki_resiliparse_with_preserve_formatting_subbed_dolma_llama3_tokenized = lm_mixture_data_config(
-    components=wiki_resiliparse_with_preserve_formatting_subbed_dolma_tokenized, weights=DOLMA_OLMO_MIXTURE_WEIGHTS
+    components=dolma_wiki_resiliparse_with_preserve_formatting_tokenization_steps,
+    weights=wiki_resiliparse_with_preserve_formatting_weights,
 )
 wiki_resiliparse_with_preserve_formatting_subbed_dolma_1_4b_model = default_train(
     name="dolma-wiki-subbed-1.4b-resiliparse-with-preserving-formatting-no-references",
@@ -74,12 +78,15 @@ wiki_resiliparse_with_preserve_formatting_subbed_dolma_1_4b_evals = default_eval
 
 
 if __name__ == "__main__":
-    wiki_readability_subbed_dolma_tokenized.update(wiki_resiliparse_with_preserve_formatting_subbed_dolma_tokenized)
-    tokenization_steps = wiki_readability_subbed_dolma_tokenized.values()
+    tokenization_step = (
+        list(tokenized_dolma_steps.values())
+        + list(dolma_wiki_readability_tokenization_steps.values())
+        + list(dolma_wiki_resiliparse_with_preserve_formatting_tokenization_steps.values())
+    )
 
     executor_main(
         steps=[
-            *tokenization_steps,
+            *tokenization_step,
             wiki_readablity_1_4b_subbed_dolma_model,
             wiki_readablity_1_4b_subbed_dolma_evals,
             wiki_resiliparse_with_preserve_formatting_subbed_dolma_1_4b_model,
