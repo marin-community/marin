94aa572e2744e8804e30da9fdf79e8bccd6fcd23
Christopher Chou <emu4yolife@gmail.com>
2025-02-24 04:30:19 +0000

    Undo

--- experiments/evals/run_alpaca_eval.py
+++ experiments/evals/run_alpaca_eval.py
@@ -5,8 +5,9 @@ from marin.execution.executor import ExecutorMainConfig, executor_main
 executor_main_config = ExecutorMainConfig()
 steps = [
     evaluate_alpaca_eval(
-        model_name="tulu-3-8b-sft",
-        model_path="gs://marin-us-east5/gcsfuse_mount/models/allenai--Llama-3-1-Tulu-3-8B-SFT",
+        model_name="pf5pe4ut/step-600",
+        model_path="gs://marin-us-central2/checkpoints/quickstart_single_script_docker_test_09_18/"
+        "pf5pe4ut/hf/pf5pe4ut/step-600",
         resource_config=SINGLE_TPU_V6E_8,
     ),
 ]
