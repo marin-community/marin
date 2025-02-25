19c76ceb9e485d576bc9b7244f663b1e5760251b
Nikil Ravi <nravi@stanford.edu>
2025-02-24 21:55:24 -0800

    wip

--- experiments/exp780_default_suite.py
+++ experiments/exp780_default_suite.py
@@ -19,30 +19,17 @@ default_suite = scaling_law_suite(
     tags=["scaling_laws"],
 )
 
-# RUNS = [
-#     "tootsie-scaling-512-81c36c",
-#     "tootsie-scaling-768-d17a90",
-#     "tootsie-scaling-1024-f4e4be",
-#     "tootsie-scaling-1536-e2a6d8",
-#     "tootsie-scaling-2048-72c648",
-# ]
 
 default_suite_scaling_laws_pred = default_scaling_law_pred(
-    ladder_runs=default_suite[:-1], # all but last run
-    #pred_run="llama-8b-tootsie-0.001-19ad63", # default_suite[:],
-    pred_run=default_suite[-1],
-    #"llama-22b-tootsie-dummy-testing-373d53",#"llama-13b-tootsie-ema","llama-8b-tootsie-0.001-19ad63",
+    ladder_runs=default_suite,
+    pred_run="llama-8b-tootsie-0.001-19ad63",
     task_losses=(
         "eval/paloma/c4_en/bpb",
         "eval/bpb",
         "eval/loss",
-        "lm_eval/hellaswag_10shot/bpb",
-        "lm_eval/hellaswag_0shot/bpb",
-        "lm_eval/boolq/bpb",
-        "lm_eval/copa/bpb",
+        "eval/paloma/c4_en/loss",
     ),
-    #task_accuracies=None
-    task_accuracies=CORE_TASKS_PLUS_MMLU,
+    task_accuracies=CORE_TASKS,
 )
 
 
@@ -50,7 +37,7 @@ default_suite_scaling_laws_pred = default_scaling_law_pred(
 if __name__ == "__main__":
     executor_main(
         steps=[
-            #*default_suite,
+            *default_suite,
             default_suite_scaling_laws_pred,
         ],
         description="suite + predictions for scaling laws on DCLM-Baseline+StarCoder+ProofPile mix",
--- experiments/exp780_dolma_suite.py
+++ experiments/exp780_dolma_suite.py
@@ -8,7 +8,7 @@ from experiments.dolma.exp442_dolma import dolma_llama3_tokenized
 from marin.execution.executor import executor_main
 from marin.scaling_laws.create_ladder_suite import scaling_law_suite
 from defaults import default_scaling_law_pred
-from experiments.evals.task_configs import CORE_TASKS, CORE_TASKS_PLUS_MMLU
+from experiments.evals.task_configs import CORE_TASKS_PLUS_MMLU
 
 dolma_suite = scaling_law_suite(
     sweep_name="scaling-law-suite-dolma",
@@ -16,21 +16,9 @@ dolma_suite = scaling_law_suite(
     tags=["scaling_laws"],
 )
 
-
-# RUNS = [
-#     "tootsie-scaling-512-81c36c",
-#     "tootsie-scaling-768-d17a90",
-#     "tootsie-scaling-1024-f4e4be",
-#     "tootsie-scaling-1536-e2a6d8",
-#     "tootsie-scaling-2048-72c648",
-# ]
-
 dolma_suite_scaling_laws_pred = default_scaling_law_pred(
-    ladder_runs=dolma_suite[:-1],
-    #pred_run="scaling-law-suite-dolma-2048-acd8a1",
-    #"llama-22b-tootsie-dummy-testing-373d53",#"llama-13b-tootsie-ema","llama-8b-tootsie-0.001-19ad63",
-    pred_run=dolma_suite[-1],
-    #"llama-22b-tootsie-dummy-testing-373d53",#"llama-13b-tootsie-ema","llama-8b-tootsie-0.001-19ad63",
+    ladder_runs=dolma_suite,
+    pred_run=None, # this will give us readouts at various scales
     task_losses=(
         "eval/paloma/c4_en/bpb",
         "eval/bpb",
@@ -40,7 +28,6 @@ dolma_suite_scaling_laws_pred = default_scaling_law_pred(
         "lm_eval/boolq/bpb",
         "lm_eval/copa/bpb",
     ),
-    #task_accuracies=None
     task_accuracies=CORE_TASKS_PLUS_MMLU,
 )
 
@@ -49,7 +36,7 @@ dolma_suite_scaling_laws_pred = default_scaling_law_pred(
 if __name__ == "__main__":
     executor_main(
         steps=[
-            #*dolma_suite,
+            *dolma_suite,
             dolma_suite_scaling_laws_pred,
         ],
         description="suite for scaling laws on Dolma mix",
