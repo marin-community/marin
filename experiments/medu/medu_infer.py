7739ad49f67f4d98dda267d0fab6ace4ba750b61
Christopher Chou <emu4yolife@gmail.com>
2025-02-28 05:11:11 +0000

    Save

--- experiments/medu/medu_infer.py
+++ experiments/medu/medu_infer.py
@@ -1,9 +1,7 @@
-from experiments.llama import llama3_tokenizer
 from marin.execution.executor import ExecutorStep, executor_main, output_path_of, this_output_path
 from marin.processing.classification.config.inference_config import InferenceConfig, RuntimeConfig, TaskConfig
 from marin.processing.classification.consolidate import ConsolidateConfig, FilterConfig, consolidate
 from marin.processing.classification.inference import run_inference
-from marin.processing.tokenize import TokenizeConfig, tokenize
 
 input_data_path = "gs://marin-us-central2/raw/dclm/a3b142c/huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/resolve/a3b142c/global-shard_01_of_10"
 medu_inference = ExecutorStep(
@@ -47,19 +45,5 @@ medu_consolidate = ExecutorStep(
     pip_dependency_groups=["ddsketch"],
 )
 
-BASE_MEDU_PATH = "gs://marin-us-east5/documents/quality_filtering/dclm-global-shard-01-of-10-medu-economics-3plus-bbb96b"
-FILE_PATTERN = "**/*.jsonl.zst"
-medu_economics_tokenized = ExecutorStep(
-    name="tokenized/medu-economics-3plus",
-    fn=tokenize,
-    config=TokenizeConfig(
-        train_paths=[f"{BASE_MEDU_PATH}/{FILE_PATTERN}"],
-        validation_paths=[],
-        cache_path=this_output_path(),
-        tokenizer=llama3_tokenizer,
-    ),
-    pip_dependency_groups=["tokenize_train"],
-)
-
 if __name__ == "__main__":
-    executor_main([medu_inference, medu_consolidate, medu_economics_tokenized])
+    executor_main([medu_inference, medu_consolidate])
