b739db44e175559aec457376a8d1993442271abf
Helw150 <wbh230@nyu.edu>
2025-03-03 14:28:36 -0500

    Int8 Cooldown Fixs

--- experiments/exp848_int8cooldown.py
+++ experiments/exp848_int8cooldown.py
@@ -1,38 +1,37 @@
-"""An experiment to evaluate the quality of individual splits of the Dolmino dataset.
+"""An experiment comparing standard v.s. int8 cooldown
 
-We cooldown a 8B model on a 30/70 mixture of some high quality Dolmino split and Dolmino DCLM.
-Link to issue: https://github.com/stanford-crfm/marin/issues/820
+We cooldown a 8B model on DCLM baine using both bf16 and int8 training to compare the results.
+Link to issue: https://github.com/stanford-crfm/marin/issues/848
 """
 
-from experiments.anneal_config import AnnealConfig
 import dataclasses
+
 from haliax.quantization import QuantizationConfig
-from experiments.defaults import default_anneal
+
+from experiments.anneal_config import AnnealConfig
 from experiments.dclm.tokenize_dclm import dclm_components_llama3
-from experiments.dolma.tokenize_dolma import tokenize_dolma_steps
-from experiments.dolmino.tokenize_dolmino import get_dolmino_step, DOLMINO_DATASETS
-from experiments.midtraining_datasets import finemath_3_plus_tokenized
-from experiments.exp72_baselines import fineweb_edu_tokenized
+from experiments.defaults import default_anneal
 from marin.execution.executor import executor_main
 from marin.processing.tokenize.data_configs import lm_mixture_data_config
 
-dolmino_dclm = dclm_components_llama3["dclm_baseline"]
+dclm = dclm_components_llama3["dclm_baseline"]
 
 NUM_ANNEAL_TOKENS = 50_000_000_000
 
-# Control model is 100% dolmino DCLM dataset
 control_dataset_config = lm_mixture_data_config(
-    components={"dolmino_dclm": dolmino_dclm},
-    weights={"dolmino_dclm": 1.0},
+    components={"dclm": dclm},
+    weights={"dclm": 1.0},
 )
 control_anneal_config = AnnealConfig(
     dataset_config=control_dataset_config, num_anneal_training_tokens=NUM_ANNEAL_TOKENS, tpu_type="v5litepod-128"
 )
 
-control_model = default_anneal(name="llama-8b-anneal-control-eu-long", anneal_config=control_anneal_config)
+control_model = default_anneal(name="llama-8b-anneal-bf16-control", anneal_config=control_anneal_config)
 
-int8_config = dataclasses.replace(control_model.config, quantization=QuantizationConfig(int8=True))
-int8_model = dataclasses.replace(control_model, config=int8_config)
+# Keep everything fixed except for int8 training.
+int8_trainer = dataclasses.replace(control_model.config.trainer, quantization=QuantizationConfig(int8=True))
+int8_config = dataclasses.replace(control_model.config, trainer=int8_trainer)
+int8_model = dataclasses.replace(control_model, config=int8_config, name="llama-8b-anneal-int8")
 
 
 if __name__ == "__main__":
