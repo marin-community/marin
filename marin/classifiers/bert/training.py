c7a54997fbb45222d317e97e08e24cbf536cf195
Nelson Liu <nfliu@nelsonliu.me>
2025-03-20 15:10:48 -0700

    Turn keep_in_memory back on

--- marin/classifiers/bert/training.py
+++ marin/classifiers/bert/training.py
@@ -98,11 +98,12 @@ def _mp_fn(
             max_length=bert_args.max_length,
         )
 
-    train_dataset = train_dataset.map(tokenize, batched=True, num_proc=8)
+    # Keep dataset in memory to avoid filling up TPU /tmp
+    train_dataset = train_dataset.map(tokenize, batched=True, num_proc=8, keep_in_memory=True)
     train_dataset = train_dataset.remove_columns(["text"])
     train_dataset = train_dataset.class_encode_column("label")
 
-    val_dataset = val_dataset.map(tokenize, batched=True, num_proc=8)
+    val_dataset = val_dataset.map(tokenize, batched=True, num_proc=8, keep_in_memory=True)
     val_dataset = val_dataset.remove_columns(["text"])
     val_dataset = val_dataset.class_encode_column("label")
 
