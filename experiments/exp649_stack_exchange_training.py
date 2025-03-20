378d7900978c36e1cba211f1a41f7095498dc0f0
Herumb Shandilya <herumbshandilya123@gmail.com>
2025-03-19 23:12:37 -0700

    deduping steps and dosctring updates

--- experiments/exp649_stack_exchange_training.py
+++ experiments/exp649_stack_exchange_training.py
@@ -26,59 +26,66 @@ from marin.execution.executor import executor_main
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("ray")
 
-# Tokenize the threaded markdown
-stack_exchange_threaded_tokenized = default_tokenize(
-    name="stack-exchange-threaded",
-    dataset=stackexchange_text_resiliparse_custom_fork,
-    tokenizer=llama3_tokenizer,
+
+def get_stack_exchange_training_steps(
+    name: str,
+    dataset: str,
+):
+    """
+    Returns the tokenized, trained, and evaluated ExecutorSteps for the Stack Exchange dataset.
+
+    Args:
+        name: The name of the experiment. This is used as a prefix for the tokenized dataset and model names.
+        dataset: The dataset to use for the experiment. For stackexachange we have 3 variants: threaded,
+                 accepted answer only, and isolated. We need to pass the path/ExecutorStep object for the dataset
+                 we want to use for the experiment.
+
+    Returns:
+        A tuple of the tokenized, trained, and evaluated ExecutorSteps.
+    """
+    tokenized_stackexchange = default_tokenize(
+        name=name,
+        dataset=dataset,
+        tokenizer=llama3_tokenizer,
+    )
+
+    tokenized_stackexchange_1_4b_model = default_train(
+        name=f"{name}-1.4b",
+        tokenized=tokenized_stackexchange,
+        model_config=llama_1_4b,
+        train_config=llama_1_4b_train_config,
+    )
+
+    tokenized_stackexchange_1_4b_evals = default_eval(step=tokenized_stackexchange_1_4b_model)
+
+    return tokenized_stackexchange, tokenized_stackexchange_1_4b_model, tokenized_stackexchange_1_4b_evals
+
+
+# Execute steps for the threaded dataset experiment pipeline
+stack_exchange_threaded_tokenized, stack_exchange_threaded_1_4b_model, stack_exchange_threaded_1_4b_evals = (
+    get_stack_exchange_training_steps(
+        name="stack-exchange-threaded",
+        dataset=stackexchange_text_resiliparse_custom_fork,
+    )
 )
 
-# Train the threaded model
-stack_exchange_threaded_1_4b_model = default_train(
-    name="stack-exchange-threaded-1.4b",
-    tokenized=stack_exchange_threaded_tokenized,
-    model_config=llama_1_4b,
-    train_config=llama_1_4b_train_config,
-)
-
-# Evaluate the threaded model
-stack_exchange_threaded_1_4b_evals = default_eval(step=stack_exchange_threaded_1_4b_model)
-
-# Tokenize the accepted answer only markdown
-stack_exchange_accepted_answer_only_tokenized = default_tokenize(
+# Execute steps for the accepted answer only dataset experiment pipeline
+(
+    stack_exchange_accepted_answer_only_tokenized,
+    stack_exchange_accepted_answer_only_1_4b_model,
+    stack_exchange_accepted_answer_only_1_4b_evals,
+) = get_stack_exchange_training_steps(
     name="stack-exchange-accepted-answer-only",
-    dataset="gs://marin-us-central2/documents/stackexchange/v2024-04-02/md-seperate",
-    tokenizer=llama3_tokenizer,
-)
-
-# Train the accepted answer only model
-stack_exchange_accepted_answer_only_1_4b_model = default_train(
-    name="stack-exchange-accepted-answer-only-1.4b",
-    tokenized=stack_exchange_accepted_answer_only_tokenized,
-    model_config=llama_1_4b,
-    train_config=llama_1_4b_train_config,
-)
-
-# Evaluate the accepted answer only model
-stack_exchange_accepted_answer_only_1_4b_evals = default_eval(step=stack_exchange_accepted_answer_only_1_4b_model)
-
-# Tokenize the isolated markdown
-stack_exchange_isolated_tokenized = default_tokenize(
-    name="stack-exchange-isolated",
     dataset="gs://marin-us-central2/documents/stackexchange/v2024-04-02/md-qa-pair",
-    tokenizer=llama3_tokenizer,
 )
 
-# Train the isolated model
-stack_exchange_isolated_1_4b_model = default_train(
-    name="stack-exchange-isolated-1.4b",
-    tokenized=stack_exchange_isolated_tokenized,
-    model_config=llama_1_4b,
-    train_config=llama_1_4b_train_config,
+# Execute steps for the isolated dataset experiment pipeline
+stack_exchange_isolated_tokenized, stack_exchange_isolated_1_4b_model, stack_exchange_isolated_1_4b_evals = (
+    get_stack_exchange_training_steps(
+        name="stack-exchange-isolated",
+        dataset="gs://marin-us-central2/documents/stackexchange/v2024-04-02/md-seperate",
+    )
 )
-
-# Evaluate the isolated model
-stack_exchange_isolated_1_4b_evals = default_eval(step=stack_exchange_isolated_1_4b_model)
 
 
 if __name__ == "__main__":
