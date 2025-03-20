7317991deb0e59a0963b277a8fea5040962a5510
Herumb Shandilya <herumbshandilya123@gmail.com>
2025-03-19 21:48:24 -0700

    update to default_eval

--- experiments/exp246_web_extraction_method_evaluation.py
+++ experiments/exp246_web_extraction_method_evaluation.py
@@ -1,85 +1,31 @@
-from experiments.evals.task_configs import CORE_TASKS
-from marin.evaluation.evaluation_config import EvaluationConfig
-from marin.evaluation.run import evaluate
-from marin.execution.executor import ExecutorStep, executor_main, this_output_path
-
-evaluate_readability = ExecutorStep(
-    name="evaluation/lm_evaluation_harness/fineweb-small-readability-1.4b",
-    fn=evaluate,
-    config=EvaluationConfig(
-        evaluator="levanter_lm_evaluation_harness",
-        model_name=None,
-        model_path="gs://marin-us-central2/checkpoints/fineweb-small-1.4b-readability-5c0e2b/",  # type: ignore
-        evaluation_path=this_output_path(),
-        evals=CORE_TASKS,
-        discover_latest_checkpoint=True,
-    ),
+from experiments.evals.evals import default_eval
+from experiments.exp246_web_extraction_method_training import (
+    fineweb_readability_1_4b_model,
+    fineweb_resiliparse_preserve_formatting_1_4b_model,
+    fineweb_trafilatura_1_4b_model,
+    fineweb_trafilatura_favor_precision_1_4b_model,
+    transform_resiliparse_default,
 )
+from marin.execution.executor import executor_main
 
+readability_eval = default_eval(fineweb_readability_1_4b_model)
 
-evaluate_trafilatura_default = ExecutorStep(
-    name="evaluation/lm_evaluation_harness/fineweb-small-trafilatura-default-1.4b",
-    fn=evaluate,
-    config=EvaluationConfig(
-        evaluator="levanter_lm_evaluation_harness",
-        model_name=None,
-        model_path="gs://marin-us-central2/checkpoints/fineweb-small-1.4b-trafilatura-6ba941/",  # type: ignore
-        evaluation_path=this_output_path(),
-        evals=CORE_TASKS,
-        discover_latest_checkpoint=True,
-    ),
-)
+trafilatura_default_eval = default_eval(fineweb_trafilatura_1_4b_model)
 
+trafilatura_favor_precision_eval = default_eval(fineweb_trafilatura_favor_precision_1_4b_model)
 
-evaluate_trafilatura_favor_precision = ExecutorStep(
-    name="evaluation/lm_evaluation_harness/fineweb-small-trafilatura-favor-precision-1.4b",
-    fn=evaluate,
-    config=EvaluationConfig(
-        evaluator="levanter_lm_evaluation_harness",
-        model_name=None,
-        model_path="gs://marin-us-central2/checkpoints/fineweb-small-1.4b-trafilatura-favor-precision-b4f367/",  # type: ignore
-        evaluation_path=this_output_path(),
-        evals=CORE_TASKS,
-        discover_latest_checkpoint=True,
-    ),
-)
+resiliparse_default_eval = default_eval(transform_resiliparse_default)
 
-
-evaluate_resiliparse_default = ExecutorStep(
-    name="evaluation/lm_evaluation_harness/fineweb-small-resiliparse-default-1.4b",
-    fn=evaluate,
-    config=EvaluationConfig(
-        evaluator="levanter_lm_evaluation_harness",
-        model_name=None,
-        model_path="gs://marin-us-central2/checkpoints/fineweb-small-1.4b-resiliparse-49c4d6/",  # type: ignore
-        evaluation_path=this_output_path(),
-        evals=CORE_TASKS,
-        discover_latest_checkpoint=True,
-    ),
-)
-
-
-evaluate_resiliparse_preserve_formatting = ExecutorStep(
-    name="evaluation/lm_evaluation_harness/fineweb-small-resiliparse-preserve-formatting-1.4b",
-    fn=evaluate,
-    config=EvaluationConfig(
-        evaluator="levanter_lm_evaluation_harness",
-        model_name=None,
-        model_path="gs://marin-us-central2/checkpoints/fineweb-small-1.4b-resiliparse-preserve-formatting-792c36/",  # type: ignore
-        evaluation_path=this_output_path(),
-        evals=CORE_TASKS,
-        discover_latest_checkpoint=True,
-    ),
-)
+resiliparse_preserve_formatting_eval = default_eval(fineweb_resiliparse_preserve_formatting_1_4b_model)
 
 
 if __name__ == "__main__":
     executor_main(
         steps=[
-            evaluate_readability,
-            evaluate_trafilatura_default,
-            evaluate_trafilatura_favor_precision,
-            evaluate_resiliparse_default,
-            evaluate_resiliparse_preserve_formatting,
+            readability_eval,
+            trafilatura_default_eval,
+            trafilatura_favor_precision_eval,
+            resiliparse_default_eval,
+            resiliparse_preserve_formatting_eval,
         ]
     )
