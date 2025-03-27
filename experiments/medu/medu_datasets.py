6de489b0f5f21a441a8d2d6f93b6a2b67c838d2f
Christopher Chou <emu4yolife@gmail.com>
2025-03-27 09:54:12 +0000

    Fix paths for dclm baseline

--- experiments/medu/medu_datasets.py
+++ experiments/medu/medu_datasets.py
@@ -1,31 +1,18 @@
+from experiments.pretraining_datasets import dclm_baseline
 from marin.execution.executor import ExecutorStep, executor_main, this_output_path
 from operations.download.gcs.transfer import TransferConfig, transfer_files
-from operations.download.huggingface.download import DownloadConfig, download
-
-dclm_baseline = ExecutorStep(
-    name="raw/dclm-baseline-1.0",
-    fn=download,
-    config=DownloadConfig(
-        hf_dataset_id="mlfoundations/dclm-baseline-1.0",
-        revision="a3b142c",
-        gcs_output_path=this_output_path(),
-        wait_for_completion=True,
-        timeout=24 * 60 * 60,
-    ),
-    override_output_path="raw/dclm",
-)
 
 # Serves as the default pretraining dataset used for MEDU experiments. We want roughly 400B tokens
 # that we can use for training because we know that the quality filter will filter about 10% of the top
 # tokens since 3+ is roughly top 10% most of the time.
 dclm_baseline_global_shard_2 = dclm_baseline.cd(
-    "a3b142c/huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/resolve/a3b142c/global-shard_02_of_10"
+    "huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/resolve/a3b142c/global-shard_02_of_10"
 )
 
 # Around 40B tokens which serves as the default seed annotation dataset for MEDU experiments. We take the top 4
 # files which amounts to about 350K examples.
 dclm_baseline_global_shard_1_local_shard_1 = dclm_baseline.cd(
-    "a3b142c/huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/resolve/a3b142c/global-shard_01_of_10/local-shard_0_of_10"
+    "huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/resolve/a3b142c/global-shard_01_of_10/local-shard_0_of_10"
 )
 
 medu_dclm_annotation_subset = ExecutorStep(
