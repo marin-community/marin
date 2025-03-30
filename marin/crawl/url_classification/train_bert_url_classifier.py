cc1fa82914207f907ee17634b8c521a1219c89bd
Nelson Liu <nfliu@nelsonliu.me>
2025-03-30 13:16:57 -0700

    Don't evaluate as often, 4 times per epoch

--- marin/crawl/url_classification/train_bert_url_classifier.py
+++ marin/crawl/url_classification/train_bert_url_classifier.py
@@ -219,7 +219,7 @@ def train_model(
             dataloader_prefetch_factor=dataloader_prefetch_factor,
             report_to="wandb",
             logging_steps=10,
-            eval_steps=0.1,
+            eval_steps=0.25,
             eval_strategy="steps",
             save_strategy="no",
             max_length=max_length,
