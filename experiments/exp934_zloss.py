3ab5906930cc1c5bfc2acb17b10d55e25d5480a5
David Hall <dlwh@stanford.edu>
2025-04-02 23:22:16 -0700

    wip

--- experiments/exp934_zloss.py
+++ experiments/exp934_zloss.py
@@ -4,8 +4,8 @@ Experiment 934: See if we should make zloss be default on
 
 import dataclasses
 
+from experiments.dclm.tokenize_dclm import dclm_mixture_config_llama3
 from experiments.defaults import default_train
-from experiments.exp72_baselines import nemotron_cc_model, nemotron_cc_tokenized
 from experiments.llama import llama_1_4b, llama_1_4b_train_config
 from marin.execution.executor import executor_main
 
@@ -20,43 +20,52 @@ llama_1_4b_train_config_wsd = dataclasses.replace(
 
 llama_1_4b_train_config_zloss = dataclasses.replace(
     llama_1_4b_train_config,
-    zloss_weight=1e-4,
+    z_loss_weight=1e-4,
 )
 
 
 llama_1_4b_train_config_wsd_zloss = dataclasses.replace(
     llama_1_4b_train_config_wsd,
-    zloss_weight=1e-4,
+    z_loss_weight=1e-4,
 )
 
-nemotron_cc_model_wsd = default_train(
-    name="nemotron_cc-1.4b-wsd",
-    tokenized=nemotron_cc_tokenized,
+
+dclm_mix_model = default_train(
+    name="dclm_mix-1.4b",
+    tokenized=dclm_mixture_config_llama3,
     model_config=llama_1_4b,
     train_config=llama_1_4b_train_config_wsd,
 )
 
-nemotron_cc_model_wsd_zloss = default_train(
-    name="nemotron_cc-1.4b-wsd-zloss",
-    tokenized=nemotron_cc_tokenized,
+dclm_mix_model_wsd = default_train(
+    name="dclm_mix-1.4b-wsd",
+    tokenized=dclm_mixture_config_llama3,
+    model_config=llama_1_4b,
+    train_config=llama_1_4b_train_config_wsd,
+)
+
+dclm_mix_model_wsd_zloss = default_train(
+    name="dclm_mix-1.4b-wsd-zloss",
+    tokenized=dclm_mixture_config_llama3,
     model_config=llama_1_4b,
     train_config=llama_1_4b_train_config_wsd_zloss,
 )
 
-nemotron_cc_model_zloss = default_train(
-    name="nemotron_cc-1.4b-zloss",
-    tokenized=nemotron_cc_tokenized,
+dclm_mix_model_zloss = default_train(
+    name="dclm_mix-1.4b-zloss",
+    tokenized=dclm_mixture_config_llama3,
     model_config=llama_1_4b,
     train_config=llama_1_4b_train_config_zloss,
 )
 
+
 if __name__ == "__main__":
     executor_main(
         steps=[
-            nemotron_cc_model,
-            nemotron_cc_model_wsd_zloss,
-            nemotron_cc_model_zloss,
-            nemotron_cc_model_wsd,
+            dclm_mix_model,
+            dclm_mix_model_wsd_zloss,
+            dclm_mix_model_zloss,
+            dclm_mix_model_wsd,
         ],
         description="Train 1.4B models on nemotron-cc using WSD and zloss.",
     )
