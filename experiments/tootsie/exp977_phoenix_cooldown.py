680832420eb05402f8f0da57bf2840917e7f7145
David Hall <dlwh@stanford.edu>
2025-04-21 21:52:23 -0700

    wip

--- experiments/tootsie/exp977_phoenix_cooldown.py
+++ experiments/tootsie/exp977_phoenix_cooldown.py
@@ -4,15 +4,18 @@ https://github.com/stanford-crfm/marin/issues/977
 Codename: sensible-starling
 
 This experiment is a cooldown run for the tootsie-8b model starting from adept-phoenix. It is trained on the
-same mix as exp934
+same mix as exp934_hq_vs_pt's best mix's full mix
+
+We also add z-loss, since in spoonbill we found that to be very helpful
 """
 from experiments.defaults import default_train
 from .exp600_tootsie import (
-    PHASE_3_END,
-    llama_8b_tootsie_phase3,
-    llama_8b_train_config_phase3,
-    phase_3_data_mixture,
-    llama_8b_tootsie_adept_phoenix
+    llama_8b_tootsie_adept_phoenix,
+    llama_8b_train_config_phase4,
 )
 
-# this is another major cooldown akin to monumental-jellyfish
+tootsie_8b_sensible_starling = dataclasses.replace(
+    default_train(
+        name="tootsie-8b-sensible-starling",
+        tokenized=llama_8b_tootsie_adept_phoenix.tokenized,
+
