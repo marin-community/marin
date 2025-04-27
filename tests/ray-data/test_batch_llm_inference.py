88c4fa098bcd1648fc928dbe1ad00acb78f1086e
Christopher Chou <emu4yolife@gmail.com>
2025-04-27 00:24:27 +0000

    Add ray data batch inference test and fix truncation

---
+++ tests/ray-data/test_batch_llm_inference.py
@@ -0,0 +1,33 @@
+import ray
+
+from marin.generation.inference import OverwriteOutputFiletypeFilenameProvider
+from marin.generation.pipeline import vLLMTextGeneration
+from tests.conftest import default_engine_kwargs, default_generation_params
+
+TEST_OUTPUT_PATH = "gs://marin-us-east5/documents/ray-data-test-llama-200m"
+
+
+def test_ray_data(gcsfuse_mount_model_path, test_file_path):
+    # model_path = model_config.ensure_downloaded("/tmp/test-llama-200m")
+
+    ds = ray.data.read_json(test_file_path, arrow_open_stream_args={"compression": "gzip"})
+
+    ds = ds.map_batches(  # Apply batch inference for all input data.
+        vLLMTextGeneration,
+        # Set the concurrency to the number of LLM instances.
+        concurrency=1,
+        # Specify the batch size for inference.
+        batch_size=16,
+        fn_constructor_kwargs={
+            "model_name": gcsfuse_mount_model_path,
+            "engine_kwargs": default_engine_kwargs,
+            "generation_kwargs": default_generation_params,
+            "template": "What is this text about? {example}",
+            "prompt_column": "text",
+            "save_templated_prompt": False,
+            "apply_chat_template": True,
+        },
+        resources={"TPU": 1, "TPU-v6e-8-head": 1},
+    )
+
+    ds = ds.write_json(TEST_OUTPUT_PATH, filename_provider=OverwriteOutputFiletypeFilenameProvider("jsonl.gz"))
