97c47c8d06dd93092bd6e7eef4979c396950e763
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2025-04-26 18:45:17 -0700

    fix

--- experiments/evals/resource_configs.py
+++ experiments/evals/resource_configs.py
@@ -12,4 +12,4 @@ SINGLE_TPU_V4_8 = ResourceConfig(num_tpu=1, tpu_type="TPU-v4-8", strategy="STRIC
 SINGLE_TPU_V4_16 = ResourceConfig(num_tpu=1, tpu_type="TPU-v4-16", strategy="STRICT_PACK")  # us-central2
 SINGLE_TPU_V6E_8 = ResourceConfig(num_tpu=1, tpu_type="TPU-v6e-8", strategy="STRICT_PACK")  # us-east5
 TPU_V6E_8_STRICT_PACK = ResourceConfig(num_tpu=8, tpu_type="TPU-v6e-8", strategy="STRICT_PACK")  # us-east5
-TPU_V4_16_STRICT_PACK = ResourceConfig(num_tpu=8, tpu_type="TPU-v4-16", strategy="STRICT_PACK")  # us-central2
+TPU_V4_16_STRICT_PACK = ResourceConfig(num_tpu=8, tpu_type="TPU-v4-16", strategy="PACK")  # us-central2
