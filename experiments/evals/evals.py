a6d782b20354313146bfe9c27569c04fc8db54a1
Christopher Chou <emu4yolife@gmail.com>
2025-04-30 05:53:48 +0000

    Fix evals in key evals

--- experiments/evals/evals.py
+++ experiments/evals/evals.py
@@ -6,7 +6,7 @@ import logging
 
 from experiments.evals.engine_configs import DEFAULT_VLLM_ENGINE_KWARGS
 from experiments.evals.resource_configs import SINGLE_TPU_V4_8, ResourceConfig
-from experiments.evals.task_configs import CORE_TASKS, KEY_MULTIPLE_CHOICE_TASKS
+from experiments.evals.task_configs import CORE_TASKS, KEY_GENERATION_TASKS, KEY_MULTIPLE_CHOICE_TASKS
 from marin.evaluation.evaluation_config import EvalTaskConfig, EvaluationConfig
 from marin.evaluation.run import evaluate
 from marin.execution.executor import (
@@ -283,14 +283,14 @@ def default_key_evals(
         stop_token_ids.append(100257)
 
     return [
-        # evaluate_lm_evaluation_harness(
-        #     model_name,
-        #     model_step_path,
-        #     KEY_GENERATION_TASKS,
-        #     max_eval_instances=max_eval_instances,
-        #     engine_kwargs=engine_kwargs,
-        #     resource_config=resource_config,
-        # ),
+        evaluate_lm_evaluation_harness(
+            model_name,
+            model_step_path,
+            KEY_GENERATION_TASKS,
+            max_eval_instances=max_eval_instances,
+            engine_kwargs=engine_kwargs,
+            resource_config=resource_config,
+        ),
         evaluate_levanter_lm_evaluation_harness(
             model_name,
             model_step_path,
@@ -298,11 +298,11 @@ def default_key_evals(
             resource_config,
             max_eval_instances=max_eval_instances,
         ),
-        # evaluate_alpaca_eval(
-        #     model_name,
-        #     model_step_path,
-        #     resource_config,
-        #     engine_kwargs,
-        #     stop_token_ids=stop_token_ids,
-        # ),
+        evaluate_alpaca_eval(
+            model_name,
+            model_step_path,
+            resource_config,
+            engine_kwargs,
+            stop_token_ids=stop_token_ids,
+        ),
     ]
