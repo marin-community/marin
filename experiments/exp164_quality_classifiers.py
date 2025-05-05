33eed226bfe60184a3bcc2f18d2b42462612bc2c
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2025-05-05 13:20:05 -0700

    address comments

--- experiments/exp164_quality_classifiers.py
+++ experiments/exp164_quality_classifiers.py
@@ -6,23 +6,25 @@ TODO: apply these quality classifiers on FineWeb (or DCLM, but that's larger), t
 
 import dataclasses
 
+from experiments.instruction_datasets import get_directory_friendly_dataset_name, get_instruction_dataset
 from marin.classifiers.utils import DatasetConfig
 from marin.execution.executor import ExecutorStep, executor_main, output_path_of, this_output_path, versioned
 from marin.processing.classification.fasttext.train_fasttext import (
     TrainFasttextClassifierConfig,
     train,
 )
+from operations.transform.conversation.conversation_to_dolma import ConversationToDolmaConfig, process_dataset
 from operations.transform.fasttext.transform import TransformFasttextToDolmaConfig
 from operations.transform.fasttext.transform import main as fasttext_to_dolma_format
 
-# openhermes_in_dolma_format = ExecutorStep(
-#     name=f"documents/{get_directory_friendly_dataset_name('teknium/OpenHermes-2.5')}",
-#     fn=process_dataset,
-#     config=ConversationToDolmaConfig(
-#         input_path=output_path_of(get_instruction_dataset("teknium/OpenHermes-2.5")),
-#         output_path=this_output_path("text"),
-#     ),
-# )
+openhermes_in_dolma_format = ExecutorStep(
+    name=f"documents/{get_directory_friendly_dataset_name('teknium/OpenHermes-2.5')}",
+    fn=process_dataset,
+    config=ConversationToDolmaConfig(
+        input_path=output_path_of(get_instruction_dataset("teknium/OpenHermes-2.5")),
+        output_path=this_output_path("text"),
+    ),
+)
 
 dclm_negative_examples_in_dolma_format = ExecutorStep(
     name="documents/dclm_negative_examples",
@@ -67,7 +69,6 @@ dclm_eli5_200k_rw_200k = ExecutorStep(
         val_frac=versioned(0.0),
         seed=versioned(0),
     ),
-    pip_dependency_groups=["fasttext"],
 )
 
 dclm_eli5_100k_oh_100k_rw_200k = ExecutorStep(
@@ -99,34 +100,32 @@ dclm_eli5_100k_oh_100k_rw_200k = ExecutorStep(
         val_frac=versioned(0.0),
         seed=versioned(0),
     ),
-    pip_dependency_groups=["fasttext"],
 )
 
-# teknium_oh_200k_rw_200k = ExecutorStep(
-#     name="classifiers/dclm_oh_200k_rw_200k",
-#     fn=train,
-#     config=TrainFasttextClassifierConfig(
-#         datasets=[
-#             DatasetConfig(
-#                 input_doc_path=output_path_of(openhermes_in_dolma_format, "text"),
-#                 label="hq",
-#                 sampling_rate=1.0,
-#                 max_sample_size=versioned(200000),
-#             ),
-#             DatasetConfig(
-#                 input_doc_path=output_path_of(dclm_negative_examples_in_dolma_format),
-#                 label="lq",
-#                 sampling_rate=1.0,
-#                 max_sample_size=None,
-#             ),
-#         ],
-#         output_path=this_output_path(),
-#         fasttext_args={"lr": versioned(0.1), "thread": 4, "wordNgrams": 2},
-#         val_frac=versioned(0.0),
-#         seed=versioned(0),
-#     ),
-#     pip_dependency_groups=["fasttext"],
-# )
+teknium_oh_200k_rw_200k = ExecutorStep(
+    name="classifiers/dclm_oh_200k_rw_200k",
+    fn=train,
+    config=TrainFasttextClassifierConfig(
+        datasets=[
+            DatasetConfig(
+                input_doc_path=output_path_of(openhermes_in_dolma_format, "text"),
+                label="hq",
+                sampling_rate=1.0,
+                max_sample_size=versioned(200000),
+            ),
+            DatasetConfig(
+                input_doc_path=output_path_of(dclm_negative_examples_in_dolma_format),
+                label="lq",
+                sampling_rate=1.0,
+                max_sample_size=None,
+            ),
+        ],
+        output_path=this_output_path(),
+        fasttext_args={"lr": versioned(0.1), "thread": 4, "wordNgrams": 2},
+        val_frac=versioned(0.0),
+        seed=versioned(0),
+    ),
+)
 
 
 def get_classifier_with_different_seed(classifier_step: ExecutorStep, seed: int):
@@ -144,12 +143,12 @@ dclm_eli5_100k_oh_100k_rw_200k_seed_2 = get_classifier_with_different_seed(dclm_
 if __name__ == "__main__":
     executor_main(
         steps=[
-            # openhermes_in_dolma_format,
+            openhermes_in_dolma_format,
             dclm_negative_examples_in_dolma_format,
             dclm_oh_100k_in_dolma_format,
             dclm_eli5_200k_rw_200k,
             dclm_eli5_100k_oh_100k_rw_200k,
-            # teknium_oh_200k_rw_200k,
+            teknium_oh_200k_rw_200k,
             dclm_eli5_100k_oh_100k_rw_200k_seed_1,
             dclm_eli5_100k_oh_100k_rw_200k_seed_2,
         ]
