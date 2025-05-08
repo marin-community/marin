a26a19ad4046e6d45f06a78f356e52b280aab757
Nikil Ravi <55033516+nikil-ravi@users.noreply.github.com>
2025-05-07 22:45:08 -0700

    Refactor speedrun (#1166)

--- experiments/speedrun/llama_30m/llama_30m.py
+++ experiments/speedrun/llama_30m/llama_30m.py
@@ -1,19 +1,18 @@
 """
-Sample speedrun with an 30M LLaMA model.
+Speedrun code for a 30M parameter model based on the LLaMA architecture.
 """
 
 import logging
 
 from experiments.llama import llama_30m
 from experiments.simple_train_config import SimpleTrainConfig
-from experiments.speedrun.speedrun import ComputeBudget, HardwareConfig, SpeedrunConfig, default_speedrun
 from marin.execution.executor import executor_main
 from marin.resources import TpuPodConfig
+from marin.speedrun.speedrun import HardwareConfig, SpeedrunConfig, default_speedrun
 
 logger = logging.getLogger("ray")
 
 speedrun_config = SpeedrunConfig(
-    compute_budget=ComputeBudget.TINY,
     model_config=llama_30m,
     train_config=SimpleTrainConfig(
         TpuPodConfig(tpu_type="v4-128"),
@@ -27,13 +26,9 @@ speedrun_config = SpeedrunConfig(
     hardware_config=HardwareConfig(
         device_type="v4-128",
         num_devices=64,
-        device_flops=275e12,  # one v4 chip is capable of 275 TFLOPs of peak compute: https://cloud.google.com/tpu/docs/v4
+        device_flops=275e12,  # from https://cloud.google.com/tpu/docs/v4
     ),
 )
-
-# can choose to validate configuration before training
-is_valid, error = speedrun_config.validate()
-logger.info(f"Speedrun validation: {is_valid}, {error}")
 
 if __name__ == "__main__":
     executor_main(steps=default_speedrun("30M_llama_fineweb", speedrun_config))
--- experiments/speedrun/llama_75m_fineweb_edu_adamax/llama_75m_fineweb_edu_adamax.py
+++ experiments/speedrun/llama_75m_fineweb_edu_adamax/llama_75m_fineweb_edu_adamax.py
@@ -1,5 +1,5 @@
 """
-Muon optimizer definition and example speedrun in a single file.
+Speedrun code for a 75M parameter model based on the LLaMA architecture, and the AdaMax optimizer.
 """
 
 import logging
@@ -10,9 +10,9 @@ from levanter.optim import OptimizerConfig
 
 from experiments.llama import llama_75m
 from experiments.simple_train_config import SimpleTrainConfig
-from experiments.speedrun.speedrun import ComputeBudget, HardwareConfig, SpeedrunConfig, default_speedrun
 from marin.execution.executor import executor_main
 from marin.resources import TpuPodConfig
+from marin.speedrun.speedrun import HardwareConfig, SpeedrunConfig, default_speedrun
 
 
 @dataclass
@@ -25,11 +25,11 @@ class AdamaxConfig(OptimizerConfig):
     def build(self, num_train_steps):
         print(f"Building optimizer: {self.__class__.__name__}")
 
-        # Try to register the class if it's not already registered
+        # try to register the class if it's not already registered
         try:
             OptimizerConfig.register_subclass("adamax")(AdamaxConfig)
         except ValueError:
-            # ignore gracefully and use the already registered class
+            # ignore and use the already registered class
             pass
 
         def _optimizer(learning_rate):
@@ -57,12 +57,11 @@ class AdamaxConfig(OptimizerConfig):
         return optax.inject_hyperparams(_optimizer)(learning_rate=self.lr_scheduler(num_train_steps))
 
 
-# --------------------- Example Speedrun Using Adamax ------------------------
+# --------------------- speedrun Using Adamax ------------------------
 
 logger = logging.getLogger("ray")
 
 speedrun_config = SpeedrunConfig(
-    compute_budget=ComputeBudget.SMALL,
     model_config=llama_75m,
     train_config=SimpleTrainConfig(
         TpuPodConfig(tpu_type="v4-128"),
@@ -77,12 +76,9 @@ speedrun_config = SpeedrunConfig(
     hardware_config=HardwareConfig(
         device_type="v4-128",
         num_devices=64,
-        device_flops=275e12,
+        device_flops=275e12,  # from https://cloud.google.com/tpu/docs/v4
     ),
 )
-
-is_valid, error = speedrun_config.validate()
-logger.info(f"Speedrun validation: {is_valid}, {error}")
 
 if __name__ == "__main__":
     executor_main(steps=default_speedrun("75M_llama_adamax", speedrun_config))
