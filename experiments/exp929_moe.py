12adcc966feeacf673cb0e94ca21220b8b67572f
Jason Wang <blahblahj.wsy@gmail.com>
2025-05-08 21:19:58 -0700

    format

--- experiments/exp929_moe.py
+++ experiments/exp929_moe.py
@@ -3,12 +3,13 @@ import logging
 import math
 
 from levanter.models.mixtral import MixtralConfig
-from marin.resources import TpuPodConfig
+
 from experiments.defaults import default_train
 from experiments.dolma.tokenize_dolma import DOLMA_OLMO_MIXTURE_WEIGHTS, tokenize_dolma_steps
 from experiments.simple_train_config import SimpleTrainConfig
-from marin.execution.executor import executor_main, versioned
+from marin.execution.executor import executor_main
 from marin.processing.tokenize.data_configs import lm_mixture_data_config
+from marin.resources import TpuPodConfig
 
 logger = logging.getLogger("ray")
 
