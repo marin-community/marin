413e96a5b84f3cf8a652b5ea0c0f8edcdc8274d9
David Hall <dlwh@stanford.edu>
2025-05-10 22:38:07 -0700

    PR comments

--- experiments/marin_models.py
+++ experiments/marin_models.py
@@ -1,9 +1,14 @@
-# name for the hf hub. cf llama3_tokenizer
+"""
+Various models and templates for Marin.
+"""
+
 marin_tokenizer = "stanford-crfm/marin-tokenizer"
+"""
+The HF Hub name for the Marin tokenizer.
+The Marin tokenizer is (currently) just the Llama 3 tokenizer with a custom chat template (MARIN_CHAT_TEMPLATE).
+"""
 
 # to be clear this is the Olmo 2 template except we use llama3's special tokens
-# we also add the {% generation -%} tag stuff that makes the assistant_mask work
-# (Levanter relies on the assistant mask.)
 MARIN_CHAT_TEMPLATE = """
 {{ bos_token }}
 {%- for message in messages -%}
@@ -18,3 +23,17 @@ MARIN_CHAT_TEMPLATE = """
 {%- if add_generation_prompt -%}
 <|start_header_id|>assistant<|end_header_id|>\n{% endif -%}
 """.strip()
+
+"""
+The chat template for the Marin tokenizer.
+
+This template is used to generate the chat template for the Marin tokenizer.
+It is a modified version of the Olmo 2 template.
+
+The modifications are:
+- Use the Llama 3 special tokens rather than Olmo (which doesn't use special tokens but rather just literally <|, etc.)
+- Add the {% generation -%} tag stuff that makes the assistant_mask in Levanter work.
+
+See [Levanter's documentation on Chat Templates](https://levanter.readthedocs.io/en/latest/reference/Data-Formats/#chat-format)
+for more information on how this works.
+"""
