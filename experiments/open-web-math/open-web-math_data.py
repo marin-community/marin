7e70568c91308f3c313ccd51e282069b5aeb7de7
Abhinav Garg <abhinavg@stanford.edu>
2025-05-13 00:29:17 -0700

    Reverting changes to non default func

--- experiments/open-web-math/open-web-math_data.py
+++ experiments/open-web-math/open-web-math_data.py
@@ -4,19 +4,21 @@ Downloads the open-web-math dataset
 (https://huggingface.co/datasets/open-web-math/open-web-math) to GCS.
 """
 
-from experiments.defaults import default_download
-from marin.execution.executor import executor_main, versioned
-from operations.download.huggingface.download import download
+from marin.execution.executor import ExecutorStep, executor_main, this_output_path, versioned
+from operations.download.huggingface.download import DownloadConfig, download
 
 ############################################################
 # download open-web-math dataset
-
-open_web_math_raw = default_download(
+open_web_math_raw = ExecutorStep(
     name="raw/open-web-math",
-    hf_dataset_id="open-web-math/open-web-math",
-    revision=versioned("fde8ef8"),
-    output_path="raw/open-web-math-fde8ef8",
-    download_fn=download,
+    fn=download,
+    config=DownloadConfig(
+        hf_dataset_id="open-web-math/open-web-math",
+        revision=versioned("fde8ef8"),
+        gcs_output_path=this_output_path(),
+        wait_for_completion=False,
+    ),
+    override_output_path="raw/open-web-math-fde8ef8",
 )
 
 ############################################################
