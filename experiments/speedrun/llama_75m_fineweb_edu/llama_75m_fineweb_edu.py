ef828bf5577895a83e7e835af883f8e350f0c18c
Nikil Ravi <nravi@stanford.edu>
2025-05-17 03:19:46 -0700

    wip

--- experiments/speedrun/llama_75m_fineweb_edu/llama_75m_fineweb_edu.py
+++ experiments/speedrun/llama_75m_fineweb_edu/llama_75m_fineweb_edu.py
@@ -8,24 +8,19 @@ from experiments.llama import llama_75m
 from experiments.simple_train_config import SimpleTrainConfig
 from marin.execution.executor import executor_main
 from marin.resources import TpuPodConfig
-from marin.speedrun.speedrun import HardwareConfig, SpeedrunConfig, default_speedrun
+from marin.speedrun.speedrun import SpeedrunConfig, default_speedrun
 
 logger = logging.getLogger("ray")
 
 speedrun_config = SpeedrunConfig(
     model_config=llama_75m,
     train_config=SimpleTrainConfig(
-        TpuPodConfig(tpu_type="v4-128"),
+        resources=TpuPodConfig(tpu_type="v4-128"),
         train_batch_size=512,
         num_train_steps=3000,
         learning_rate=3e-3,
         weight_decay=0.1,
         steps_per_eval=1000,
-    ),
-    hardware_config=HardwareConfig(
-        device_type="v4-128",
-        num_devices=64,
-        device_flops=275e12,  # from https://cloud.google.com/tpu/docs/v4
     ),
 )
 
