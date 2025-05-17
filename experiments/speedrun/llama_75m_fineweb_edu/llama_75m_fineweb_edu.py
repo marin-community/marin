663291fbf97a181f2c80aef1bf374f3b80fe136d
Nikil Ravi <nravi@stanford.edu>
2025-05-17 06:16:01 -0700

    wip

--- experiments/speedrun/llama_75m_fineweb_edu/llama_75m_fineweb_edu.py
+++ experiments/speedrun/llama_75m_fineweb_edu/llama_75m_fineweb_edu.py
@@ -4,32 +4,27 @@ Speedrun code for a 75M parameter model based on the LLaMA architecture.
 
 import logging
 
-from experiments.exp72_baselines import fineweb_edu_tokenized
 from experiments.llama import llama_75m
 from experiments.simple_train_config import SimpleTrainConfig
 from marin.execution.executor import executor_main
 from marin.resources import TpuPodConfig
-from marin.speedrun.speedrun import HardwareConfig, SpeedrunConfig, default_speedrun
+from marin.speedrun.speedrun import SpeedrunConfig
 
 logger = logging.getLogger("ray")
 
 speedrun_config = SpeedrunConfig(
+    author="Nikil Ravi",
+    affiliation="Marin Community",
+    description="75M parameter model based on LLaMA architecture.",
     model_config=llama_75m,
     train_config=SimpleTrainConfig(
-        TpuPodConfig(tpu_type="v4-128"),
         resources=TpuPodConfig(tpu_type="v4-128", slice_count=2),
         train_batch_size=512,
         num_train_steps=3000,
         learning_rate=3e-3,
         weight_decay=0.1,
         steps_per_eval=1000,
-    ),
-    tokenized_dataset=fineweb_edu_tokenized,
-    hardware_config=HardwareConfig(
-        device_type="v4-128",
-        num_devices=64,
-        device_flops=275e12,  # from https://cloud.google.com/tpu/docs/v4
-    ),
+    ),    
 )
 
 if __name__ == "__main__":
@@ -37,6 +32,8 @@ if __name__ == "__main__":
     print(f"Number of devices: {speedrun_config.num_devices}")
     print(f"Device FLOPs: {speedrun_config.device_flops}")
     print(f"Total peak FLOPs: {speedrun_config.device_flops * speedrun_config.num_devices}")
+
+    print(f"Estimated model flops: {speedrun_config.estimate_model_flops()}")
     
     # Comment out the actual run for now
     # executor_main(steps=default_speedrun("75M_llama_fineweb_edu", speedrun_config))
