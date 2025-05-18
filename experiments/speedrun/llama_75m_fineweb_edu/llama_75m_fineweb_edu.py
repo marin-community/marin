61a4b1147431b831afdc4e9397ce6ae966e90eae
Nikil Ravi <nravi@stanford.edu>
2025-05-18 00:42:04 -0700

    wip

--- experiments/speedrun/llama_75m_fineweb_edu/llama_75m_fineweb_edu.py
+++ experiments/speedrun/llama_75m_fineweb_edu/llama_75m_fineweb_edu.py
@@ -6,19 +6,21 @@ import logging
 
 from experiments.llama import llama_75m
 from experiments.simple_train_config import SimpleTrainConfig
-from marin.execution.executor import executor_main
 from marin.resources import TpuPodConfig
-from marin.speedrun.speedrun import SpeedrunConfig, default_speedrun
+from marin.speedrun.speedrun import Author, SpeedrunConfig
 
 logger = logging.getLogger("ray")
 
 speedrun_config = SpeedrunConfig(
-    author="Nikil Ravi",
-    affiliation="Marin Community",
+    author=Author(
+        name="Nikil Ravi",
+        affiliation="Marin Community",
+        url="https://www.linkedin.com/in/nikilravi/",
+    ),
     description="75M parameter model based on LLaMA architecture.",
     model_config=llama_75m,
     train_config=SimpleTrainConfig(
-        resources=TpuPodConfig(tpu_type="v4-128", slice_count=2, device_flops_override=1e15),
+        resources=TpuPodConfig(tpu_type="v4-128", slice_count=2),
         train_batch_size=512,
         num_train_steps=3000,
         learning_rate=3e-3,
@@ -26,6 +28,8 @@ speedrun_config = SpeedrunConfig(
         steps_per_eval=1000,
     ),
 )
+
+speedrun_config.print_flops_info()
 
 if __name__ == "__main__":
     # Debug info about devices
