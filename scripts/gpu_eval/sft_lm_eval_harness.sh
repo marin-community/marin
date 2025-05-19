132574099ed28ce0db97d83ee8badfb808061ad7
Helw150 <wbh230@nyu.edu>
2025-05-19 04:08:57 -0400

    Note

--- scripts/gpu_eval/sft_lm_eval_harness.sh
+++ scripts/gpu_eval/sft_lm_eval_harness.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# This is not great Marin practice - we want everything to run inside the Executor framework!
+# However, for the 05-19 Launch, there were a wide variety of issues for Evals on TPU
+# - Disk Space Fills up when running too many evaluations at once
+# - VLLM isn't releasing TPUs causing crashed jobs due to resource contention
+# - OLMo was unexplainably OOMing when Marin model was not.
+# In order to get the evaluations done on time and to keep the evaluation setup consistent
+# across models I ended up using the following script for Marin 8B SFT evals!
+# Documenting here for posterity, but this should not become a standard practice - Will
 
 MODEL=$1
 
