c89a687f049bb961a35555fda2083f1ce9ff115f
Helw150 <wbh230@nyu.edu>
2025-05-22 20:12:54 -0400

    Initial Gemstone Scraping

--- experiments/models.py
+++ experiments/models.py
@@ -41,8 +41,9 @@ GCS_FUSE_MOUNT_PATH = "gcsfuse_mount/models"
 
 def download_model_step(model_config: ModelConfig) -> ExecutorStep:
     model_name = get_directory_friendly_name(model_config.hf_repo_id)
+    model_revision = get_directory_friendly_name(model_config.hf_revision)
     download_step = ExecutorStep(
-        name=f"{GCS_FUSE_MOUNT_PATH}/{model_name}",
+        name=f"{GCS_FUSE_MOUNT_PATH}/{model_name}/{model_revision}",
         fn=download_hf,
         config=DownloadConfig(
             hf_dataset_id=model_config.hf_repo_id,
@@ -53,7 +54,7 @@ def download_model_step(model_config: ModelConfig) -> ExecutorStep:
         ),
         # must override because it because if we don't then it will end in a hash
         # if it ends in a hash, then we cannot determine the local path
-        override_output_path=f"{GCS_FUSE_MOUNT_PATH}/{model_name}",
+        override_output_path=f"{GCS_FUSE_MOUNT_PATH}/{model_name}/{model_revision}",
     )
 
     return download_step
