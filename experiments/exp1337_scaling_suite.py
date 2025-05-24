266c1956b0e79861d6f5c2bcf37f3806b3f69f5e
Percy Liang <percyliang@gmail.com>
2025-05-23 23:12:24 -0700

    make scaling uniform

--- experiments/exp1337_scaling_suite.py
+++ experiments/exp1337_scaling_suite.py
@@ -2,6 +2,7 @@
 Creates a suite of training runs of different model sizes and number of tokens trained on the same data ("modernized Pythia").
 """
 
+from math import sqrt
 from dataclasses import dataclass, replace
 
 from experiments.dclm.tokenize_dclm import dclm_mixture_config_llama3
@@ -20,6 +21,7 @@ from experiments.llama import (
     llama_300m,
     llama_300m_train_config,
 )
+from experiments.tootsie.exp600_tootsie import tootise_phase1_config
 from experiments.simple_train_config import SimpleTrainConfig
 from experiments.tootsie.exp859_big_tootsies import nemotron_mix
 from levanter.models.llama import LlamaConfig
@@ -27,36 +29,57 @@ from marin.execution.executor import executor_main
 from marin.processing.tokenize.data_configs import LMMixtureDatasetConfig
 from marin.resources import TpuPodConfig
 
+# We want to have a consistent way of scaling up the learning rate.
+# Strategy: figure out a "base LR" based on what we did with the Marin 8B model
+# Then do base LR * sqrt(batch_size) / width
+MARIN_8B_LR = tootise_phase1_config.learning_rate
+MARIN_8B_BS = tootise_phase1_config.train_batch_size
+MARIN_8B_WIDTH = llama_8b.hidden_dim
+BASE_LR = MARIN_8B_LR / sqrt(MARIN_8B_BS) * MARIN_8B_WIDTH
+print(f"BASE_LR: {BASE_LR}")
+
+def scaling_train_config(model_config: LlamaConfig, train_config: SimpleTrainConfig) -> SimpleTrainConfig:
+    """Return a train config that scales the learning rate based on the model size."""
+    # Use the same batch size for all models (chosen to work for the largest 70B model, but okay-ish for smaller models)
+    train_config = replace(train_config,
+        train_batch_size=8192,
+        weight_decay=0.05,
+        z_loss_weight=1e-4,
+    )
+
+    # Scale the learning rate
+    learning_rate = BASE_LR * sqrt(train_config.train_batch_size) / model_config.hidden_dim
+    return replace(
+        train_config,
+        learning_rate=learning_rate,
+    )
+
 # Like the one in exp859_big_tootsies but with a constant batch size
 llama_32b_train_config = SimpleTrainConfig(
     resources=TpuPodConfig(tpu_type="v4-2048", slice_count=1),
     num_train_steps=1_000_000,  # To override later
-    train_batch_size=8192,
-    weight_decay=0.05,
-    learning_rate=4.2e-4,  # TODO: fix this
+    train_batch_size=-1,  # To override later
+    learning_rate=float("nan"),  # To override later
     decay=0.4,
     ema_beta=0.995,
     lr_schedule="linear",
     cycle_length=None,
     steps_per_eval=1000,
     steps_per_task_eval=10000,
-    z_loss_weight=1e-4,
 )
 
 # Like the one in exp750_tootsie70b but with a constant batch size
 llama_70b_train_config_mk6 = SimpleTrainConfig(
     resources=TpuPodConfig(tpu_type="v6e-128", slice_count=6),
     num_train_steps=1_000_000,  # To override later
-    train_batch_size=1536,
-    weight_decay=0.05,
-    learning_rate=3.5e-4,  # TODO: fix this
+    train_batch_size=-1,  # To override later
+    learning_rate=float("nan"),  # To override later
     decay=0.4,
     ema_beta=0.995,
     lr_schedule="linear",
     cycle_length=None,
     steps_per_eval=1000,
     steps_per_task_eval=10000,
-    z_loss_weight=1e-4,
 )
 
 
@@ -116,6 +139,8 @@ def create_train_step(data_spec: DataSpec, train_spec: TrainSpec):
     num_tokens = num_parameters * 20 * train_spec.data_factor
     num_train_steps = num_tokens // (train_spec.train_config.train_batch_size * model_config.seq_len)
     train_config = replace(train_spec.train_config, num_train_steps=num_train_steps)
+
+    train_config = scaling_train_config(model_config, train_config)
 
     return default_train(
         name=name,
