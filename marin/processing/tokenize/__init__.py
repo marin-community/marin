0fe7940c810663708b12638807cad24e7717fbe4
Helw150 <wbh230@nyu.edu>
2025-05-24 11:29:09 -0400

    Lint Tokenize

--- marin/processing/tokenize/__init__.py
+++ marin/processing/tokenize/__init__.py
@@ -3,8 +3,8 @@ from .data_configs import (
     add_validation_sets_to_mixture,
     lm_data_config,
     lm_mixture_data_config,
-    step_to_lm_mixture_component,
     mixture_for_evaluation,
+    step_to_lm_mixture_component,
 )
 from .tokenize import (
     TokenizeConfig,
