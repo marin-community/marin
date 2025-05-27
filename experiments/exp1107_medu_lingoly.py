2350c71e9977f9ae030b38ac37dfef54afd54528
Christopher Chou <emu4yolife@gmail.com>
2025-05-27 01:31:26 +0000

    Make the code less Union-y

--- experiments/exp1107_medu_lingoly.py
+++ experiments/exp1107_medu_lingoly.py
@@ -1,3 +1,10 @@
+"""
+This experiment is used to target the linguistic olympiad benchmark using MEDU,
+a process that automatically looks for data that is similar to the targeted dataset.
+
+The benchmark is from: https://huggingface.co/datasets/ambean/lingOly
+"""
+
 from experiments.datashop.datashop_datasets import datashop_dclm_annotation_subset, datashop_dclm_pretraining_subset
 from experiments.datashop.datashop_runner import DatashopRunner, DatashopRunnerConfig
 from experiments.datashop.default_configs import default_medu_config_kwargs, default_text_generation_config_kwargs
@@ -5,7 +12,7 @@ from experiments.eval_datasets import lingoly
 from experiments.models import get_model_local_path, llama_3_3_70b_instruct
 from marin.datashop.pipeline import CorpusContent
 from marin.execution.executor import ExecutorStep, output_path_of, this_output_path
-from operations.transform.lingoly.to_dolma import ConvertLingolyToDolmaConfig, convert_lingoly_to_dolma
+from marin.transform.lingoly.to_dolma import ConvertLingolyToDolmaConfig, convert_lingoly_to_dolma
 
 lingoly_dolma = ExecutorStep(
     name="documents/lingoly/dolma",
