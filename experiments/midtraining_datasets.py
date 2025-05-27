99a640c8e76cb4e4645521dd7040a3fc9ee58f42
Christopher Chou <emu4yolife@gmail.com>
2025-05-27 03:36:12 +0000

    Basic scaffold

--- experiments/midtraining_datasets.py
+++ experiments/midtraining_datasets.py
@@ -1,4 +1,4 @@
-from experiments.defaults import default_tokenize
+from experiments.defaults import default_download, default_tokenize
 from experiments.llama import llama3_tokenizer
 from marin.download.huggingface.download_hf import DownloadConfig, download_hf
 from marin.execution.executor import ExecutorStep, this_output_path
@@ -22,3 +22,17 @@ finemath_3_plus_tokenized = default_tokenize(
     dataset=finemath_3_plus,
     tokenizer=llama3_tokenizer,
 ).with_output_path("tokenized/finemath_3_plus-a26b0f/")
+
+lavita_medical_qa_datasets = default_download(
+    name="raw/lavita_medical_qa",
+    hf_dataset_id="lavita/medical-qa-datasets",
+    revision="59d48e2",
+    override_output_path="raw/lavita_medical_qa",
+)
+
+openlifescienceai_medmcqqa = default_download(
+    name="raw/openlifescienceai_medmcqqa",
+    hf_dataset_id="openlifescienceai/medmcqa",
+    revision="91c6572",
+    override_output_path="raw/openlifescienceai_medmcqqa",
+)
