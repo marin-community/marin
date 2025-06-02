68f9f60ee6d1acaf8c6265bb4290c83002238129
Christopher Chou <emu4yolife@gmail.com>
2025-06-02 20:51:02 +0000

    Add experiment

--- marin/run/ray_run.py
+++ marin/run/ray_run.py
@@ -89,7 +89,8 @@ async def submit_and_track_job(entrypoint: str, dependencies: list, env_vars: di
     current_dir = os.getcwd()
     client = JobSubmissionClient(REMOTE_DASHBOARD_URL)
     runtime_dict = {
-        "pip": dependencies,
+        # "pip": dependencies,
+        "uv": dependencies,
         "working_dir": current_dir,
         "env_vars": env_vars,
         "config": {"setup_timeout_seconds": 1800},
