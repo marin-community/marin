cea219b371ca9d5107de97499aa9e2f6fc6cd9c6
Ahmed Ahmed <ahmedah@stanford.edu>
2025-06-11 13:17:45 -0700

    update docker to 2.45 (#1382)
    * update docker to 2.45
    
    * delete accidentally added files

--- tests/snapshots/dclm_hq/accept_changes.py
+++ tests/snapshots/dclm_hq/accept_changes.py
@@ -5,8 +5,8 @@ import re
 from tqdm import tqdm
 
 from marin.schemas.web.convert import HtmlToMarkdownConfig, ResiliparseConfig
-from marin.web.convert import convert_page
 from marin.transform.dolmino.transform_dclm_hq import find_html_in_cc
+from marin.web.convert import convert_page
 
 
 def save_html(json_data, input_path):
