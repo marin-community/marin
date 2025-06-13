0b934d801c506b483e5c4e69d6021c5205c5c5ae
Chi Heem W <chiheemwong@gmail.com>
2025-06-13 21:50:26 +0000

    linting

--- experiments/exp9999_test_nemotron.py
+++ experiments/exp9999_test_nemotron.py
@@ -1,13 +1,8 @@
 from instruction_datasets import INSTRUCTION_DATASET_NAME_TO_CONFIG, download_dataset_step, transform_dataset_step
+
 from marin.execution.executor import (
-    ExecutorStep,
     executor_main,
-    output_path_of,
-    this_output_path,
-    versioned,
 )
-
-
 
 
 def main():
@@ -15,7 +10,7 @@ def main():
 
 
 if __name__ == "__main__":
-    dataset_names = ["open-thoughts/OpenThoughts3-1.2M"]
+    dataset_names = ["open-thoughts/OpenThoughts3-1.2M", "nvidia/Llama-Nemotron-Post-Training-Dataset-v1-SFT"]
     all_steps = []
     for dataset_name in dataset_names:
         config = INSTRUCTION_DATASET_NAME_TO_CONFIG[dataset_name]
