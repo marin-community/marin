0286022ff8536af3cce3dd4b92b5e1a7f831eda9
Kaiyue Wen <kaiyue@sc.stanford.edu>
2025-06-13 14:39:50 -0700

    fix typo

--- experiments/tootsie/exp1388_nadamw32b.py
+++ experiments/tootsie/exp1388_nadamw32b.py
@@ -15,37 +15,37 @@ from marin.resources import TpuPodConfig
 warmstart_checkpoint = llama_32b_tootsie.cd("checkpoints/step-77096/").nonblocking()
 
 
-ndamw_config = AdamConfig(
-    # Yolo hypers for ndamw from Kaiyue
+nadamw_config = AdamConfig(
+    # Yolo hypers for nadamw from Kaiyue
     learning_rate=llama_32b_train_config.learning_rate,
     weight_decay=llama_32b_train_config.weight_decay,
     beta1=0.95,
     beta2=0.95,
-    nesterov=True
+    nesterov=True,
 )
 
 llama_32b_warmstart_train = dataclasses.replace(
     llama_32b_train_config,
     initialize_from_checkpoint_path=warmstart_checkpoint,
-    optimizer_config=ndamw_config,
+    optimizer_config=nadamw_config,
     resources=TpuPodConfig("v4-2048", 1),
     reset_data_loader_on_init=False,
 )
 
-llama_32b_ndamw = default_train(
-    name="marin-32b-ndamw-4",
+llama_32b_nadamw = default_train(
+    name="marin-32b-nadamw-4",
     tokenized=nemotron_mix,
     model_config=llama_32b,
     train_config=llama_32b_warmstart_train,
-    tags=["llama", "32b", "ema", "exp1388", "tootsie", "ndamw"],
+    tags=["llama", "32b", "ema", "exp1388", "tootsie", "nadamw"],
     eval_harness_tasks=[],
-).with_output_path("checkpoints/marin-32b-ndamw-4")
+).with_output_path("checkpoints/marin-32b-nadamw-4")
 
 
 if __name__ == "__main__":
     executor_main(
         [
-            llama_32b_ndamw,
+            llama_32b_nadamw,
         ],
-        description="Give ndamw a shot on 32B with Nemotron and Starcoderdata",
+        description="Give nadamw a shot on 32B with Nemotron and Starcoderdata",
     )
