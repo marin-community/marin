93f4c905f77ff255769895d69845ba8425fc178e
Chi Heem W <chiheemwong@gmail.com>
2025-06-13 17:58:29 +0000

    add temp test script

---
+++ experiments/exp9999_test_nemotron.py
@@ -0,0 +1,27 @@
+from instruction_datasets import INSTRUCTION_DATASET_NAME_TO_CONFIG, download_dataset_step, transform_dataset_step
+from marin.execution.executor import (
+    ExecutorStep,
+    executor_main,
+    output_path_of,
+    this_output_path,
+    versioned,
+)
+
+
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    all_steps = []
+    for config in [
+        "open-thoughts/OpenThoughts3-1.2M",
+    ]:
+        downloaded_dataset = download_dataset_step(config)
+        all_steps.append(downloaded_dataset)
+        transformed_dataset = transform_dataset_step(config, downloaded_dataset)
+        all_steps.append(transformed_dataset)
+
+    executor_main(steps=all_steps)
