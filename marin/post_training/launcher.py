8908f29508ac9a997ae4c99921b811d5f5c1c803
Sherry Yang <sherryy@google.com>
2025-06-25 16:34:54 -0700

    lint fixes

--- marin/post_training/launcher.py
+++ marin/post_training/launcher.py
@@ -1,4 +1,5 @@
 import os
+
 from tpu_pod_launcher import TPUPodClient, TPUPodProject, create_cli
 
 SETUP_SCRIPT = """\
@@ -40,42 +41,51 @@ source ~/miniconda3/bin/activate llama3_train
 python -c "import jax; print(jax.devices())"
 """.strip()
 
-USER = os.environ['USER']
+USER = os.environ["USER"]
 
-def check_devices(project: TPUPodProject, verbose: bool=False):
+
+def check_devices(project: TPUPodProject, verbose: bool = False):
     project.ssh(CHECK_DEVICES, verbose=verbose)
 
-def setup(project: TPUPodProject, verbose: bool=False):
+
+def setup(project: TPUPodProject, verbose: bool = False):
     project.copy(verbose=verbose)
     project.ssh(SETUP_SCRIPT, verbose=verbose)
-    project.ssh('mkdir ~/.config/', verbose=verbose)
-    project.ssh('mkdir ~/.config/gcloud/', verbose=verbose)
-    project.scp(f'/home/{USER}/.config/gcloud/application_default_credentials.json', '~/.config/gcloud/', verbose=verbose)
+    project.ssh("mkdir ~/.config/", verbose=verbose)
+    project.ssh("mkdir ~/.config/gcloud/", verbose=verbose)
+    project.scp(
+        f"/home/{USER}/.config/gcloud/application_default_credentials.json", "~/.config/gcloud/", verbose=verbose
+    )
 
-def debug(project: TPUPodProject, verbose: bool=False):
-    import IPython; IPython.embed()
+
+def debug(project: TPUPodProject, verbose: bool = False):
+    import IPython
+
+    IPython.embed()
+
 
 def create_project(tpu_name: str, zone: str) -> TPUPodProject:
     return TPUPodProject(
         client=TPUPodClient(
-            tpu_project='hai-gcp-models',
+            tpu_project="hai-gcp-models",
             tpu_zone=zone,
-            user=f'{USER}',
-            key_path=f'/home/{USER}/.ssh/google_compute_engine',
+            user=f"{USER}",
+            key_path=f"/home/{USER}/.ssh/google_compute_engine",
         ),
         tpu_name=tpu_name,
-        copy_dirs=[(os.getcwd(), '~/llama3_train/')],
-        working_dir='~/llama3_train/',
-        copy_excludes=['.git', '__pycache__', '*.pkl', '*.json', '*.jsonl', '*.ipynb'],
-        kill_commands=['sudo pkill -9 python'],
+        copy_dirs=[(os.getcwd(), "~/llama3_train/")],
+        working_dir="~/llama3_train/",
+        copy_excludes=[".git", "__pycache__", "*.pkl", "*.json", "*.jsonl", "*.ipynb"],
+        kill_commands=["sudo pkill -9 python"],
     )
 
+
 if __name__ == "__main__":
-    launch_config_path = os.path.join(os.path.dirname(__file__), 'launch_config.json')
+    launch_config_path = os.path.join(os.path.dirname(__file__), "launch_config.json")
 
     available_tpus = [
-        ('ray-marin-us-central1-worker-4e01d4d4-tpu', 'us-central1-a'),
-        ('post-training-v5p-8', 'us-east5-a'),
+        ("ray-marin-us-central1-worker-4e01d4d4-tpu", "us-central1-a"),
+        ("post-training-v5p-8", "us-east5-a"),
     ]
 
     tpu_projects = {name: create_project(name, zone) for name, zone in available_tpus}
@@ -83,6 +93,6 @@ if __name__ == "__main__":
     create_cli(
         projects=tpu_projects,
         setup=setup,
-        custom_commands={'debug': debug, 'check_devices': check_devices},
+        custom_commands={"debug": debug, "check_devices": check_devices},
         launch_config_path=launch_config_path,
     )
