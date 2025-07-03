ecc0e551f1bc5a803af5d95e4673d534598cabf3
Christopher Chou <emu4yolife@gmail.com>
2025-07-03 06:19:42 +0000

    Format

--- marin/run/slurm_run.py
+++ marin/run/slurm_run.py
@@ -42,9 +42,9 @@ import sys
 import tempfile
 import time
 
-from huggingface_hub import HfFolder
 
 import wandb
+from huggingface_hub import HfFolder
 
 # Setup logger
 logger = logging.getLogger("slurm")
