f74b67940431fcb26f8a8ebecfaf19f3b71bccea
Tony Lee <tonyh.lee@yahoo.com>
2025-07-05 23:33:49 -0700

    fix imports

--- marin/post_training/environments/math_env.py
+++ marin/post_training/environments/math_env.py
@@ -4,10 +4,9 @@ from typing import Any
 import datasets
 import jax
 import numpy as np
-from tqdm.auto import tqdm
-
 from post_training.inference import batch_inference
 from post_training.utils import validate_format
+from tqdm.auto import tqdm
 
 from .marin_env import EnvStep, MarinEnv
 from .math_utils import grade_answer, last_boxed_only_string, remove_boxed
