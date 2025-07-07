f4089b9462c73f9f7743a0fd419950227fde2277
Tony Lee <tonyh.lee@yahoo.com>
2025-07-06 20:28:40 -0700

    rename test

--- tests/post_training/test_load_environments.py
+++ tests/post_training/test_load_environments.py
@@ -2,7 +2,6 @@ from marin.post_training.environments.olym_math_env import OlymMathEnv
 from marin.post_training.load_environments import load_environment_from_spec
 
 
-def test_math_env_loaded():
-    """Test whether MathEnv examples are loaded correctly."""
+def test_load_environment_from_spec():
     env = load_environment_from_spec("olym_math:difficulty=hard", tokenizer=None)
     assert isinstance(env, OlymMathEnv), "Loaded environment should be an instance of OlymMathEnv"
