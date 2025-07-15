a4276efe3f4c15849f5e3bc2949560e66b6f9c79
Helw150 <wbh230@nyu.edu>
2025-07-14 23:14:54 -0400

    Didn't Need Chunked After All

--- experiments/exp1427_two_step_scaling.py
+++ experiments/exp1427_two_step_scaling.py
@@ -60,12 +60,6 @@ def create_eval_steps() -> list:
     return steps
 
 
-def chunked(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i : i + n]
-
-
 if __name__ == "__main__":
     all_steps = create_eval_steps()
     executor_main(all_steps)
