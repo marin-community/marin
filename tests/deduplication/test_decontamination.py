2d2231d966982fc2d37a738533c07f809b0eed96
Christopher Chou <emu4yolife@gmail.com>
2025-07-15 15:18:55 +0000

    Remove external dependencies

--- tests/deduplication/test_decontamination.py
+++ tests/deduplication/test_decontamination.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tempfile
 
 import fsspec
 import pytest
@@ -8,15 +9,10 @@ import ray
 from marin.processing.classification.dedupe import DedupeConfig, NGramConfig, dedupe
 from marin.utils import fsspec_exists
 
-BASE_INPUT_DIR = "gs://marin-us-east1/documents/test-data/decontamination"
-BASE_ATTRIBUTE_OUTPUT_DIR = "gs://marin-us-east1/attributes/test-data/decontamination"
-GSM8K_DECONTAMINATION_PATH = "gs://marin-us-east1/decontamination/gsm8k-dolma-9f147d/gsm8k"
-BLOOM_FILTER_OUTPUT_DIR = "gs://marin-us-east1/decontamination/bloom-filters/gsm8k-test"
-
 
 @pytest.fixture
 def sample_documents():
-    """Create sample documents with duplicates for testing"""
+    """Create sample documents for testing decontamination"""
     return [
         {
             "id": "gsm8k",
@@ -36,53 +32,85 @@ def sample_documents():
     ]
 
 
+@pytest.fixture
+def test_set_documents():
+    """Set of documents to decontaminate against."""
+    return [
+        {
+            "id": "gsm8k",
+            "text": (
+                "Janet’s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?"  # noqa: E501, RUF001
+            ),
+            "source": "test",
+        },
+        {
+            "id": "gsm8k-middle",
+            "text": (
+                "James decides to run 3 sprints 3 times a week. He runs 60 meters each sprint. How many total meters does he run a week?"  # noqa: E501
+            ),
+            "source": "test",
+        },
+    ]
+
+
 @ray.remote
 def _run_dedupe(dedupe_config):
     ray.get(dedupe.remote(dedupe_config))
 
 
-def test_exact_decontamination_paragraph(ray_tpu_cluster, sample_documents):
-    output_file_path = os.path.join(BASE_INPUT_DIR, "test_docs.jsonl.gz")
-    with fsspec.open(output_file_path, "w", compression="gzip") as f:
-        for doc in sample_documents:
-            f.write(json.dumps(doc) + "\n")
+def test_exact_decontamination_paragraph(ray_tpu_cluster, sample_documents, test_set_documents):
+    with (
+        tempfile.TemporaryDirectory() as temp_input_dir,
+        tempfile.TemporaryDirectory() as temp_attribute_dir,
+        tempfile.TemporaryDirectory() as temp_decontamination_dir,
+        tempfile.TemporaryDirectory() as temp_bloom_filter_dir,
+    ):
+        input_file_path = os.path.join(temp_input_dir, "test_docs.jsonl.gz")
+        with fsspec.open(input_file_path, "w", compression="gzip") as f:
+            for doc in sample_documents:
+                f.write(json.dumps(doc) + "\n")
 
-    dedupe_config = DedupeConfig(
-        input_path=BASE_INPUT_DIR,
-        output_path=BASE_ATTRIBUTE_OUTPUT_DIR,
-        attribute_name="duplicate_text",
-        min_length=0,
-        min_words=0,
-        estimated_doc_count=10000,
-        false_positive_rate=0.001,
-        ngram=NGramConfig(
-            ngram_length=8,
-            stride=0,
-            overlap_threshold=0.0,  # For debugging purposes, this outputs all possible duplicates
-        ),
-        decontaminate=True,
-        decontaminate_path=GSM8K_DECONTAMINATION_PATH,
-        bloom_filter_path=BLOOM_FILTER_OUTPUT_DIR,
-    )
+        test_set_file_path = os.path.join(temp_decontamination_dir, "test_set.jsonl.gz")
+        with fsspec.open(test_set_file_path, "w", compression="gzip") as f:
+            for doc in test_set_documents:
+                f.write(json.dumps(doc) + "\n")
 
-    ray.get(_run_dedupe.remote(dedupe_config))
+        dedupe_config = DedupeConfig(
+            input_path=temp_input_dir,
+            output_path=temp_attribute_dir,
+            attribute_name="duplicate_text",
+            min_length=0,
+            min_words=0,
+            estimated_doc_count=10000,
+            false_positive_rate=0.001,
+            ngram=NGramConfig(
+                ngram_length=8,
+                stride=0,
+                overlap_threshold=0.0,  # For debugging purposes, this outputs all possible duplicates
+            ),
+            decontaminate=True,
+            decontaminate_path=temp_decontamination_dir,
+            bloom_filter_path=temp_bloom_filter_dir,
+        )
 
-    attribute_file_path = os.path.join(BASE_ATTRIBUTE_OUTPUT_DIR, "test_docs.jsonl.gz")
-    with fsspec.open(attribute_file_path, "r", compression="gzip") as f:
-        attributes = [json.loads(line) for line in f]
+        ray.get(_run_dedupe.remote(dedupe_config))
 
-    # Duplicates
-    assert len(attributes[0]["attributes"]["duplicate_text"]) == 1
-    # The amount of overlap is 100%
-    assert attributes[0]["attributes"]["duplicate_text"][0][2] == 1
+        attribute_file_path = os.path.join(temp_attribute_dir, "test_docs.jsonl.gz")
+        with fsspec.open(attribute_file_path, "r", compression="gzip") as f:
+            attributes = [json.loads(line) for line in f]
 
-    # Only middle paragraph is a duplicate
-    assert attributes[1]["attributes"]["duplicate_text"][0][2] == 0
-    assert attributes[1]["attributes"]["duplicate_text"][1][2] == 1
-    assert attributes[1]["attributes"]["duplicate_text"][2][2] == 0
+        # Duplicates
+        assert len(attributes[0]["attributes"]["duplicate_text"]) == 1
+        # The amount of overlap is 100%
+        assert attributes[0]["attributes"]["duplicate_text"][0][2] == 1
 
-    # Not a duplicate at all
-    assert len(attributes[2]["attributes"]["duplicate_text"]) == 1
-    assert attributes[2]["attributes"]["duplicate_text"][0][2] == 0
+        # Only middle paragraph is a duplicate
+        assert attributes[1]["attributes"]["duplicate_text"][0][2] == 0
+        assert attributes[1]["attributes"]["duplicate_text"][1][2] == 1
+        assert attributes[1]["attributes"]["duplicate_text"][2][2] == 0
 
-    assert fsspec_exists(os.path.join(BLOOM_FILTER_OUTPUT_DIR, "decontaminated_bloom_filter.bin"))
+        # Not a duplicate at all
+        assert len(attributes[2]["attributes"]["duplicate_text"]) == 1
+        assert attributes[2]["attributes"]["duplicate_text"][0][2] == 0
+
+        assert fsspec_exists(os.path.join(temp_bloom_filter_dir, "decontaminated_bloom_filter.bin"))
