553050c555db29cbeefcc37dabfe9d25db8d6ece
Suhas Kotha <kotha.suhas@gmail.com>
2025-08-12 16:00:51 -0700

    seed science key

--- experiments/data_efficiency/seed_science.py
+++ experiments/data_efficiency/seed_science.py
@@ -4,8 +4,14 @@ Mid-training setup where we have a single learning rate schedule and a different
 Hyperparameters are tuned for the baseline of all data at the end of training.
 """
 
-from experiments.data_efficiency.train import DataEfficiencyConfig, data_efficiency_train_step, data_efficiency_eval_ensemble
+from experiments.data_efficiency.train import (
+    DataEfficiencyConfig,
+    data_efficiency_train_step,
+    data_efficiency_eval_ensemble,
+)
 from marin.execution.executor import executor_main
+
+key = "seed-science-8-7"
 
 seed_science_train_steps_dict = {
     (train_seed, data_seed): data_efficiency_train_step(
@@ -20,32 +26,50 @@ seed_science_train_steps_dict = {
             lr=lr,
             weight_decay=weight_decay,
             wandb_project_name="suhas-data-efficiency",
-            wandb_additional_tags=["seed-science-7-9"],
+            wandb_additional_tags=[key],
             model_name=model_name,
             nametag=f"-ts{train_seed}-ds{data_seed}",
+            tpu_type="v4-16",
         )
     )
     for base_train_steps in [800]
     for epochs in [16]
     for lr in [3e-3]
-    for weight_decay in [0.8]
+    for weight_decay in [1.6]
     for batch_size in [64]
-    for model_name in ["150m4k"]
-    for train_seed, data_seed in [(0, i) for i in range(5)] + [(i, 0) for i in range(1, 5)] + [(i, i) for i in range(1, 5)]
+    for model_name in ["300m4k"]
+    for train_seed, data_seed in [(0, i) for i in range(5)]
+    + [(i, 0) for i in range(1, 5)]
+    + [(i, i) for i in range(1, 5)]
 }
 
 seed_science_train_steps = list(seed_science_train_steps_dict.values())
 
 ensemble_eval_steps = [
     [
-        data_efficiency_eval_ensemble([seed_science_train_steps_dict[(0, i)] for i in range(max_runs - 1, -1, -1)], key="seed-science-7-9", run_prefix="ss", name_prefix=""),
-        data_efficiency_eval_ensemble([seed_science_train_steps_dict[(i, 0)] for i in range(max_runs - 1, -1, -1)], key="seed-science-7-9", run_prefix="ss", name_prefix=""),
-        data_efficiency_eval_ensemble([seed_science_train_steps_dict[(i, i)] for i in range(max_runs - 1, -1, -1)], key="seed-science-7-9", run_prefix="ss", name_prefix=""),
+        data_efficiency_eval_ensemble(
+            [seed_science_train_steps_dict[(0, i)] for i in range(max_runs - 1, -1, -1)],
+            key=key,
+            run_prefix="ss",
+            name_prefix="",
+        ),
+        data_efficiency_eval_ensemble(
+            [seed_science_train_steps_dict[(i, 0)] for i in range(max_runs - 1, -1, -1)],
+            key=key,
+            run_prefix="ss",
+            name_prefix="",
+        ),
+        data_efficiency_eval_ensemble(
+            [seed_science_train_steps_dict[(i, i)] for i in range(max_runs - 1, -1, -1)],
+            key=key,
+            run_prefix="ss",
+            name_prefix="",
+        ),
     ]
     for max_runs in range(1, 6)
 ]
 
-ensemble_eval_steps = sum(ensemble_eval_steps, [])
+ensemble_eval_steps = [step for sublist in ensemble_eval_steps for step in sublist]
 
 if __name__ == "__main__":
     executor_main(
