9bb12aacadb89871eefed332fedb6599a05a1a1b
David Hall <dlwh@cs.stanford.edu>
2025-08-29 15:29:30 -0700

    Update copyright year in add_apache_license.py

--- scripts/add_apache_license.py
+++ scripts/add_apache_license.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2024 The Marin Authors
+# Copyright 2025 The Marin Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
