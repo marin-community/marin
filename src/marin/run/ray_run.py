b51d57ef2130a691a376559aad9d2af8f158608e
Russell Power <russell.power@gmail.com>
2025-09-16 18:57:45 -0700

    move auto-stop changes to separate branch

--- src/marin/run/ray_run.py
+++ src/marin/run/ray_run.py
@@ -75,8 +75,6 @@ async def submit_and_track_job(
     extra: str,
     env_vars: dict,
     no_wait: bool,
-    submission_id: str,
-    client: JobSubmissionClient,
     *,
     entrypoint_num_cpus: float | None = None,
     entrypoint_num_gpus: float | None = None,
@@ -86,6 +84,7 @@ async def submit_and_track_job(
     """Submit a job to Ray and optionally track logs."""
 
     current_dir = os.getcwd()
+    client = JobSubmissionClient(REMOTE_DASHBOARD_URL)
 
     # Inject GIT_COMMIT into the environment for logging
     env_vars["GIT_COMMIT"] = subprocess.getoutput("git rev-parse HEAD")
@@ -93,11 +92,12 @@ async def submit_and_track_job(
     logger.info(f"Submitting job with entrypoint: {entrypoint}")
     logger.info(f"Extras: {extra}")
     logger.info(f"env_vars: {json.dumps(env_vars, indent=4)}")
+    submission_id = generate_submission_id(entrypoint)
 
     runtime_dict = {
         "working_dir": current_dir,
         "config": {"setup_timeout_seconds": 1800},
-        "excludes": [".git", "tests/", "docs/"],
+        "excludes": [".git", "tests/"],
     }
 
     # add the TPU dependency for cluster jobs.
@@ -181,7 +181,6 @@ def main():
         default=None,
         help="TPU type to reserve for the entrypoint (e.g. v4-8)",
     )
-    parser.add_argument("--auto-stop", action="store_true", help="Automatically stop the cluster on shutdown.")
     parser.add_argument("cmd", help="The command to run in the Ray cluster.", nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
@@ -236,29 +235,18 @@ def main():
         entrypoint_resources = (entrypoint_resources or {}) | tpu_res
 
     # Submit the job and track it asynchronously
-    submission_id = generate_submission_id(full_cmd)
-    client = JobSubmissionClient(REMOTE_DASHBOARD_URL)
-    try:
-        asyncio.run(
-            submit_and_track_job(
-                full_cmd,
-                args.extra,
-                env_vars,
-                args.no_wait,
-                submission_id=submission_id,
-                client=client,
-                entrypoint_num_cpus=args.entrypoint_num_cpus,
-                entrypoint_num_gpus=args.entrypoint_num_gpus,
-                entrypoint_memory=args.entrypoint_memory,
-                entrypoint_resources=entrypoint_resources,
-            )
+    asyncio.run(
+        submit_and_track_job(
+            full_cmd,
+            args.extra,
+            env_vars,
+            args.no_wait,
+            entrypoint_num_cpus=args.entrypoint_num_cpus,
+            entrypoint_num_gpus=args.entrypoint_num_gpus,
+            entrypoint_memory=args.entrypoint_memory,
+            entrypoint_resources=entrypoint_resources,
         )
-    except KeyboardInterrupt:
-        pass
-    finally:
-        if args.auto_stop:
-            logger.info(f"Auto-stopping job {submission_id}...")
-            client.stop_job(submission_id)
+    )
 
 
 if __name__ == "__main__":
