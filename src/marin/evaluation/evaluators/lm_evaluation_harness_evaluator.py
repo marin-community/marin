a6018618eec1ff8eee6c994ee9230c8e42f3df5c
Christopher Chou <49086305+BabyChouSr@users.noreply.github.com>
2025-09-19 16:17:49 -0700

    Update logprob and evaluation to take wandb tags (#1614)
    * Refactor evaluation files
    
    * Refactor utils
    
    * Refactor logprob file to not hardcode the tpu type
    
    * Restore
    
    * Add scheduling strategy change
    
    * Add scheduling strategy
    
    * Add tpu type
    
    * Fix entropy viz dry run
    
    * Add wandb tags to evals api

--- src/marin/evaluation/evaluators/lm_evaluation_harness_evaluator.py
+++ src/marin/evaluation/evaluators/lm_evaluation_harness_evaluator.py
@@ -52,6 +52,7 @@ class LMEvaluationHarnessEvaluator(VllmTpuEvaluator):
         evals: list[EvalTaskConfig],
         output_path: str,
         max_eval_instances: int | None = None,
+        wandb_tags: list[str] | None = None,
     ) -> None:
         """
         Runs EleutherAI's lm-eval harness on the specified model and set of  tasks.
@@ -90,9 +91,14 @@ class LMEvaluationHarnessEvaluator(VllmTpuEvaluator):
                 evaluation_tracker_args = simple_parse_args_string(f",output_path={result_filepath}")
                 evaluation_tracker = EvaluationTracker(**evaluation_tracker_args)
 
-                wandb_args_dict = simple_parse_args_string(f"project=marin,job_type=eval,name={model.name}")
-                wandb_config_args_dict = simple_parse_args_string("")
-                wandb_logger = WandbLogger(**wandb_args_dict, **wandb_config_args_dict)
+                wandb_args_dict = {
+                    "project": "marin",
+                    "job_type": "eval",
+                    "name": model.name,
+                    "tags": wandb_tags,
+                }
+                # wandb_config_args_dict = simple_parse_args_string("")
+                wandb_logger = WandbLogger(**wandb_args_dict)
 
                 results = simple_evaluate(
                     model="vllm",
--- src/marin/generation/ray_utils.py
+++ src/marin/generation/ray_utils.py
@@ -17,11 +17,16 @@ from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 
 
 # Used in normal ray remote args scheduling strategies
-def scheduling_strategy_fn(tensor_parallel_size: int, strategy: str):
+def scheduling_strategy_fn(
+    tensor_parallel_size: int, strategy: str, tpu_type: str, include_head_in_scheduling_strategy: bool
+):
 
     # One bundle per tensor parallel worker
+    tpus_and_cpus = [{"TPU": 1, "CPU": 1}] * tensor_parallel_size
+    tpu_head = [{f"{tpu_type}-head": 1}]
+
     pg = ray.util.placement_group(
-        [{"TPU": 1, "CPU": 1}] * tensor_parallel_size,
+        tpus_and_cpus + tpu_head,
         strategy=strategy,  # STRICT_PACK means same node, PACK can span multiple nodes
     )
     return PlacementGroupSchedulingStrategy(pg, placement_group_capture_child_tasks=True)
