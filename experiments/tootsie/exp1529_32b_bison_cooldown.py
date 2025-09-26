5f16342f632b99235c7eacb639bd1dd859312241
Helw150 <wbh230@nyu.edu>
2025-09-26 03:48:39 -0400

    Lint

--- experiments/tootsie/exp1529_32b_bison_cooldown.py
+++ experiments/tootsie/exp1529_32b_bison_cooldown.py
@@ -1,3 +1,17 @@
+# Copyright 2025 The Marin Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 https://github.com/marin-community/marin/issues/1529
 
@@ -130,7 +144,7 @@ tootsie_32b_cooldown_bison = default_train(
 
 # Loss Spiked, see if flat LR works. (Will: It doesn't but preserving for posterity)
 
-qwen_phase3_checkpoint_for_phase4 = tootsie_32b_cooldown_bison.cd(f"checkpoints/step-190000").nonblocking()
+qwen_phase3_checkpoint_for_phase4 = tootsie_32b_cooldown_bison.cd("checkpoints/step-190000").nonblocking()
 
 bison_train_config_flat = dataclasses.replace(
     bison_train_config,
