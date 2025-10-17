c03b960976f5879de1d14500f39c553ee1c4d456
Ziqing Huang <ziqingh@stanford.edu>
2025-10-16 18:03:30 -0700

    Add uncheatable evals to default

--- experiments/defaults.py
+++ experiments/defaults.py
@@ -189,7 +189,11 @@ def default_tokenize(
 
 @lru_cache  # LRU to make the executor happier
 def default_validation_sets(tokenizer: str, base_path: str = "tokenized/") -> dict[str, TokenizerStep]:
-    return paloma_tokenized(base_path=base_path, tokenizer=tokenizer)
+    from experiments.evals.exp1600_uncheatable_evals import uncheatable_eval_tokenized
+
+    validation_sets = dict(paloma_tokenized(base_path=base_path, tokenizer=tokenizer))
+    validation_sets.update(uncheatable_eval_tokenized(base_path=base_path, tokenizer=tokenizer))
+    return validation_sets
 
 
 def simulated_epoching_train(
