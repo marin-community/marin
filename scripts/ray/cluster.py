9e94a0b394db1d70b6cbc2269dc6df1ad6f5fa81
Ryan Williams <ryan.williams@openathena.ai>
2025-10-18 01:17:06 -0400

    rm unused import

--- scripts/ray/cluster.py
+++ scripts/ray/cluster.py
@@ -33,7 +33,7 @@ from pathlib import Path
 
 import click
 
-from marin.cluster import cleanup, gcp, monitoring, ray
+from marin.cluster import gcp, monitoring, ray
 from marin.cluster.cleanup import cleanup_iteration, submit_cleanup_cron_job
 from marin.cluster.config import (
     RayClusterConfig,
