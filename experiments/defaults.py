2168590f596b7d8f2dab793e889931d67097cb4d
Pinlin [Calvin] Xu <calvinxu806@gmail.com>
2025-10-28 19:52:45 -0700

    check in changes; temporarily enable TP

--- experiments/defaults.py
+++ experiments/defaults.py
@@ -325,6 +325,8 @@ def default_train(
     inner_config = TrainLmConfig(
         data=pretraining_data,
         trainer=TrainerConfig(
+            model_axis_size=2,
+            tensor_parallel_axes=["heads"],
             tracker=WandbConfig(
                 project="marin",
                 tags=[*tags],
@@ -404,7 +406,7 @@ def default_train(
     return ExecutorStep(
         name=os.path.join("checkpoints", name),
         description=(
-            f"Train a {compute_num_parameters(model_config, vocab_size) :,} parameter model for "
+            f"Train a {compute_num_parameters(model_config, vocab_size):,} parameter model for "
             f"{train_config.num_train_steps} (steps) * "
             f"{train_config.train_batch_size} (batch_size) * "
             f"{model_config.seq_len} (seq_len) "
