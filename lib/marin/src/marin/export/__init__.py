589fd98f85a56071a7d109a691f74f98515d75b1
William Held <Wbh230@nyu.edu>
2025-11-14 13:34:22 -0500

    HuggingFace Conversion of 32B (#1996)
    * HuggingFace Conversion of 32B
    
    * Rename
    
    * Increase Concurrent GB to Avoid Deadlock
    
    * Not Needed
    
    * Fix
    
    * Remove Probably Broken Multislice report
    
    * Fix

--- lib/marin/src/marin/export/__init__.py
+++ lib/marin/src/marin/export/__init__.py
@@ -13,3 +13,8 @@
 # limitations under the License.
 
 from marin.export.hf_upload import UploadToHfConfig, _actually_upload_to_hf, upload_dir_to_hf
+from marin.export.levanter_checkpoint import (
+    ConvertCheckpointStepConfig,
+    convert_checkpoint_to_hf,
+    convert_checkpoint_to_hf_step,
+)
