2c13646ae957e773d7c9a27a182aa2c106ec69d4
krypticmouse <herumbshandilya123@gmail.com>
2025-11-16 18:25:04 -0800

    skeleton evals and stuff

---
+++ lib/marin/src/marin/evaluation/evaluators/dspy_evaluator.py
@@ -0,0 +1,38 @@
+import dspy
+import requests
+
+from openai import AsyncOpenAI
+from langprobe import EvaluateBench
+
+from marin.evaluation.evaluators.evaluator import Evaluator, ModelConfig
+from levanter.inference.openai import InferenceServer, InferenceServerConfig
+
+
+class DspyEvaluator(Evaluator):
+    def __init__(self, model: ModelConfig, endpoint: str, output_path: str, max_eval_instances: int | None = None, wandb_tags: list[str] | None = None, **kwargs):
+        super().__init__()
+
+        self.endpoint = endpoint
+        self.output_path = output_path
+        self.max_eval_instances = max_eval_instances
+        self.wandb_tags = wandb_tags
+
+        self.model = model
+        self.endpoint = self._validate_endpoint()
+        self.client = AsyncOpenAI(base_url=self.endpoint, api_key="dspy")
+        self.langprobe = EvaluateBench(kwargs)
+
+
+    def _validate_endpoint(self) -> str:
+        response = requests.get(self.endpoint + "/health")
+        if response.status_code == 200:
+            return self.endpoint
+        else:
+            return self._run_oai_server()
+
+    def _run_oai_server(self) -> str:
+        # Run oai server and return the endpoint
+        return "http://localhost:8000"
+
+    def evaluate(self, modules: dspy.Module, dataset: list[dspy.Example], optimizer: dspy.Teleprompter, **kwargs) -> None:
+        return self.langprobe.evaluate(modules, dataset, optimizer, **kwargs)
