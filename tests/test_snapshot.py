c811da61eb63bc698e642a65575350c67891b2d7
David Hall <david.hall@openathena.ai>
2026-01-01 12:10:29 -0800

    docfix

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -43,7 +43,7 @@ def read_file(file_path):
 
 
 def compare_outputs(input_name, expected_file, output_file, diff_path):
-    """Compare expected and actual output files using"""
+    """Compare expected and actual output files using difflib"""
     with open(expected_file, "r") as f, open(output_file, "r") as g:
         if f.read() == g.read():
             return
