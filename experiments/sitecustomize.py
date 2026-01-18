2fab1996cceb511a0a259d7bb9676602eae6e577
Pranshu Chaturvedi <pranshu.01.c@gmail.com>
2026-01-17 23:31:40 -0800

    Update OLMoE sweep commands and fix sitecustomize header

--- experiments/sitecustomize.py
+++ experiments/sitecustomize.py
@@ -1,3 +1,17 @@
+# Copyright 2025 The Marin Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Runtime environment import-order hardening for Ray jobs.
 
 Ray's pip runtime env uses a virtualenv that (by default) includes system
@@ -60,4 +74,3 @@ def _harden_sys_path() -> None:
 
 
 _harden_sys_path()
-
