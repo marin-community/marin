e51e5442a16d9fc3c55af40f155426c4a545245f
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2026-01-20 11:41:17 -0800

    init commit

--- lib/levanter/src/levanter/main/eval_lm.py
+++ lib/levanter/src/levanter/main/eval_lm.py
@@ -141,7 +141,7 @@ def main(config: EvalLmConfig):
             for name, dataset in config.data.validation_sets(Pos).items():
                 if config.trainer.max_eval_batches is not None:
                     dataset = dataset.take(config.trainer.max_eval_batches * config.trainer.eval_batch_size)
-                loader = DataLoader(dataset, batch_size=config.trainer.eval_batch_size)
+                loader = DataLoader(dataset, batch_size=config.trainer.eval_batch_size, axis_resources=compute_axis_mapping)
                 entropy_hist = levanter.analysis.compute_entropy_histogram(
                     model,
                     Vocab,
@@ -161,7 +161,7 @@ def main(config: EvalLmConfig):
             for name, dataset in config.data.validation_sets(Pos).items():
                 if config.trainer.max_eval_batches is not None:
                     dataset = dataset.take(config.trainer.max_eval_batches * config.trainer.eval_batch_size)
-                    loader = DataLoader(dataset, batch_size=config.trainer.eval_batch_size)
+                    loader = DataLoader(dataset, batch_size=config.trainer.eval_batch_size, axis_resources=compute_axis_mapping)
                     top2_gap_hist = levanter.analysis.compute_top2_gap_histogram(
                         model,
                         Vocab,
