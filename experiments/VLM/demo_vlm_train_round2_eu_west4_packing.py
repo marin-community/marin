4d5bc394caf7763ac42f0c0b11c1c3340a320317
ruili <ruili0@stanford.edu>
2026-01-24 13:52:01 +0000

    autosave: 2026-01-24 13:52:01

--- experiments/VLM/demo_vlm_train_round2_eu_west4_packing.py
+++ experiments/VLM/demo_vlm_train_round2_eu_west4_packing.py
@@ -78,7 +78,7 @@ BATCH_SIZE = TPU_CHIPS * PER_DEVICE_PARALLELISM * GRADIENT_ACCUMULATION_STEPS  #
 # ============================================================================
 
 # Flash attention block size (set to None to disable flash attention)
-FLASH_ATTENTION_BLOCK_SIZE = 512
+FLASH_ATTENTION_BLOCK_SIZE = 256
 
 # Vision encoder: SigLIP-like (matches google/siglip-so400m-patch14-384)
 vision_config = SiglipVisionConfig(
