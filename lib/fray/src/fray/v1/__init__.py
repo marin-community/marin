3214e70a9c746ee9d05784cf9c1aa8717c057d0f
Russell Power <russell.power@gmail.com>
2026-01-30 10:48:08 -0800

    Put back v1.

---
+++ lib/fray/src/fray/v1/__init__.py
@@ -0,0 +1,50 @@
+# Copyright 2025 The Marin Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Fray v1: Legacy execution contexts for distributed and parallel computing."""
+
+from fray.v1.cluster import (
+    Cluster,
+    CpuConfig,
+    Entrypoint,
+    EnvironmentConfig,
+    GpuConfig,
+    JobId,
+    JobInfo,
+    JobRequest,
+    LocalCluster,
+    ResourceConfig,
+    create_cluster,
+    current_cluster,
+)
+from fray.v1.cluster.base import JobStatus
+from fray.v1.isolated_env import JobGroup, TemporaryVenv
+
+__all__ = [
+    "Cluster",
+    "CpuConfig",
+    "Entrypoint",
+    "EnvironmentConfig",
+    "GpuConfig",
+    "JobGroup",
+    "JobId",
+    "JobInfo",
+    "JobRequest",
+    "JobStatus",
+    "LocalCluster",
+    "ResourceConfig",
+    "TemporaryVenv",
+    "create_cluster",
+    "current_cluster",
+]
---
+++ lib/fray/src/fray/v1/cluster/ray/tpu/__init__.py
@@ -0,0 +1,60 @@
+# Copyright 2025 The Marin Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Ray TPU orchestration for gang scheduling and multislice coordination."""
+
+from fray.v1.cluster.ray.tpu.execution import (
+    HEALTH_CHECK_TIMEOUT,
+    START_ACTOR_TIMEOUT,
+    MultisliceInfo,
+    ResourcePoolManager,
+    SliceActor,
+    SliceInfo,
+    SlicePoolManager,
+    TpuCancelled,
+    TpuFailed,
+    TPUHostActor,
+    TPUHostInfo,
+    TpuPreempted,
+    TpuRunError,
+    TpuSuccess,
+    get_current_tpu_is_preempted,
+    run_on_pod,
+    run_on_pod_multislice,
+    run_on_pod_ray,
+)
+
+__all__ = [
+    "HEALTH_CHECK_TIMEOUT",
+    "START_ACTOR_TIMEOUT",
+    "TPU_CONFIGS",
+    "MultisliceInfo",
+    "ResourcePoolManager",
+    "SliceActor",
+    "SliceInfo",
+    "SlicePoolManager",
+    "TPUConfig",
+    "TPUHostActor",
+    "TPUHostInfo",
+    "TpuCancelled",
+    "TpuFailed",
+    "TpuPreempted",
+    "TpuRunError",
+    "TpuSuccess",
+    "get_current_tpu_is_preempted",
+    "get_tpu_config",
+    "run_on_pod",
+    "run_on_pod_multislice",
+    "run_on_pod_ray",
+]
