c7f2e0ee24f4a722ead7d3b027548a79d4146333
Russell Power <russell.power@gmail.com>
2026-03-18 17:15:15 -0700

    Move harbor dependency to marin-community/harbor (#3836)
    Fixes #3835
    
    - Replace vendored `lib/harbor/` with a git dependency on
    `marin-community/harbor`
    - Remove `lib/harbor/src` from Ray PYTHONPATH (harbor now installed as a
    package)
    - Remove `lib/harbor/**` from pre-commit exclude patterns
    - Add `python_version >= '3.12'` marker on harbor optional dep (fork
    compat)

--- lib/harbor/src/harbor/registry/client/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright The Marin Authors
-# SPDX-License-Identifier: Apache-2.0
-
-from harbor.registry.client.base import BaseRegistryClient
-from harbor.registry.client.factory import RegistryClientFactory
-
-__all__ = ["BaseRegistryClient", "RegistryClientFactory"]
