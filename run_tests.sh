01f733eeff4cd9f757a49ea450a6cd8b1730ddac
ruili <ruili0@stanford.edu>
2026-03-25 21:59:38 +0000

    Remove files not for upstream

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
- uv run pytest lib/levanter/tests/test_train_image_anyres.py -vs > out1.txt 2>&1
- uv run pytest lib/levanter/tests/test_train_image.py -vs > out12.txt 2>&1 
-uv run pytest lib/levanter/tests/test_llava_onevision.py -vs > out2.txt 2>&1
-uv run pytest lib/levanter/tests/test_llava_onevision_wo_anyres.py -vs > out3.txt 2>&1
-uv run pytest lib/levanter/tests/test_image.py -vs > out4.txt 2>&1
-uv run pytest lib/levanter/tests/test_image_anyres.py -vs > out5.txt 2>&1
