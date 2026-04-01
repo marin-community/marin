d8951219d8fbe9918e76f70ee8a05292c54f6741
Helw150 <wbh230@nyu.edu>
2026-04-01 16:46:43 -0700

    Partial Review Comments

--- experiments/pretraining_datasets/hplt.py
+++ experiments/pretraining_datasets/hplt.py
@@ -10,6 +10,7 @@ adding ~450B unique tokens from European web crawls.
 
 import os.path
 
+from fray.v2 import ResourceConfig
 from marin.datakit.download.hplt import download_hplt_v3_step
 from marin.execution.executor import ExecutorStep, InputName, this_output_path, versioned
 from marin.processing.tokenize import TokenizeConfig, tokenize
@@ -49,6 +50,7 @@ def tokenize_hplt_v3(
             cache_path=this_output_path(),
             tokenizer=versioned(tokenizer),
             max_workers=max_workers,
+            worker_resources=ResourceConfig(ram="20g", disk="5g"),
         ),
     )
 
