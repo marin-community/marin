4588a96ae7306bb161c4ac4c592c3e1a962af459
teetone <tonyh.lee@yahoo.com>
2026-06-05 21:06:21 -0700

    new exps

--- experiments/posttrain/instruction_datasets.py
+++ experiments/posttrain/instruction_datasets.py
@@ -463,6 +463,15 @@ INSTRUCTION_DATASET_NAME_TO_CONFIG = {
         subsets=[],
         splits=["train"],
     ),
+    "fzzhang/qwen3_4b_chimera_fixedtopics_probval_instill_n8_valredundancy5_round1": InstructionDatasetConfig(
+        hf_dataset_id="fzzhang/qwen3_4b_chimera_fixedtopics_probval_instill_n8_valredundancy5_round1",
+        revision="6a39228",
+        adapter=multi_turn_adapter(),
+        metadata_columns=[],
+        name="fzzhang/qwen3_4b_chimera_fixedtopics_probval_instill_n8_valredundancy5_round1",
+        subsets=[],
+        splits=["train"],
+    ),
     "teetone/qwen3_8b_openthoughts4_science26K_instill_n8_valredundancy5_round1": InstructionDatasetConfig(
         hf_dataset_id="teetone/qwen3_8b_openthoughts4_science26K_instill_n8_valredundancy5_round1",
         revision="546f8a8",
