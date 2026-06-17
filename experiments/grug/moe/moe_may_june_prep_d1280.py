80c5b24b4ba1ae3ee358d8b08db18d7c9d12696a
ClassicLarry <larry36d@gmail.com>
2026-06-16 19:37:53 -0700

    june_prep_runs: switch all 6 cells from EP=4 to EP=2

--- experiments/grug/moe/moe_may_june_prep_d1280.py
+++ experiments/grug/moe/moe_may_june_prep_d1280.py
@@ -33,7 +33,7 @@ _DIM: int = 1280
 _BS: int = 256  # TPB = 256 * 4096 = 1,048,576 (~1M tokens/batch)
 _SEQ: int = 4096
 _STEPS: int = 1178741
-_EP: int = 4
+_EP: int = 2
 
 _heuristic = MoeMuonHHeuristic(min_lr_ratio=0.05)
 _model_base = _heuristic.build_model_config(_DIM, seq_len=_SEQ)
