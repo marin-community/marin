e2544ff01cde2e306a604dce91c3523de846e8df
Russell Power <607207+rjpower@users.noreply.github.com>
2026-08-01 00:34:52 +0000

    [datakit] Allocate 64 GB verifier workers

--- experiments/datakit/scripts/verify_fuzzy_dups_testbed.py
+++ experiments/datakit/scripts/verify_fuzzy_dups_testbed.py
@@ -53,8 +53,8 @@ DEFAULT_INSPECTION_LIMIT = 10_000
 TEXT_PREVIEW_CHARS = 500
 SHARED_POOL_NAME = "fuzzy-verification-testbed"
 CANDIDATE_IMPORT_VERSION = 1
-WORKER_RESOURCES = ResourceConfig(cpu=2, ram="16g", disk="8g")
-VERIFICATION_TASK_RESOURCES = ResourceConfig(cpu=2, ram="1g", disk="8g")
+WORKER_RESOURCES = ResourceConfig(cpu=2, ram="64g", disk="8g")
+VERIFICATION_TASK_RESOURCES = ResourceConfig(cpu=2, ram="60g", disk="8g")
 COORDINATOR_RESOURCES = ResourceConfig(cpu=1, ram="4g", disk="16g", preemptible=False)
 VERIFIED_COLUMNS = [
     "id",
