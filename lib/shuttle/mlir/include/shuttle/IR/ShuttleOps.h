dcd29896907b9620203840df613b50bc3702b504
David Hall <david.hall@openathena.ai>
2026-08-10 02:07:51 -0700

    Include MLIR bytecode interface for Shuttle ops

--- lib/shuttle/mlir/include/shuttle/IR/ShuttleOps.h
+++ lib/shuttle/mlir/include/shuttle/IR/ShuttleOps.h
@@ -4,6 +4,7 @@
 #ifndef SHUTTLE_IR_SHUTTLEOPS_H_
 #define SHUTTLE_IR_SHUTTLEOPS_H_
 
+#include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/AffineMap.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/OpDefinition.h"
