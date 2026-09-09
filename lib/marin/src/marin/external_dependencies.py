aead8ed03c909adefadc622bf7d92cc682de65a7
Ahmad Qamar <ahmad@openathena.ai>
2026-09-09 11:25:15 +0000

    Pin qualified pause admission and optimizer remainder runtime

--- lib/marin/src/marin/external_dependencies.py
+++ lib/marin/src/marin/external_dependencies.py
@@ -75,7 +75,7 @@ MARIN_SKYRL = ExternalDependency(
     distribution="marinskyrl",
     repository="https://github.com/marin-community/MarinSkyRL.git",
     version="0.1.0",
-    commit="c3d572822ed2208ba3e50fb42b93ac7efd84a5d6",
+    commit="00f32e2b83c687d4770473005beee015e5e9559f",
     runtime_requirements=(),
 )
 
