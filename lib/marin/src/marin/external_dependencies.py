7716d1bbf0483b6a0d6f1030a59a5aeda07ab795
Ahmad Qamar <ahmad@openathena.ai>
2026-09-10 03:15:28 +0000

    Pin independently qualified off-policy mask runtime

--- lib/marin/src/marin/external_dependencies.py
+++ lib/marin/src/marin/external_dependencies.py
@@ -75,7 +75,7 @@ MARIN_SKYRL = ExternalDependency(
     distribution="marinskyrl",
     repository="https://github.com/marin-community/MarinSkyRL.git",
     version="0.1.0",
-    commit="6bd355b8af5b5996aae5be2fa11b1cc968950e77",
+    commit="915a765822ffa74183f729115ea95259c2fd99c5",
     runtime_requirements=(),
 )
 
