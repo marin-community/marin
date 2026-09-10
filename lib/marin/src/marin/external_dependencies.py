15822af92b432d55eae0407d014e20a0e4d3ceb7
Ben Feuer <benfeuer@Bens-MacBook-Pro.local>
2026-09-10 13:05:45 -0400

    [evals] Pin repaired evaluator runtimes

--- lib/marin/src/marin/external_dependencies.py
+++ lib/marin/src/marin/external_dependencies.py
@@ -57,7 +57,7 @@ EVALCHEMY = ExternalDependency(
     distribution="evalchemy",
     repository="https://github.com/marin-community/evalchemy.git",
     version="0.1.0",
-    commit="cf66d0b14975379bf7fc5c4a7cfff1400c636f1c",
+    commit="341c1058d1c2fbe4bca687831bd927a01dff4467",
     runtime_requirements=(),
 )
 
@@ -66,7 +66,7 @@ HARBOR = ExternalDependency(
     distribution="harbor",
     repository="https://github.com/marin-community/harbor.git",
     version="0.8.1",
-    commit="05d47b73cd34ac9f481445ce68ce716e465655ba",
+    commit="ce2a55949f99f2d3baa48e5b523e0703f81280c0",
     runtime_requirements=("daytona==0.200.2", "gcsfs==2026.7.0", "pydantic-settings==2.14.2", "s3fs==2026.7.0"),
 )
 
