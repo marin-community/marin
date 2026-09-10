7c286efd19d4da6bd4ec5e9a33f9112fd06cad2c
Ben Feuer <benfeuer@Bens-MacBook-Pro.local>
2026-09-10 14:09:30 -0400

    [evals] Pin corrected DS-1000 adapter

--- lib/marin/src/marin/external_dependencies.py
+++ lib/marin/src/marin/external_dependencies.py
@@ -66,7 +66,7 @@ HARBOR = ExternalDependency(
     distribution="harbor",
     repository="https://github.com/marin-community/harbor.git",
     version="0.8.1",
-    commit="ce2a55949f99f2d3baa48e5b523e0703f81280c0",
+    commit="0420474a3544a8e917ee7e4ba42a44a0a85ed8f8",
     runtime_requirements=("daytona==0.200.2", "gcsfs==2026.7.0", "pydantic-settings==2.14.2", "s3fs==2026.7.0"),
 )
 
