7eec002b21442903e6646d5afd31f8a827160b11
Ben Feuer <benfeuer@Bens-MacBook-Pro.local>
2026-09-10 14:50:30 -0400

    [evals] Pin robust FinanceBench judgment

--- lib/marin/src/marin/external_dependencies.py
+++ lib/marin/src/marin/external_dependencies.py
@@ -57,7 +57,7 @@ EVALCHEMY = ExternalDependency(
     distribution="evalchemy",
     repository="https://github.com/marin-community/evalchemy.git",
     version="0.1.0",
-    commit="92a2b4f923124065864e4eb53a3e8925577ce09a",
+    commit="8b64112acf3dfc7469e259580264b832e0797e5b",
     runtime_requirements=(),
 )
 
