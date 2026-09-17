5eb5a79400b3234c676235886fb97b505caa3f37
Helw150 <wbh230@nyu.edu>
2026-09-17 15:47:27 -0700

    [sft] Cover GLM Terminus-2 JSON rendering

--- tests/datakit/download/test_penfever_rollouts.py
+++ tests/datakit/download/test_penfever_rollouts.py
@@ -32,7 +32,7 @@ def test_opencode_filters_rows_without_recorded_initial_prompt(prompts):
     assert transform(row) == []
 
 
-@pytest.mark.parametrize("cohort", ["minimax-m27-131k", "qwen35-122b-32k"])
+@pytest.mark.parametrize("cohort", ["glm52-terminus2", "minimax-m27-131k", "qwen35-122b-32k"])
 def test_terminal_protocol_keeps_json_responses_and_observations(cohort):
     transform = row_to_chat_doc(_dataset(cohort))
     [document] = transform(
