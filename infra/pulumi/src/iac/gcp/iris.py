d73c833f6e240679479ed53c3ad6faa99b531801
David Lundgren <david.m.lundgren@gmail.com>
2026-09-23 12:46:09 -0700

    [iac] Extend contractor Iris access across clusters

--- infra/pulumi/src/iac/gcp/iris.py
+++ infra/pulumi/src/iac/gcp/iris.py
@@ -50,6 +50,7 @@ def iam_grants(project: str, principals: Mapping[str, GcpEncryptedMember]) -> Gc
                         role="roles/iap.httpsResourceAccessor",
                         members=(
                             f"serviceAccount:iris-controller@{project}.iam.gserviceaccount.com",
+                            f"serviceAccount:contractor@{project}.iam.gserviceaccount.com",
                             "serviceAccount:ravwojdyla@rav-openathena.iam.gserviceaccount.com",
                             principals["human-014"],
                             principals["human-032"],
